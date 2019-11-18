dfb61db56736df9cb024ddab1ff8fdf1a4939aa9
Tobi Abiodun <abiodun.tobi@gmail.com>
2019-11-18 08:53:53 -0500

    More dashboard page

---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="chart-container">
+        <canvas ref="chart"></canvas>
+    </div>
+</template>
+
+<script>
+    import Chart from 'chart.js';
+
+    export default {
+        props: ['chartData', 'options'],
+        mounted () {
+            let ctx = this.$refs.chart;
+            new Chart(ctx, {
+                type: 'line',
+                data: this.chartData,
+                options: this.options
+            });
+
+        }
+    }
+</script>
+
+<style>
+    .chart-container {
+        width: 900px;
+        margin: auto;
+    }
+</style>
---
+++ src/utils/charts.js
@@ -0,0 +1,8 @@
+let _seed = Date.now();
+export function rand(min, max) {
+    var seed = _seed;
+    min = min === undefined ? 0 : min;
+    max = max === undefined ? 1 : max;
+    _seed = (seed * 9301 + 49297) % 233280;
+    return min + (_seed / 233280) * (max - min);
+}
