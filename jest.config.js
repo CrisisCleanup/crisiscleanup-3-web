5343bc08c928a1a06861ee36aa66ec290c4032be
Braden Mars <bradenmars@bradenmars.me>
2019-12-27 17:55:59 -0600

    test(config): add image mock

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,10 @@ module.exports = {
   // TODO: integrate coveralls or something similiar
   moduleDirectories: ["src", "node_modules"],
   moduleFileExtensions: ["js", "json", "vue"],
+  moduleNameMapper: {
+    ".*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/.jest/mocks/image.js"
+  },
   transform: {
     // compile any *.vue files with vue-jest
     ".*\\.(vue)$": "vue-jest",
