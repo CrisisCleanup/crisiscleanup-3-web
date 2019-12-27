183b7057406848aebf5b58215fbd9b79e39b7ec5
Braden Mars <bradenmars@bradenmars.me>
2019-12-27 17:46:04 -0600

    test(basecheckbox): add tests for basecheckbox component

---
+++ src/components/__tests__/BaseCheckbox.test.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * Tests for BaseCheckbox
+ *
+ * Components
+ */
+
+import { mount } from "@vue/test-utils";
+import BaseCheckbox from "../BaseCheckbox";
+
+describe("BaseCheckbox", () => {
+  it("should not log any errors", () => {
+    const spy = jest.spyOn(global.console, "error");
+    mount(BaseCheckbox);
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it("should render correctly and match snapshot", () => {
+    const wrapper = mount(BaseCheckbox);
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("should match snapshot with props", () => {
+    const wrapper = mount(BaseCheckbox, {
+      propsData: { value: true }
+    });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
