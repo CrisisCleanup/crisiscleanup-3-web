dc5f30728c82114b1a3ef21fa4023328288861c1
Braden Mars <bradenmars@bradenmars.me>
2019-12-27 22:02:59 -0600

    test: add tests for OverlayMap component

---
+++ src/components/__tests__/OverlayMap.test.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * Tests for OverlayMap
+ *
+ * Components
+ */
+
+import { shallowMount } from "@vue/test-utils";
+import OverlayMap from "../OverlayMap";
+
+const mountWithOptions = () =>
+  shallowMount(OverlayMap, {
+    mocks: {
+      $log: global.console
+    }
+  });
+
+describe("OverlayMap", () => {
+  jest.mock("leaflet");
+  it("should not log any errors", () => {
+    const spy = jest.spyOn(global.console, "error");
+    mountWithOptions();
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it("should render correctly and match snapshot", () => {
+    const wrapper = mountWithOptions();
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
