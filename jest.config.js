117c0768486cf55d1b3282f14f54f641a5ba6951
Braden Mars <bradenmars@bradenmars.me>
2019-12-29 00:53:30 -0600

    test(config): fix jest module import resolution

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,8 @@ module.exports = {
   moduleFileExtensions: ["js", "json", "vue"],
   moduleNameMapper: {
     ".*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/.jest/mocks/image.js"
+      "<rootDir>/.jest/mocks/image.js",
+    "@/(.*)$": "<rootDir>/src/$1"
   },
   transform: {
     // compile any *.vue files with vue-jest
