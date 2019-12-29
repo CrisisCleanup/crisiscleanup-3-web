703261a7d950da39c10746b7bf04516ba6d09a49
Braden Mars <bradenmars@bradenmars.me>
2019-12-29 00:53:56 -0600

    test: add tests for StatusDropDown component

---
+++ src/components/__tests__/StatusDropDown.test.js
@@ -0,0 +1,79 @@
+/**
+ *
+ * Tests for StatusDropDown
+ *
+ * Components
+ */
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import Vuex from "vuex";
+import StatusDropDown from "../StatusDropDown";
+jest.mock("@/models/Status");
+
+library.add(fas);
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+// mock filter
+localVue.filter(
+  "getStatusName",
+  jest.fn(() => "status.open_unassigned")
+);
+
+const mockWorkType = {
+  id: 1,
+  name_t: "workType.ash",
+  description_t: "workTypeDescription.ash",
+  key: "ash",
+  file_prefix: "Ash"
+};
+
+const mountWithOptions = store =>
+  shallowMount(StatusDropDown, {
+    stubs: {
+      "font-awesome-icon": true,
+      "a-select": "<input type='select'>",
+      "v-popover": true,
+      badge: true
+    },
+    propsData: {
+      currentWorkType: mockWorkType
+    },
+    localVue,
+    store
+  });
+
+describe("StatusDropDown", () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store();
+  });
+
+  it("should not log any errors", () => {
+    const spy = jest.spyOn(global.console, "error");
+    mountWithOptions(store);
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it("should change current item on keypress", () => {
+    const wrapper = mountWithOptions(store);
+    // mock sanity check
+    expect(wrapper.vm.statuses.length).toBe(2);
+    expect(wrapper.vm.currentItem).toBe(1);
+    wrapper.vm.nextItem({ keyCode: 40 });
+    expect(wrapper.vm.currentItem).toBe(2);
+    wrapper.vm.nextItem({ keyCode: 40 });
+    expect(wrapper.vm.currentItem).toBe(2);
+    wrapper.vm.nextItem({ keyCode: 38 });
+    expect(wrapper.vm.currentItem).toBe(1);
+  });
+
+  it("should render correctly and match snapshot", () => {
+    const wrapper = mountWithOptions(store);
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
---
+++ src/models/__mocks__/Status.js
@@ -0,0 +1,34 @@
+export const MockStatus = [
+  {
+    id: 1,
+    status_name_t: "status.open_unassigned",
+    substatus_name_t: "substatus.unassigned",
+    description_t: "statusDescription.open_unassigned",
+    status: "open_unassigned",
+    primary_state: "open",
+    substatus: "unassigned",
+    claimed_class: "open-unassigned-claimed",
+    unclaimed_class: "open-unassigned-unclaimed",
+    completed_by_anybody: "0.00",
+    completed_by_ccu: "0.00",
+    completed_db: "0.00"
+  },
+  {
+    id: 2,
+    status_name_t: "status.open_assigned",
+    substatus_name_t: "substatus.assigned",
+    description_t: "statusDescription.open_assigned",
+    status: "open_assigned",
+    primary_state: "open",
+    substatus: "assigned",
+    claimed_class: "open-assigned-claimed",
+    unclaimed_class: "open-assigned-unclaimed",
+    completed_by_anybody: "0.25",
+    completed_by_ccu: "0.25",
+    completed_db: "0.25"
+  }
+];
+
+export default {
+  all: jest.fn(() => MockStatus)
+};
