06dc82fdb91c22fa3b52754876276081186bfd59
Braden Mars <bradenmars@bradenmars.me>
2019-12-30 00:50:14 -0600

    test: fix basebutton tests, cleanup

--- src/components/__tests__/BaseButton.test.js
+++ src/components/__tests__/BaseButton.test.js
@@ -8,37 +8,51 @@
 import { mount } from '@vue/test-utils';
 import BaseButton from '../BaseButton';
 
+const mountWithOptions = ({ props } = {}) =>
+  mount(BaseButton, {
+    stubs: ['font-awesome-icon'],
+    propsData: {
+      text: 'button',
+      alt: 'alt-text',
+      title: 'btntitle',
+      type: 'primary',
+      icon: 'dashboard',
+      suffixIcon: 'dashboard',
+      ...props,
+    },
+  });
+
 describe('BaseButton', () => {
   it('should not log any errors', () => {
     const spy = jest.spyOn(global.console, 'error');
-    mount(BaseButton);
+    mountWithOptions();
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('should perform action', () => {
-    const mockAction = jest.fn();
-
-    const wrapper = mount(BaseButton, { propsData: { action: mockAction } });
-    wrapper.find('button').trigger('click');
-    expect(wrapper.vm.loading).toBeTruthy();
-    expect(mockAction).toHaveBeenCalled();
+  it('should perform action', async () => {
+    expect.assertions(1);
+    const mockAction = () => true;
+    const props = { action: mockAction };
+    const wrapper = mountWithOptions(BaseButton, { props });
+    wrapper.vm.loading = true;
+    await wrapper.vm.performAction();
+    expect(wrapper.vm.loading).toBe(false);
   });
 
-  it('should match snapshot with props', () => {
-    const testProps = {
-      type: 'primary',
-      text: 'Button Text',
-      alt: 'alternate text',
-      size: 'large',
+  it('should perform action', async () => {
+    expect.assertions(1);
+    const mockAction = () => {
+      throw new Error();
     };
-    const wrapper = mount(BaseButton, {
-      propsData: testProps,
-    });
-    expect(wrapper.element).toMatchSnapshot();
+    const props = { action: mockAction };
+    const wrapper = mountWithOptions(BaseButton, { props });
+    wrapper.vm.loading = true;
+    await wrapper.vm.performAction();
+    expect(wrapper.vm.loading).toBe(false);
   });
 
   it('should render correctly and match snapshot', () => {
-    const wrapper = mount(BaseButton);
+    const wrapper = mountWithOptions();
     expect(wrapper.element).toMatchSnapshot();
   });
 });
