a5a2ab20daed6d98142c9e27b9bd12a224ebbca2
Braden Mars <bradenmars@bradenmars.me>
2020-01-07 02:07:01 -0600

    test(e2e): add should login test

--- cypress/integration/unauthenticated_spec.js
+++ cypress/integration/unauthenticated_spec.js
@@ -19,4 +19,12 @@ describe('The Login Page', () => {
   it('should match image snapshot', () => {
     cy.visit('/login').then(() => cy.get('div.login').toMatchImageSnapshot());
   });
+
+  it('should login', () => {
+    cy.visit('/login');
+    cy.get('input[type="email"]').type('junk@aarontitus.net');
+    cy.get('input[type="password"]').type('12345678');
+    cy.get('button').click();
+    cy.url().should('match', /login/);
+  });
 });
