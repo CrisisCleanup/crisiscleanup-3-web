5ed19f9e1ec1e7d7c60374119e3f9286bfe13f10
Braden Mars <bradenmars@bradenmars.me>
2020-01-29 23:10:42 -0600

    refactor(nav): refactor nav side bar into NavButton and NavMenu components

---
+++ src/components/navigation/NavButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <router-link
+    :to="route.to"
+    class="menu-item router-link p-2 border-b border-t border-gray-800"
+    :class="{ 'router-link-active': isActive }"
+  >
+    <div :key="route.key" class="flex flex-col items-center">
+      <ccu-icon
+        :alt="$t(`nav.${route.key}`)"
+        :type="route.icon || route.key"
+        :size="route.iconSize"
+      />
+      <div class="menu-text mt-1">{{ $t(`nav.${route.key}`) }}</div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'NavButton',
+  props: {
+    route: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    isActive() {
+      return this.$t(this.$route.name).includes(this.route.key.toLowerCase());
+    },
+  },
+};
+</script>
+
+<style scoped>
+.router-link-active.menu-item {
+  background-color: transparent;
+  border-left: solid 3px theme('colors.primary.light');
+}
+
+.menu-text {
+  line-height: 15px;
+  color: white;
+  text-decoration: none !important;
+}
+</style>
---
+++ src/components/navigation/NavMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-32 sidebar">
+    <div class="logo flex justify-center p-3">
+      <img src="@/assets/crisiscleanup_logo.png" style="height: 53px" />
+    </div>
+    <div class="menu">
+      <NavButton v-for="r in routes" :key="r.key" :route="r" />
+    </div>
+  </div>
+</template>
+
+<script>
+import NavButton from './NavButton';
+
+export default {
+  name: 'NavMenu',
+  components: { NavButton },
+  props: {
+    routes: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.sidebar {
+  background-color: #2d2d2d;
+}
+</style>
