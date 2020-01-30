0e719a794e0ec20920fe75e00db0e99f381078a5
Braden Mars <bradenmars@bradenmars.me>
2020-01-29 23:31:58 -0600

    test(nav): add Test for NavMenu

---
+++ src/components/navigation/__tests__/NavMenu.test.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * Tests for NavMenu
+ *
+ * Components/Navigation
+ */
+
+import { shallowMount } from '@vue/test-utils';
+import NavMenu from '../NavMenu';
+
+const shallowMountWithOptions = () =>
+  shallowMount(NavMenu, {
+    stubs: ['NavButton'],
+    propsData: {
+      routes: [
+        {
+          to: '/mockroute',
+          key: 'mockroute',
+          icon: 'mock',
+          iconSize: 'small',
+        },
+      ],
+    },
+  });
+
+describe('NavMenu', () => {
+  it('should not log any errors', () => {
+    const spy = jest.spyOn(global.console, 'error');
+    shallowMountWithOptions();
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('should render correctly and match snapshot', () => {
+    const wrapper = shallowMountWithOptions();
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
