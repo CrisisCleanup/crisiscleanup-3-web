f291129ffdd954bdb740472d0af6fc50948c044d
Vishnu Karthk <vishnu@arroyodev.com>
2020-02-19 17:16:57 -0600

    created caller id card base

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="name-mobile-position">
+    <!------Name------>
+    <div class="user_name"><h1>full name</h1></div>
+    <!---Mobile--->
+    <div class="mobile-number">(601) 123-4567</div>
+    </div>
+    <!---Profile Photo--->
+    <div class="profile-photo">
+      <img src="" alt="Profile Photo" />
+    </div>
+    <!---Buttons--->
+    <div class="take-callsbtn">
+      <button>Take Calls</button>
+    </div>
+    <div class="return-callsbtn">
+      <button>Return Calls</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CallerCard',
+};
+</script>
+
+<style>
+.name-mobile-position {
+  float: right;
+  padding-bottom: 20px;
+}
+.user_name {
+  font-size: 20px;
+  font-weight: bold;
+  text-align: left;
+}
+.mobile-number {
+  font-size: 18px;
+  text-align: left;
+}
+.profile-photo {
+  float: left;
+  padding-bottom: 20px;
+}
+.take-callsbtn {
+  background-color: #ffd700;
+  color: #000;
+  text-align: center;
+  float: left;
+  border: none;
+  padding: 10px 15px;
+  display: grid;
+  font-size: 16px;
+}
+:hover .take-callsbtn {
+  opacity: 0.7;
+}
+.return-callsbtn {
+  display: grid;
+  background-color: #404040;
+  color: #fefefe;
+  text-align: center;
+  float: right;
+  padding: 10px 15px;
+  font-size: 16px;
+}
+:hover .return-callsbtn {
+  opacity: 0.7;
+}
+</style>
--- src/pages/phone/Index.vue
+++ src/pages/phone/Index.vue
@@ -10,6 +10,13 @@
         class="my-4 mx-3 bg-white shadow w-64 h-auto content-center flex-wrap "
       >
         <operatorstats />
+      <div class="flex">
+        <div class="w-1/4 m-35 p-6 shadow text-base bg-white relative">
+          <contact-card></contact-card>
+        </div>
+        <div class="w-1/4 m-10 p-6 shadow bg-white relative">
+          <h1>Hello</h1>
+        </div>
       </div>
     </div>
   </div>
@@ -18,8 +25,9 @@
 <script>
 import genstatscard from '../../components/GeneralStatsCard.vue';
 import operatorstats from '../../components/OperatorStatisticsCard';
+import ContactCard from '../../components/ContactCard'
 export default {
   name: 'Phone',
-  components: { genstatscard, operatorstats },
+  components: { genstatscard, operatorstats, 'contact-card': Conta },
 };
 </script>
