f3100c66b7eb1d67d01200c1f978af70e8fec61b
trystanmeadows <trystan@arroyodev.com>
2020-02-24 14:29:41 -0600

    General Stats Card for Phone UI page

--- src/pages/phone/Index.vue
+++ src/pages/phone/Index.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="p-6 bg-gray-100 h-full overflow-auto relative">
     <div v-if="!loading">
-      <div class="flex">
-        <!-- **** Example Block **** -->
-        <div class="w-1/4 m-4 p-6 shadow text-base bg-white relative">
-          <h1>Example Block</h1>
-          <!-- <my-custom-component /> -->
-        </div>
-        <!-- ******************** -->
-      </div>
+
+    <div class='m-3 bg-white shadow w-64 h-auto content-center flex-wrap'>
+       <genstatscard />       
+    </div>
+
     </div>
   </div>
 </template>
 
 <script>
+import genstatscard from '../../components/GeneralStatsCard.vue';
 export default {
   name: 'Phone',
+  components:{ genstatscard }
+
 };
 </script>
