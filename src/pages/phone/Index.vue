a6622786ea4cd94d4b73fff780637c1173440e94
Braden Mars <bradenmars@bradenmars.me>
2020-02-25 15:18:03 -0600

    fix: merge conflicts, layout, linting

--- src/pages/phone/Index.vue
+++ src/pages/phone/Index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="py-4 px-6 bg-gray-100 h-full overflow-auto relative">
     <div v-if="!loading">
+      <div class="flex">
+        <div class="w-1/4 m-35 p-6 shadow text-base bg-white relative">
+          <contact-card />
+        </div>
+      </div>
       <div
         class="my-2 mx-3 bg-white shadow w-64 h-auto content-center flex-wrap"
       >
@@ -10,13 +15,6 @@
         class="my-4 mx-3 bg-white shadow w-64 h-auto content-center flex-wrap "
       >
         <operatorstats />
-      <div class="flex">
-        <div class="w-1/4 m-35 p-6 shadow text-base bg-white relative">
-          <contact-card></contact-card>
-        </div>
-        <div class="w-1/4 m-10 p-6 shadow bg-white relative">
-          <h1>Hello</h1>
-        </div>
       </div>
     </div>
   </div>
@@ -25,9 +23,9 @@
 <script>
 import genstatscard from '../../components/GeneralStatsCard.vue';
 import operatorstats from '../../components/OperatorStatisticsCard';
-import ContactCard from '../../components/ContactCard'
+import ContactCard from '../../components/ContactCard';
 export default {
   name: 'Phone',
-  components: { genstatscard, operatorstats, 'contact-card': Conta },
+  components: { genstatscard, operatorstats, 'contact-card': ContactCard },
 };
 </script>
