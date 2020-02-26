625a3a8f19a7a0c0ff6b4a6d0c4a42e9652d0994
trystanmeadows <trystan@arroyodev.com>
2020-02-26 12:26:12 -0600

    Phone UI Update for LeaderBoard Component

---
+++ src/components/LeaderBoard.vue
@@ -0,0 +1,54 @@
+<template>
+ <div>
+    <div class = 'mx-3 my-2 w-auto'>
+     <div class='flex flex-row justify-between'>
+     <h2 class='font-semibold text-base m-4'>Leaderboard</h2>
+     <button type='button'>All Time</button>
+     </div>
+    </div>
+    <div class='h-0 w-auto border shadow' />
+    <div class='flex flex-row justify-between content-center mx-1 my-1'>
+    <div class='flex justify-between content-center px-2'>
+    <img class='h-8 w-8 rounded-full mx-auto' src=""/>
+    <div class='flex flex-col'>
+       <p class='text-sm font-semibold justify-around' :userProfile = 'userProfile' :userStatus = 'userStatus'>{{userProfile}} {{userStatus}}</p>
+       <p class='text-xs' :userGroup = 'userGroup'>{{userGroup}}</p>
+    </div>
+    </div>
+    <div class='flex flex-row content-center '>
+    <div class='mr-5' :intake = 'intake'>{{intake}}</div>
+    <div class='mx-2' :Return = 'Return'>{{Return}}</div>
+    </div>
+    </div>
+ </div>
+</template>
+<script>
+export default { 
+    name:'leaderBoard',
+    props:{
+        userProfile:{
+            type: String,
+            default:'Calvin Baker'
+        },
+        userStatus:{
+            type: String,
+            default:'online'
+        },
+        userGroup:{
+            type: String,
+            default: 'Frozen Titanium Simpletons'
+        },
+        intake:{
+         type: Number,
+         default: 56
+        },
+        Return:{
+         type: Number,
+         default: 144
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
--- src/pages/phone/Index.vue
+++ src/pages/phone/Index.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="py-4 px-6 bg-gray-100 h-full overflow-auto relative">
+  <div class="py-4 px-6 bg-gray-100 h-full overflow-auto relative grid grid-cols-12 gap-4">
+    <div class='flex'>
     <div v-if="!loading">
-    <div class='my-2 mx-3 bg-white shadow w-64 h-auto content-center flex-wrap'>
-       <genstatscard />       
+     <div class='flex flex-row justify-around'>
+    <div class='my-2 mx-3 bg-white shadow w-3/4 h-auto content-center flex-wrap'>
+       <genstatscard />     
+    </div>
+    
+     <div class='my-2 bg-white shadow w-11/12 h-56 content-center flex-wrap'>
+      <leaderBoard />
+    </div>
     </div>
     <div class='my-4 mx-3 bg-white shadow w-64 h-auto content-center flex-wrap '>
-    <operatorstats />
+       <operatorstats />
+    </div>
     </div>
     </div>
   </div>
 </template>
-
 <script>
 import genstatscard from '../../components/GeneralStatsCard.vue';
 import operatorstats from '../../components/OperatorStatisticsCard';
+import leaderBoard from '../../components/LeaderBoard';
 export default {
   name: 'Phone',
-  components:{ genstatscard, operatorstats }
+  components:{ genstatscard, operatorstats, leaderBoard }
 
 };
 </script>
