585f6274577ed2389777192a247cb15b56dd1371
Tobi Abiodun <abiodun.tobi@gmail.com>
2020-02-27 08:38:38 -0500

    Add a few more button cases to get started with storybooks

--- src/components/__stories__/BaseButton.stories.js
+++ src/components/__stories__/BaseButton.stories.js
@@ -2,3 +2,21 @@ export default { title: 'Basics|BaseButton' };
 
 export const withText = () =>
   "<base-button type='primary' text='Button' ></base-button>";
+
+export const withNoStyling = () => "<base-button text='Button' ></base-button>";
+
+export const withIcon = () =>
+  "<base-button type='primary' text='Button' icon='file'></base-button>";
+
+export const withCustomIcon = () =>
+  "<base-button type='primary' text='Button' ccu-icon='trash'></base-button>";
+
+export const withAsyncAction = () => ({
+  template:
+    '<base-button type="primary" :action="doAsync">Load for 1 second</base-button>',
+  methods: {
+    async doAsync() {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    },
+  },
+});
