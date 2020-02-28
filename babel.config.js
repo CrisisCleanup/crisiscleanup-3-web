4527ffc05663d207d99f56cf9ebf6a662ee2f4ca
Braden Mars <bradenmars@bradenmars.me>
2020-02-28 17:29:14 -0600

    feat(pkg): update dependencies

--- babel.config.js
+++ babel.config.js
@@ -14,7 +14,10 @@ module.exports = {
       },
     ],
   ],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-transform-runtime', { corejs: 3 }],
+  ],
   env: {
     test: {
       presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
