56d10f5205f0859c0b55020ac74991230829b13e
Braden Mars <bradenmars@bradenmars.me>
2020-02-28 19:46:29 -0600

    fix(story): regenerate lockfile, fix static storybook

---
+++ .storybook/babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { modules: false }],
+    [
+      '@vue/babel-preset-jsx',
+      {
+        injectH: false,
+      },
+    ],
+    [
+      '@vue/app',
+      {
+        useBuiltIns: 'entry',
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-transform-runtime', { corejs: 3 }],
+  ],
+  env: {
+    test: {
+      presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+    },
+  },
+};
