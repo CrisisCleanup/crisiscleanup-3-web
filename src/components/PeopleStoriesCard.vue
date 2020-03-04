f1b47a2a1e27c23755166149189fd40d251f37cb
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-04 16:30:46 -0600

    prettier

--- src/components/PeopleStoriesCard.vue
+++ src/components/PeopleStoriesCard.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <!--Header-->
-    <p class="text-lg text-crisiscleanup-dark-400 justify-between">Stories from people you helped</p>
+    <p class="text-lg text-crisiscleanup-dark-400 justify-between">
+      Stories from people you helped
+    </p>
     <!--line-->
     <hr class="bg-white" />
     <!--Individual Stories Card-->
     <div class="stories">
       <i-story />
-    </div>
     </div>
   </div>
 </template>
@@ -18,6 +19,6 @@ export default {
   name: 'PeopleStoriesCard',
   components: {
     'i-story': IndividualStoriesCard,
-  }
-}
+  },
+};
 </script>
