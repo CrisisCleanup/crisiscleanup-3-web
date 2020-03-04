b0d36a359ae79a9ee76ebfc15b963bae85ad0a86
trystanmeadows <trystan@arroyodev.com>
2020-03-04 14:30:31 -0600

    LeaderBoard Update for phone UI

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -6,20 +6,27 @@
      <button type='button'>All Time</button>
      </div>
     </div>
-    <div class='h-0 w-auto border shadow' />
+    <div class='h-0 w-auto border' />
+      <div class='flex flex-row justify-end text-xs mx-2 my-4'>
+      <p class='opacity-1 mx-3'>intake</p>
+      <p class='opacity-1'>Return</p>
+     </div>
     <div class='flex flex-row justify-between content-center mx-1 my-1'>
     <div class='flex justify-between content-center px-2'>
     <img class='h-8 w-8 rounded-full mx-auto' src=""/>
     <div class='flex flex-col'>
-       <p class='text-sm font-semibold justify-around' :userProfile = 'userProfile' :userStatus = 'userStatus'>{{userProfile}} {{userStatus}}</p>
+       <p class='text-sm font-semibold justify-around' :userProfile = 'userProfile'>{{userProfile}}<font-awesome-icon class='' icon=''/></p>
+       <div class='online flex flex-clumn'>{{userStatus}}</div>
        <p class='text-xs' :userGroup = 'userGroup'>{{userGroup}}</p>
     </div>
     </div>
-    <div class='flex flex-row content-center '>
+    <div class='flex flex-row content-center'>
     <div class='mr-5' :intake = 'intake'>{{intake}}</div>
     <div class='mx-2' :Return = 'Return'>{{Return}}</div>
     </div>
+    <div class='flex flex-row content-center'>
     </div>
+   </div>
  </div>
 </template>
 <script>
@@ -50,5 +57,16 @@ export default {
 }
 </script>
 <style scoped>
-
+.online{
+ color: #00A313;
+}
+.talking{
+ color: #009BFF;
+}
+.offline{
+ color: #ED4747;
+}
+.away{
+ color: #F79820;
+}
 </style>
