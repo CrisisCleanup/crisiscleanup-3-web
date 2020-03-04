478bf35cf7b2045a4e25405b24a0d8c7bd82f947
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-04 16:28:54 -0600

    prettier/prettier fixes

--- src/components/IndividualStoriesCard.vue
+++ src/components/IndividualStoriesCard.vue
@@ -9,7 +9,13 @@
       <div class="flex-col justify-between m-3">
         <p class="text-lg justify-between">Jane Smith Story</p>
         <p class="text-sm justify-between">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
         </p>
         <!---Read Full Story Button--->
         <div id="moreinfobtn" class="more-infobtn">
@@ -26,5 +32,5 @@
 <script>
 export default {
   name: 'IndividualStoriesCard',
-}
+};
 </script>
