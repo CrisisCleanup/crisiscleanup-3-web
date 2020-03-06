08f2265a811f4bf8b136733c777d4c3af5be9f73
Braden Mars <bradenmars@bradenmars.me>
2020-03-06 01:56:11 -0600

    feat(phone): use new Loader layout component

--- src/pages/phone/Index.vue
+++ src/pages/phone/Index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="py-4 px-6 bg-gray-100 h-full overflow-auto relative">
-    <div v-if="!loading">
+  <Loader :loading="loading" class="h-full overflow-auto py-4 px-6 bg-gray-100">
+    <template #content>
       <div class="flex">
         <div
           class="my-2 mx-3 bg-white shadow w-64 h-auto content-center flex-wrap"
@@ -18,20 +18,26 @@
       >
         <operatorstats />
       </div>
-    </div>
-  </div>
+    </template>
+  </Loader>
 </template>
 
 <script>
 import User from '@/models/User';
 import { mapActions, mapState } from 'vuex';
+import Loader from '@/components/Loader';
 import genstatscard from '../../components/GeneralStatsCard.vue';
 import operatorstats from '../../components/OperatorStatisticsCard';
 import ContactCard from '../../components/ContactCard';
 
 export default {
   name: 'Phone',
-  components: { genstatscard, operatorstats, 'contact-card': ContactCard },
+  components: {
+    genstatscard,
+    operatorstats,
+    'contact-card': ContactCard,
+    Loader,
+  },
   data() {
     return {
       loading: false,
