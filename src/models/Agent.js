4bd37913837ec91d96e261f31cdf2f10f0dbd377
Braden Mars <bradenmars@bradenmars.me>
2020-03-06 00:48:56 -0600

    feat(agent): set model pkey, use relationship field for user

--- src/models/Agent.js
+++ src/models/Agent.js
@@ -4,19 +4,18 @@ import { Model } from '@vuex-orm/core';
 export default class Agent extends Model {
   static entity = 'agents';
 
+  static primaryKey = 'agent_id';
+
   static fields() {
     return {
-      agent_id: this.attr(),
+      agent_id: this.attr(null),
+      user_id: this.attr(null),
       password: this.string(''),
-      user: this.attr({}),
+      user: this.belongsTo(User, 'user_id'),
       arn: this.string(''),
       instance_id: this.string(''),
     };
   }
 
   static apiConfig = {};
-
-  get agentUser() {
-    return User.find(this.user);
-  }
 }
