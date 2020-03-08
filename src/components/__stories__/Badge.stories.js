cdcbb02d8b8624b3b25a5fd8406475101be05d83
Braden Mars <bradenmars@bradenmars.me>
2020-03-08 00:56:48 -0600

    feat(story): add story for Badge component

---
+++ src/components/__stories__/Badge.stories.js
@@ -0,0 +1,11 @@
+import Badge from '@/components/Badge';
+
+export default {
+  title: 'Elements|Badge',
+  component: Badge,
+};
+
+export const withDefaults = () => `<badge>15</badge>`;
+
+export const withProps = () =>
+  `<badge width="33px" height="33px" class="text-white bg-red-600">25</badge>`;
