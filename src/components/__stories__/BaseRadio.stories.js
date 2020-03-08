e5ace71696df619675031e625d13749555f57ae0
Braden Mars <bradenmars@bradenmars.me>
2020-03-08 03:06:01 -0500

    feat(story): add story for BaseRadio component

---
+++ src/components/__stories__/BaseRadio.stories.js
@@ -0,0 +1,15 @@
+import BaseRadio from '@/components/BaseRadio';
+
+export default {
+  title: 'Basics|BaseRadio',
+  component: BaseRadio,
+};
+
+export const withBasics = () => ({
+  data() {
+    return {
+      radioValue: '',
+    };
+  },
+  template: `<base-radio name="My Radio" label="radio1" :value="radioValue" @change="radioValue = $event"/>`,
+});
