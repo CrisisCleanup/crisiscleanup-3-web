3f6dc2167d8cde16e1e1734e4be9fd527e4b37d6
Braden Mars <bradenmars@bradenmars.me>
2020-03-09 01:40:38 -0500

    feat(link): add new BaseLink component

---
+++ src/components/BaseLink.vue
@@ -0,0 +1,36 @@
+<template>
+  <component :is="linkComponent" :to="to" :href="href" class="link">
+    <base-text :variant="textVariant">
+      <slot />
+    </base-text>
+  </component>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+import { VARIANTS as TextVariants } from '@/components/BaseText';
+
+export default {
+  name: 'BaseLink',
+  props: {
+    href: VueTypes.string,
+    textVariant: VueTypes.oneOf(TextVariants).def('body'),
+    to: VueTypes.string,
+  },
+  computed: {
+    linkComponent() {
+      return this.to ? 'router-link' : 'a';
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.link {
+  @apply text-primary-dark;
+  text-decoration: underline;
+  &:hover {
+    text-decoration: none;
+  }
+}
+</style>
