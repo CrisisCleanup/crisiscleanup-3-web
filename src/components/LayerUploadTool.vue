321e56e087063905843d44c9180345cb3323a16e
Tobi Abiodun <abiodun.tobi@gmail.com>
2020-03-10 19:57:21 -0400

    Fix layer uploads

--- src/components/LayerUploadTool.vue
+++ src/components/LayerUploadTool.vue
@@ -4,103 +4,149 @@
       <div class="flex">
         <DragDrop
           class="border w-72 bg-white"
-          :choose-title="
-            shapefileData
-              ? $t('Choose another file')
-              : $t('dragDrop.choose_files')
-          "
-          :drag-title="
-            (shapefileData && shapefileData.filename) ||
-              $t('layersVue.drag_drop_shp_kml')
-          "
+          :choose-title="$t('dragDrop.choose_files')"
+          :drag-title="$t('layersVue.drag_drop_shp_kml')"
           @files="handleFileUpload"
-        ></DragDrop>
+        >
+          <template v-if="shapefileStructure">
+            <div v-for="key in Object.keys(shapefileStructure)">
+              {{ key }}
+            </div>
+
+            <div class="text-underline text-primary-dark">
+              {{ $t('Choose another file') }}
+            </div>
+          </template>
+        </DragDrop>
       </div>
       <div
         v-if="shapefileStructure"
         class="bg-white px-2 border-t border-b border-r w-1/2"
       >
-        <div
-          class="border-b font-semibold flex justify-between items-center h-10"
-        >
-          {{ shapefileData.filename }}
+        <div v-for="data in shapefiles" class="">
+          <div
+            class="border-b font-semibold flex justify-between items-center h-10"
+          >
+            {{ data.filename }}
+            <div class="flex">
+              <base-button
+                :text="$t('actions.see_sample')"
+                size="small"
+                class="border border-black px-2 py-1"
+                :action="
+                  () => {
+                    shapefileInfo[data.filename].showingSampleModal = true;
+                    shapefileInfo = { ...shapefileInfo };
+                  }
+                "
+              />
+              <modal
+                v-if="shapefileInfo[data.filename].showingSampleModal"
+                modal-classes="bg-white max-w-lg shadow"
+                @close="
+                  () => {
+                    shapefileInfo[data.filename].showingSampleModal = false;
+                    shapefileInfo = { ...shapefileInfo };
+                  }
+                "
+              >
+                <div class="h-64 overflow-auto p-4">
+                  <div
+                    v-for="(value, key) in shapefileStructure[data.filename]
+                      .sample"
+                    :key="key"
+                  >
+                    {{ key }}: {{ value }}
+                  </div>
+                </div>
+              </modal>
+            </div>
+          </div>
           <div class="flex">
+            <div class="flex w-1/2 flex-col items-center mr-2">
+              <form-select
+                v-if="!loading"
+                :value="shapefileInfo[data.filename].shapefileKey"
+                @input="
+                  value => {
+                    shapefileInfo[data.filename].shapefileKey = value;
+                    shapefileInfo = { ...shapefileInfo };
+                  }
+                "
+                :options="data.fields"
+                :placeholder="$t('layersVue.select_key_shapefile')"
+                select-classes="bg-white border w-full"
+                class="form-field"
+              />
+
+              <textarea
+                :value="shapefileInfo[data.filename].shapefileCustomName"
+                @input="
+                  event => {
+                    shapefileInfo[data.filename].shapefileCustomName =
+                      event.target.value;
+                    shapefileInfo = { ...shapefileInfo };
+                  }
+                "
+                :placeholder="$t('layersVue.custom_name_template')"
+                rows="2"
+                class="form-field text-base w-full border border-crisiscleanup-dark-100 placeholder-crisiscleanup-dark-200 outline-none p-1 resize-none"
+              />
+            </div>
+            <div class="w-1/2">
+              <div class="flex w-full">
+                <form-select
+                  v-if="!loading"
+                  :value="shapefileInfo[data.filename].shapefileType"
+                  @input="
+                    value => {
+                      shapefileInfo[data.filename].shapefileType = value;
+                      shapefileInfo = { ...shapefileInfo };
+                    }
+                  "
+                  :options="locationTypes"
+                  item-key="id"
+                  label="name_t"
+                  select-classes="bg-white border w-full"
+                  :placeholder="$t('layersVue.location_type')"
+                  class="form-field"
+                />
+              </div>
+              <div class="flex w-full">
+                <form-select
+                  v-if="!loading"
+                  :value="shapefileInfo[data.filename].shapefileAccess"
+                  @input="
+                    value => {
+                      shapefileInfo[data.filename].shapefileAccess = value;
+                      shapefileInfo = { ...shapefileInfo };
+                    }
+                  "
+                  :options="['private', 'public', 'shared']"
+                  item-key="id"
+                  label="name_t"
+                  select-classes="bg-white border w-full"
+                  :placeholder="$t('layersVue.degree_of_sharing')"
+                  class="form-field"
+                />
+              </div>
+            </div>
+          </div>
+          <code v-if="shapefileInfo[data.filename].shapefileCustomName">{{
+            customSample(data.filename)
+          }}</code>
+          <div class="my-2 flex justify-center">
             <base-button
-              :text="$t('actions.see_sample')"
-              size="small"
-              class="border border-black px-2 py-1"
+              :text="$t('actions.upload')"
               :action="
                 () => {
-                  showingSampleModal = true;
+                  uploadShapefile(data.filename);
                 }
               "
-            />
-            <modal
-              v-if="showingSampleModal"
-              modal-classes="bg-white max-w-lg shadow"
-              @close="showingSampleModal = false"
-            >
-              <div class="h-64 overflow-auto p-4">
-                <div v-for="(value, key) in shapefileData.sample" :key="key">
-                  {{ key }}: {{ value }}
-                </div>
-              </div>
-            </modal>
-          </div>
-        </div>
-        <div class="flex">
-          <div class="flex w-1/2 flex-col items-center mr-2">
-            <form-select
-              v-if="!loading"
-              v-model="shapefileKey"
-              :options="shapefileData.fields"
-              :placeholder="$t('layersVue.select_key_shapefile')"
-              select-classes="bg-white border w-full"
-              class="form-field"
-            />
-
-            <textarea
-              v-model="shapefileCustomName"
-              :placeholder="$t('layersVue.custom_name_template')"
-              rows="4"
-              class="form-field text-base w-full border border-crisiscleanup-dark-100 placeholder-crisiscleanup-dark-200 outline-none p-1 resize-none"
+              variant="solid"
+              size="medium"
             />
           </div>
-          <div class="w-1/2">
-            <div class="flex w-full">
-              <form-select
-                v-if="!loading"
-                v-model="shapefileType"
-                :options="locationTypes"
-                item-key="id"
-                label="name_t"
-                select-classes="bg-white border w-full"
-                :placeholder="$t('layersVue.location_type')"
-                class="form-field"
-              />
-            </div>
-            <div class="flex w-full">
-              <form-select
-                v-if="!loading"
-                v-model="shapefileAccess"
-                :options="['private', 'public', 'shared']"
-                item-key="id"
-                label="name_t"
-                select-classes="bg-white border w-full"
-                :placeholder="$t('layersVue.degree_of_sharing')"
-                class="form-field"
-              />
-            </div>
-          </div>
-        </div>
-        <code v-if="shapefileCustomName">{{ customSample }}</code>
-        <div class="my-3 flex justify-center">
-          <base-button
-            :text="$t('actions.upload')"
-            :action="uploadShapefile"
-            variant="solid"
-            size="medium"
-          />
         </div>
       </div>
     </form>
@@ -121,19 +167,15 @@ export default {
       file: '',
       shapefileStructure: null,
       fileList: [],
-      shapefileKey: '',
-      shapefileCustomName: '',
-      shapefileType: null,
-      shapefileAccess: null,
-      showingSampleModal: false,
       loading: false,
+      shapefileInfo: {},
     };
   },
   computed: {
     locationTypes() {
       return LocationType.all();
     },
-    tableData() {
+    shapefiles() {
       if (!this.shapefileStructure) {
         return [];
       }
@@ -147,31 +189,6 @@ export default {
         };
       });
     },
-    shapefileData() {
-      if (this.tableData.length) {
-        const [shapeFileData] = this.tableData;
-        return shapeFileData;
-      }
-      return null;
-    },
-    customSample() {
-      let returnString = this.shapefileCustomName;
-      const matches = this.shapefileCustomName.match(/{.+?}/g);
-      if (matches) {
-        const replaceArray = matches.map(match =>
-          match.replace('{', '').replace('}', ''),
-        );
-        for (let i = 0; i <= replaceArray.length - 1; i++) {
-          if (this.shapefileData.sample[replaceArray[i]]) {
-            returnString = returnString.replace(
-              `{${replaceArray[i]}}`,
-              this.shapefileData.sample[replaceArray[i]],
-            );
-          }
-        }
-      }
-      return returnString;
-    },
   },
   async mounted() {
     await LocationType.api().get('/location_types', {
@@ -182,6 +199,26 @@ export default {
     });
   },
   methods: {
+    customSample(filename) {
+      let returnString = this.shapefileInfo[filename].shapefileCustomName;
+      const matches = this.shapefileInfo[filename].shapefileCustomName.match(
+        /{.+?}/g,
+      );
+      if (matches) {
+        const replaceArray = matches.map(match =>
+          match.replace('{', '').replace('}', ''),
+        );
+        for (let i = 0; i <= replaceArray.length - 1; i++) {
+          if (this.shapefileStructure[filename].sample[replaceArray[i]]) {
+            returnString = returnString.replace(
+              `{${replaceArray[i]}}`,
+              this.shapefileStructure[filename].sample[replaceArray[i]],
+            );
+          }
+        }
+      }
+      return returnString;
+    },
     async handleFileUpload(fileList) {
       this.fileList = fileList;
 
@@ -204,20 +241,27 @@ export default {
       );
       this.loading = false;
       this.shapefileStructure = result.data;
+      Object.keys(this.shapefileStructure).forEach(key => {
+        this.shapefileInfo[key] = { showingSampleModal: false };
+      });
     },
-    async uploadShapefile() {
-      if (!this.validateUpload()) {
+    async uploadShapefile(filename) {
+      if (!this.validateUpload(filename)) {
         return;
       }
 
       const formData = new FormData();
       formData.append('file', this.fileList[0]);
-      formData.append('key', this.shapefileKey);
-      formData.append('note_key', this.shapefileKey);
-      formData.append('type', this.shapefileType);
+      formData.append('key', this.shapefileInfo[filename].shapefileKey);
+      formData.append('note_key', this.shapefileInfo[filename].shapefileKey);
+      formData.append('type', this.shapefileInfo[filename].shapefileType);
+      formData.append('filename', filename);
       formData.append('shared', this.shapefileAccess || 'shared');
-      if (this.shapefileCustomName) {
-        formData.append('name_template', this.shapefileCustomName);
+      if (this.shapefileInfo[filename].shapefileCustomName) {
+        formData.append(
+          'name_template',
+          this.shapefileInfo[filename].shapefileCustomName,
+        );
       }
 
       try {
@@ -232,14 +276,16 @@ export default {
           },
         );
         this.$emit('addedLayer', results.data);
-        this.shapefileStructure = null;
         await this.$toasted.success(this.$t('layersVue.successful_upload'));
       } catch (error) {
         await this.$toasted.error(getErrorMessage(error));
       }
     },
-    validateUpload() {
-      if (!this.shapefileCustomName && !this.shapefileKey) {
+    validateUpload(filename) {
+      if (
+        !this.shapefileInfo[filename].shapefileCustomName ||
+        !this.shapefileInfo[filename].shapefileKey
+      ) {
         this.$toasted.error(this.$t('layersVue.please_provide_name'));
         return false;
       }
@@ -252,6 +298,6 @@ export default {
 
 <style scoped>
 .form-field {
-  @apply my-2;
+  @apply my-1;
 }
 </style>
