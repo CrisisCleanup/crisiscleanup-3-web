e77551fae1818603a59b7d3ace6581a3c87bc0e2
Tobi Abiodun <abiodun.tobi@gmail.com>
2020-03-13 09:04:56 -0400

    Add success modal

--- src/pages/RequestAccess.vue
+++ src/pages/RequestAccess.vue
@@ -84,6 +84,36 @@
             :action="requestAccess"
           />
         </form>
+        <modal
+          v-if="showSuccessModal"
+          modal-classes="bg-white w-1/2 h-108 shadow p-3"
+          closeable
+          @close="$router.push('/login')"
+        >
+          <div class="flex flex-col items-center justify-center mt-32">
+            <img src="" />
+            <base-text variant="h1" class="mb-6">{{
+              $t('~~Success')
+            }}</base-text>
+            <base-text class="w-3/4 text-center" variant="body" wieght="300">{{
+              $t(
+                '~~Your request has been sent to {requested_to} at {organization} please please contact this person directly and encourage them to approve access quickly',
+              )
+            }}</base-text>
+            <base-button
+              :text="$t('~~Got It')"
+              size="large"
+              variant="solid"
+              class="mt-10"
+              :action="
+                () => {
+                  $router.push('/login');
+                }
+              "
+            ></base-button>
+          </div>
+          <div slot="footer"></div>
+        </modal>
       </div>
     </template>
   </HomeLayout>
@@ -113,6 +143,7 @@ export default {
           mobile: this.mobile,
           requested_to: this.requestedTo,
         });
+        this.showSuccessModal = true;
       } catch (error) {
         await this.$toasted.error(getErrorMessage(error));
       }
@@ -127,6 +158,7 @@ export default {
       confirmPassword: '',
       mobile: '',
       requestedTo: '',
+      showSuccessModal: false,
     };
   },
 };
