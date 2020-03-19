fb85784615c55e99612c26ef6f6bc68488160736
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-19 14:55:26 -0500

    made ConnectCallPopUp added name, language + icons

---
+++ src/components/ConnectCallPopUp.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <base-button
+      variant="outline"
+      :action="
+        () => {
+          isShowingModal = true;
+        }
+      "
+      class="m-3 px-5 py-2"
+      text="incoming call"
+    ></base-button>
+    <modal
+      v-if="isShowingModal"
+      title="You have a live incoming call from:"
+      modal-classes="w-108"
+      @ok="isShowingModal = false"
+      @close="isShowingModal = false"
+    >
+      <div class="flex flex-row justify-content">
+        <div class="flex flex-row">
+          <img
+            src="../../.storybook/littleperson.jpg"
+            alt="person icon"
+            class="px-1 h-4"
+          />
+          <!--- Survivor Name --->
+          <base-text
+            variant="body"
+            class="justify-between justify-between text-crisiscleanup-grey-400"
+            >Julie Smith</base-text
+          >
+        </div>
+        <div class="flex flex-row">
+          <img
+            src="../../.storybook/greyglobe.jpg"
+            alt="globe icon"
+            class="px-1 h-4"
+          />
+          <!--- Survivor Language --->
+          <base-text
+            variant="body"
+            class="justify-between justify-between text-crisiscleanup-grey-400"
+            >English</base-text
+          >
+        </div>
+      </div>
+    </modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConnectCallPopUp',
+  data() {
+    return {
+      isShowingModal: false,
+    };
+  },
+};
+</script>
