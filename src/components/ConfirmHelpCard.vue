dfe54d923abea8a1e30236a571ddedc943530835
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-19 02:13:55 -0500

    fixed margins on confirmhelp & inboundcalls cards

--- src/components/ConfirmHelpCard.vue
+++ src/components/ConfirmHelpCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <base-text>
+    <base-text class="m-3">
       At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
       praesentium voluptatum deleniti atque corrupti quos dolores et quas
       molestias excepturi sint occaecati cupiditate non provident, similique
--- src/components/InboundCallsCard.vue
+++ src/components/InboundCallsCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <base-text>
+    <base-text class="m-3">
       Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
       doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
       inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
