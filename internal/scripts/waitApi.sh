8dd65d53bd47877b7ae98ff73a28e5d28193503b
Braden Mars <bradenmars@bradenmars.me>
2020-03-18 22:26:13 -0500

    ci(e2e): use dockerize to wait for api

--- internal/scripts/waitApi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-printf "%s" "waiting for Crisis Cleanup Api..."
-until $(curl --output /dev/null --silent --head --fail http://localhost:5000/health); do
-  printf "%c" "."
-  sleep 5
-done
-sleep 20 # final delay
-printf "\n%s\n" "CC3Api Online!"
