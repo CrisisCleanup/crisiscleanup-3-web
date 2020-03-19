7eb19faa4cf30aae959221c8d08a2b03796449c1
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-19 17:58:08 -0500

    Added Checkbox & text tp SampleScriptCard

--- src/components/SampleScriptCard.vue
+++ src/components/SampleScriptCard.vue
@@ -6,6 +6,15 @@
         >"Crisis Cleanup Hotline, My name is Carl {{ name }}. How may I help
         you?"</base-text
       >
+      <!--- Do Not Show Again Checkbox
+      <div class="ml-auto m-3">
+        <base-checkbox>
+          <base-text variant="bodysm" class="text-crisiscleanup-grey-300"
+            >do not show this again</base-text
+          >
+        </base-checkbox>
+      </div>
+      --->
     </div>
   </div>
 </template>
