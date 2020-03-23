e4dd471189324d6b86c69cc96fc09e195a6250e6
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-22 19:47:17 -0500

    made training video responsive, added navigation

--- src/pages/Training.vue
+++ src/pages/Training.vue
@@ -5,8 +5,15 @@
     </template>
     <template #grid-content>
       <!--- Navigation --->
-      <div class="grid--nav border-2">
-        <h1>I am in grid--nav</h1>
+      <div class="grid--nav">
+        <router-link
+          v-for="item in navigation"
+          :key="item.key"
+          :to="item.route || '#'"
+          class="font-h1 font-display text-h1 text-crisiscleanup-dark-500"
+        >
+          {{ lang.nav[item.key] }}
+        </router-link>
       </div>
       <!--- Footer --->
       <div class="grid--actions border-2">
@@ -25,24 +32,27 @@
           </p>
         </div>
         <!--- Content --->
-        <div class="flex flex-row m-5 align-left">
+        <div class="flex flex-row justify-between m-5">
           <!--- Training Video --->
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/3wumEh8wcak"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
+          <div class="flex-col w-1/2 h-screen">
+            <div class="video-container">
+              <iframe
+                class="resp-video"
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/3wumEh8wcak"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              />
+            </div>
+          </div>
           <!--- Training Video Description --->
-          <div class="flex-col">
-            <h1
-              class="h-7 px-10 text-4xl font-bold text-crisiscleanup-dark-500 py-5"
-            >
+          <div class="flex-col w-1/2">
+            <h1 class="px-10 text-3xl font-bold text-crisiscleanup-dark-500">
               Mandatory Crisis Cleanup Training Video
             </h1>
-            <p class="px-10 py-5 text-xl text-crisiscleanup-dark-400">
+            <p class="px-10 text-xl text-crisiscleanup-dark-400">
               Please Take a Minute to Complete a Short Training
             </p>
           </div>
@@ -81,5 +91,44 @@ export default {
   components: {
     HomeLayout,
   },
+  data() {
+    return {
+      lang: {
+        register: this.$t('actions.register'),
+        relief_org: this.$t('publicNav.relief_orgs_only'),
+        nav: {
+          home: this.$t('publicNav.home'),
+          aboutUs: this.$t('publicNav.about_us'),
+          blog: this.$t('publicNav.blog'),
+          map: this.$t('publicNav.map'),
+          training: this.$t('publicNav.training'),
+          contact: this.$t('publicNav.contact'),
+        },
+        footer: {
+          demo: this.$t('publicNav.demo'),
+          contact: this.$t('publicNav.contact'),
+          terms: this.$t('publicNav.terms'),
+          privacy: this.$t('publicNav.privacy'),
+        },
+      },
+      navigation: HomeNavigation,
+    };
+  },
 };
 </script>
+
+<style scoped>
+.resp-video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  border: 0;
+  width: 100%;
+  height: 100%;
+}
+.video-container {
+  position: relative;
+  overflow: hidden;
+  padding-top: 56.25%;
+}
+</style>
