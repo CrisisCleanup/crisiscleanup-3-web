f40b5bb2f491df258624916969f04f48373c3867
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-23 21:46:39 -0500

    created master leaderboard card, added profile card
    - styled profile card via flex-grids
    - added content
    - imported 'leaderboardprofilecard' to 'leaderboard'

---
+++ src/components/Leaderboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="flex flex-row m-5 justify-between">
+      <base-text variant="h3">Leaderboard</base-text>
+      <!-- all time -->
+    </div>
+    <hr />
+    <div class="flex flex-col">
+      <base-text variant="h4">Calls</base-text>
+    </div>
+    <!-- Profiles -->
+    <LeaderboardProfileCard />
+  </div>
+</template>
+
+<script>
+import LeaderboardProfileCard from './LeaderboardProfileCard';
+export default {
+  name: 'Leaderboard',
+  components: {
+    LeaderboardProfileCard,
+  },
+};
+</script>
