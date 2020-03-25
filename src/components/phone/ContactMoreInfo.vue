61ed7d569fa5812b83a7124758d49d8b3cc93f60
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-25 00:23:16 -0500

    updated contactmoreinfo styling/layout/positioning

--- src/components/phone/ContactMoreInfo.vue
+++ src/components/phone/ContactMoreInfo.vue
@@ -6,26 +6,58 @@
     <div class="info">
       <div class="flex flex-col">
         <div class="justify-between flex flex-row m-3">
-          <base-text variant="h4" class="text-crisiscleanupgrey-200"
-            >Languages</base-text
-          >
-          <base-text variant="h4" class="text-crisiscleanupgrey-200"
-            >eng</base-text
-          >
+          <div class="flex flex-col w-1/2">
+            <base-text
+              variant="h4"
+              weight="300"
+              class="text-crisiscleanup-grey-800"
+              >Languages</base-text
+            >
+          </div>
+          <div class="flex flex-col">
+            <div class="align-left">
+              <div class="flex flex-row">
+                <div class="flex flex-col px-2">
+                  <tag
+                    class="text-crisiscleanup-red-300 border-crisiscleanup-red-300"
+                    >English</tag
+                  >
+                </div>
+                <div class="flex flex-col px-2">
+                  <tag
+                    class="text-crisiscleanup-dark-blue border-crisiscleanup-dark-blue"
+                    >Spanish</tag
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="justify-between flex flex-row m-3">
-          <base-text variant="h4" class="text-crisiscleanupgrey-200"
-            >Counties</base-text
-          >
-          <base-text class="text-sm">Texas, Louisiana</base-text>
+          <div class="flex flex-col w-1/2">
+            <base-text
+              variant="h4"
+              weight="300"
+              class="text-crisiscleanup-grey-800"
+              >Counties</base-text
+            >
+          </div>
+          <div class="flex flex-col w-1/2">
+            <base-text class="text-sm">Texas, Louisiana</base-text>
+          </div>
         </div>
-        <div class="justify-between flex flex-row m-3">
-          <base-text variant="h4" class="text-crisiscleanupgrey-200"
-            >My Availability</base-text
-          >
-          <base-button class="text-crisis-cleanup-yellow-900 align-right"
-            >Show</base-button
-          >
+        <div class="flex flex-row m-3">
+          <div class="flex flex-col w-1/2">
+            <base-text
+              variant="h4"
+              class="text-crisiscleanup-grey-800"
+              weight="300"
+              >My Availability</base-text
+            >
+          </div>
+          <div class="flex flex-col w-1/2">
+            <base-link class="text-crisis-cleanup-yellow-900">Show</base-link>
+          </div>
         </div>
       </div>
     </div>
