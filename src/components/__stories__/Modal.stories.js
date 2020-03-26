7089aabc684dfea7827449746d679462066bfa98
Braden Mars <bradenmars@bradenmars.me>
2020-03-26 03:06:02 -0500

    fix(story): remove unnecessary curlies from modal story

--- src/components/__stories__/Modal.stories.js
+++ src/components/__stories__/Modal.stories.js
@@ -13,7 +13,7 @@ export const basicModal = () => ({
   },
   template: `
     <div>
-      <base-button variant="outline" :action="() =>  { isShowingModal = true }" text="Show Modal"></base-button>
+      <base-button variant="outline" :action="() => (isShowingModal = true )" text="Show Modal"></base-button>
       <modal v-if="isShowingModal" title="Basic Modal" modal-classes="w-108" @ok="isShowingModal = false" @close="isShowingModal = false">
         A simple modal with a title
       </modal>
@@ -29,14 +29,14 @@ export const modalWithCustomHeaderAndFooter = () => ({
   },
   template: `
     <div>
-      <base-button variant="outline" :action="() =>  { isShowingModal = true }" text="Show Modal"></base-button>
+      <base-button variant="outline" :action="() => (isShowingModal = true )" text="Show Modal"></base-button>
       <modal v-if="isShowingModal" title="Basic Modal" modal-classes="w-108" @ok="isShowingModal = false" @close="isShowingModal = false">
         <div slot="header" class="flex p-1 justify-center bg-black text-white">
           Custom Header
         </div>
         Modal with custom header and footer
         <div slot="footer" class="flex p-1 justify-center">
-          <base-button variant="outline" :action="() =>  { isShowingModal = false }" text="Custom Close"></base-button>
+          <base-button variant="outline" :action="() =>  (isShowingModal = false)" text="Custom Close"></base-button>
         </div>
       </modal>
     </div>
