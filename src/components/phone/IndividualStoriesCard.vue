40773058eb129c7b1ec58605e5578f4abcfe8ac6
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-26 02:06:50 -0500

    added temp image to stories card, fixed spacing

--- src/components/phone/IndividualStoriesCard.vue
+++ src/components/phone/IndividualStoriesCard.vue
@@ -2,21 +2,21 @@
   <div>
     <div class="flex flex-row justify-between">
       <!--- story media/picture --->
-      <div class="flex-col m-3">
-        <img src="" alt="PFP" />
+      <div class="flex-col m-3 w-3/5">
+        <img src="@/assets/storiescardss.jpg" alt="PFP" class="w-full" />
       </div>
       <!--- story --->
-      <div class="flex-col justify-between m-3">
+      <div class="flex-col justify-between m-5 w-2/3">
         <!--- name --->
         <base-text
           variant="h2"
-          class="justify-between font-bold text-crisiscleanup-dark-400"
+          class="justify-between font-bold text-crisiscleanup-dark-400 pb-1"
           >Jane Smith Story</base-text
         >
         <!--- Date --->
         <base-text
           variant="bodysm"
-          class="justify-between py-2 text-crisiscleanup-dark-400"
+          class="justify-between text-crisiscleanup-dark-400 pb-1"
         >
           On Friday, December 3rd, you talked with Jane Smith (W5832). Team
           Rubicon helped Jane Smith yesterday.
@@ -24,7 +24,7 @@
         <!--- body blurb --->
         <base-text
           variant="bodysm"
-          class="justify-between py-2 text-crisiscleanup-dark-400"
+          class="justify-between text-crisiscleanup-dark-400 pb-1"
         >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
