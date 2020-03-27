5f273bb51cc4609e582befeb3454d409e715e379
Braden Mars <bradenmars@bradenmars.me>
2020-03-27 14:07:41 -0500

    feat(phone): add PhoneController page

---
+++ src/pages/phone/Controller.vue
@@ -0,0 +1,38 @@
+<template>
+  <phone-layout>
+    <template #grid-start>
+      <div class="grid-start">
+        <agent-block />
+      </div>
+    </template>
+    <template #grid-main>
+      <div class="grid-main">
+        <agent-board />
+      </div>
+    </template>
+    <template #grid-end>
+      <div class="grid-end flex">
+        <case-form class="shadow-xl" />
+      </div>
+    </template>
+  </phone-layout>
+</template>
+
+<script>
+import PhoneLayout from '@/layouts/Phone.vue';
+import AgentBlock from '@/components/phone/blocks/Agent.vue';
+import CaseForm from '@/pages/CaseForm';
+import AgentBoard from '@/components/phone/AgentBoard/Board.vue';
+
+export default {
+  name: 'Controller',
+  components: {
+    PhoneLayout,
+    CaseForm,
+    AgentBlock,
+    AgentBoard,
+  },
+};
+</script>
+
+<style></style>
