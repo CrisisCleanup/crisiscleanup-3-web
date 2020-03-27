f2126f6d44f3fdd2f8755eff710c1f5f2b227eba
Braden Mars <bradenmars@bradenmars.me>
2020-03-27 08:19:43 -0500

    feat(model): add PhoneStatus model

---
+++ src/models/PhoneStatus.js
@@ -0,0 +1,24 @@
+import { Model } from '@vuex-orm/core';
+
+export default class PhoneStatus extends Model {
+  static entity = 'phone_statuses';
+
+  static fields() {
+    return {
+      id: this.attr(),
+      substatus: this.string(''),
+      status: this.string(''),
+      primary_state: this.string(''),
+      substatus_name_t: this.string(''),
+      status_name_t: this.string(''),
+      primary_state_name_t: this.string(''),
+      completion: this.number(1),
+      try_again_delay: this.number(0),
+      list_order: this.number(0),
+      created_at: this.attr(),
+      updated_at: this.attr(),
+      created_by: this.attr(),
+      updated_by: this.attr(),
+    };
+  }
+}
