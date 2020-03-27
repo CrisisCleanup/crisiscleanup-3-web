19b3d1d3ec308f8d45e5594aeb835727e7eb299b
Braden Mars <bradenmars@bradenmars.me>
2020-03-27 14:00:19 -0500

    feat(phone): add new Phone layout component

---
+++ src/layouts/Phone.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="phonegrid grid-container">
+    <slot name="grid-start"></slot>
+    <slot name="grid-main"></slot>
+    <slot name="grid-end"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PhoneLayout',
+};
+</script>
+
+<style lang="scss">
+$areas: start main end;
+
+.phonegrid {
+  @apply bg-crisiscleanup-light-grey;
+  &.grid-container {
+    display: inline-grid;
+    grid-column-gap: 2rem;
+    @apply h-full p-6;
+    grid:
+      [r1] 'start main end' [r1end]
+      / 1fr 2fr 1fr;
+    .grid {
+      @each $area in $areas {
+        &--#{$area} {
+          grid-area: $area;
+        }
+      }
+    }
+  }
+}
+</style>
