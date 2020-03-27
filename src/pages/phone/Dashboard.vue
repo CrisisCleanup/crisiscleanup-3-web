5a134b013d2024daf481122eae53dd82381dc627
Braden Mars <bradenmars@bradenmars.me>
2020-03-27 14:09:36 -0500

    feat(phone): update dashboard layout/styling, use Blocks/Layout

--- src/pages/phone/Dashboard.vue
+++ src/pages/phone/Dashboard.vue
@@ -1,61 +1,28 @@
 <template>
-  <Loader :loading="loading" class="h-full overflow-auto py-4 px-6 bg-gray-100">
+  <Loader :loading="loading">
     <template #content>
-      <div class="flex flex-row justify-between">
-        <!-- Left Column -->
-        <div class="flex flex-col">
-          <!-- Contact Card -->
-          <div
-            class="my-2 mx-3 bg-white shadow w-64 h-auto content-center flex-wrap"
-          >
-            <contact-card
-              :name="currentUser.full_name"
-              :mobile="currentUser.mobile"
-              :profile-src="currentUser.profilePictureUrl"
-            />
+      <phone-layout>
+        <template #grid-start>
+          <div class="grid--start">
+            <agent-block />
           </div>
-          <!-- Agent Analytics Card -->
-          <div
-            class="my-2 mx-3 bg-white shadow w-64 h-auto content-center flex-wrap "
-          >
-            <AgentAnalyticsCard />
-          </div>
-          <!-- Operator Stats -->
-          <div
-            class="my-4 mx-3 bg-white shadow w-64 h-auto content-center flex-wrap "
-          >
-            <operatorstats />
-          </div>
-        </div>
-        <!-- Second Column -->
-        <div class="flex=col justify-between">
-          <!-- Graph PLZ ADD IT-->
-          <div class="flex flex-row w-full justify-between">
-            <!-- Leaderboard -->
-            <div
-              class="my-4 mx-3 bg-white shadow w-2/3 h-auto content-center flex-wrap "
-            >
-              <Leaderboard />
-            </div>
-            <!-- News / Training Card -->
-            <div class="flex-col">
-              <div
-                class="my-4 mx-3 bg-white shadow  h-auto content-center flex-wrap "
-              >
+        </template>
+        <template #grid-main>
+          <div class="grid--main">
+            <div class="dash-grid">
+              <div class="grid--leader">
+                <leaderboard />
+              </div>
+              <div class="grid--train">
                 <training-card />
+              </div>
+              <div class="grid--story">
+                <stories-card />
               </div>
             </div>
           </div>
-          <div class="flex flex-row justify-between">
-            <!-- Stories Card -->
-            <div
-              class="my-2 mx-3 bg-white shadow w-80 h-auto content-center flex-wrap "
-            >
-              <stories-card />
-            </div>
-          </div>
-        </div>
-      </div>
+        </template>
+      </phone-layout>
       <cc-popup :is-showing-modal="callIncoming" />
     </template>
   </Loader>
@@ -66,23 +33,22 @@ import User from '@/models/User';
 import { mapActions, mapGetters } from 'vuex';
 import Loader from '@/components/Loader.vue';
 import PeopleStoriesCard from '@/components/phone/PeopleStoriesCard.vue';
-import ContactCard from '@/components/phone/ContactCard.vue';
-import operatorstats from '@/components/phone/OperatorStatisticsCard.vue';
 import NewsTrainingCard from '@/components/phone/NewsTrainingCard.vue';
-import AgentAnalyticsCard from '@/components/phone/AgentAnalyticsCard.vue';
 import ConnectCallPopUp from '@/components/phone/ConnectCallPopUp.vue';
 import Leaderboard from '@/components/phone/Leaderboard.vue';
+import PhoneLayout from '@/layouts/Phone.vue';
+import AgentBlock from '@/components/phone/blocks/Agent.vue';
 
 export default {
   name: 'Phone',
   components: {
-    operatorstats,
-    'contact-card': ContactCard,
+    PhoneLayout,
+    AgentBlock,
+
     'stories-card': PeopleStoriesCard,
     'training-card': NewsTrainingCard,
     Loader,
     'cc-popup': ConnectCallPopUp,
-    AgentAnalyticsCard,
     Leaderboard,
   },
   data() {
@@ -111,3 +77,26 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+$areas: leader train story;
+
+.grid--main {
+  .dash-grid {
+    display: inline-grid;
+    grid-gap: 2rem;
+    grid:
+      [r1] 'leader' [r1end]
+      [r2] 'train' [r2end]
+      [r3] 'story' [r3end]
+      / auto;
+    .grid {
+      @each $area in $areas {
+        &--#{$area} {
+          grid-area: $area;
+        }
+      }
+    }
+  }
+}
+</style>
