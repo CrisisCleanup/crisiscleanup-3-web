dc427078fb7f58d21529c38eebb993a7c0018b3d
Jimmy Arroyo <jamesarroyo5@gmail.com>
2020-03-27 03:39:43 -0600

    Configured trainings modal to display a training when it was started and to also allow the user to mark their training as completed once they finished all of the modules

--- src/components/phone/TrainingsModal.vue
+++ src/components/phone/TrainingsModal.vue
@@ -6,70 +6,93 @@
       @ok="visible = false"
       @close="visible = false"
     >
-      <header class="w-full border-b border-grey p-4">
-        <base-text
-          variant="body"
-          weight="700"
-          class="text-crisiscleanup-dark-500"
-        >
-          {{ lang.header.text }}
-        </base-text>
-      </header>
-      <div class="flex">
-        <div class="flex-col m-3">
-          <img
-            src="@/assets/call_center_people.png"
-            width="200px"
-            alt="Call Center Users"
-          />
+      <div v-if="!selectedTraining">
+        <header class="w-full border-b border-grey p-4">
+          <base-text
+            variant="body"
+            weight="700"
+            class="text-crisiscleanup-dark-500"
+          >
+            {{ lang.header.text }}
+          </base-text>
+        </header>
+        <div class="flex">
+          <div class="flex-col m-3">
+            <img
+              src="@/assets/call_center_people.png"
+              width="200px"
+              alt="Call Center Users"
+            />
+          </div>
+          <div class="flex-col m-3 my-8 items-center">
+            <!-- Greeting -->
+            <base-text weight="900" class="text-xl pb-3">
+              {{ lang.subHeader.text }}
+            </base-text>
+            <base-text class="pb-3">
+              {{ lang.subHeader.secondaryText }}
+            </base-text>
+          </div>
         </div>
-        <div class="flex-col m-3 my-8 items-center">
-          <!-- Greeting -->
-          <base-text weight="900" class="text-xl pb-3">
-            {{ lang.subHeader.text }}
-          </base-text>
-          <base-text class="pb-3">
-            {{ lang.subHeader.secondaryText }}
-          </base-text>
+        <trainings-card
+          v-for="(training, idx) in trainings"
+          :key="idx"
+          :image-path="training.imagePath"
+          :description="training.description"
+          :time-to-complete="training.timeToComplete"
+          :completed="training.completed"
+          @onTrainingSelected="
+            () => {
+              selectedTraining = training;
+            }
+          "
+        ></trainings-card>
+        <!-- Footer -->
+        <div class="flex justify-around mt-10"></div>
+        <div slot="footer" class="flex p-1 justify-center mb-3">
+          <base-button
+            v-if="allTrainingCompleted"
+            variant="solid"
+            class="px-3 py-2"
+            :action="() => finishTraining()"
+          >
+            {{ lang.actions.complete.text }}
+          </base-button>
+          <base-button
+            variant="outline"
+            class="px-3 ml-3"
+            size="medium"
+            :action="() => close()"
+          >
+            {{ lang.actions.cancel.text }}
+          </base-button>
         </div>
       </div>
-      <trainings-card
-        v-for="(training, idx) in trainings"
-        :key="idx"
-        :image-path="training.imagePath"
-        :description="training.description"
-        :time-to-complete="training.timeToComplete"
-      ></trainings-card>
-      <!-- Footer -->
-      <div class="flex justify-around mt-10"></div>
-      <div slot="footer" class="flex p-1 justify-center mb-3">
-        <base-button
-          variant="solid"
-          class="px-3 py-2"
-          :action="() => complete()"
-        >
-          {{ lang.actions.complete.text }}
-        </base-button>
-        <base-button
-          variant="outline"
-          class="px-3 ml-3"
-          size="medium"
-          :action="() => close()"
-        >
-          {{ lang.actions.cancel.text }}
-        </base-button>
+      <div v-if="selectedTraining">
+        <training-detail
+          :training="selectedTraining"
+          @onCancel="selectedTraining = null"
+          @onComplete="
+            () => {
+              onTrainingCompleted(selectedTraining);
+            }
+          "
+        ></training-detail>
       </div>
+      <div slot="footer"></div>
     </modal>
   </div>
 </template>
 
 <script>
 import TrainingsCard from './TrainingsCard.vue';
+import Training from './Training.vue';
 
 export default {
   name: 'TrainingsModal',
   components: {
     'trainings-card': TrainingsCard,
+    'training-detail': Training,
   },
   props: {
     visible: Boolean,
@@ -77,18 +100,82 @@ export default {
   data() {
     return {
       isShowingTrainingModal: false,
+      selectedTraining: null,
+      allTrainingCompleted: false,
       trainings: [
         {
+          completed: false,
+          videoUrl: 'https://www.youtube.com/embed/3wumEh8wcak',
           imagePath: require('@/assets/newstrainingss.jpg'),
+          title: 'Mandatory Crisis Cleanup Training Video',
           description:
             'Then go and tempor incididunt ut labore et dolore magna aliqua.',
           timeToComplete: '10 minutes',
+          questions: [
+            {
+              prompt:
+                'You can use Crisis Cleanup to do donations management, case management, logistics, etc.',
+              answers: [
+                { text: 'True', correct: false },
+                { text: 'False', correct: true },
+              ],
+            },
+            {
+              prompt: 'How much does Crisis Cleanup cost?',
+              answers: [
+                { text: 'An arm and a leg.', correct: false },
+                { text: 'Free.', correct: true },
+                { text: 'Your firstborn child.', correct: false },
+                { text: '$1,000 per year.', correct: false },
+              ],
+            },
+            {
+              prompt: 'How much does Crisis Cleanup cost?',
+              answers: [
+                { text: 'An arm and a leg.', correct: false },
+                { text: 'Free.', correct: true },
+                { text: 'Your firstborn child.', correct: false },
+                { text: '$1,000 per year.', correct: false },
+              ],
+            },
+          ],
         },
         {
+          completed: false,
+          videoUrl: 'https://www.youtube.com/embed/3wumEh8wcak',
           imagePath: require('@/assets/newstrainingss.jpg'),
+          title: 'Mandatory Crisis Cleanup Training Video',
           description:
             'Then go and tempor incididunt ut labore et dolore magna aliqua.',
           timeToComplete: '15 minutes',
+          questions: [
+            {
+              prompt:
+                'You can use Crisis Cleanup to do donations management, case management, logistics, etc.',
+              answers: [
+                { text: 'True', correct: false },
+                { text: 'False', correct: true },
+              ],
+            },
+            {
+              prompt: 'How much does Crisis Cleanup cost?',
+              answers: [
+                { text: 'An arm and a leg.', correct: false },
+                { text: 'Free.', correct: true },
+                { text: 'Your firstborn child.', correct: false },
+                { text: '$1,000 per year.', correct: false },
+              ],
+            },
+            {
+              prompt: 'How much does Crisis Cleanup cost?',
+              answers: [
+                { text: 'An arm and a leg.', correct: false },
+                { text: 'Free.', correct: true },
+                { text: 'Your firstborn child.', correct: false },
+                { text: '$1,000 per year.', correct: false },
+              ],
+            },
+          ],
         },
       ],
     };
@@ -117,7 +204,14 @@ export default {
     },
   },
   methods: {
-    complete() {
+    onTrainingCompleted(selectedTraining) {
+      selectedTraining.completed = true;
+      this.selectedTraining = null;
+      // Check to see if all trainings are completed.
+      const trainingsCompleted = this.trainings.map(t => t.completed);
+      this.allTrainingCompleted = trainingsCompleted.every(b => b);
+    },
+    finishTraining() {
       this.$emit('onComplete', true);
       this.close();
     },
