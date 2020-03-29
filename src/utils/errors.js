aefbcbdc203592892beace38e4311d8e5afc92a8
Eric Satterwhite <esatterwhite@dcinvestors.com>
2020-03-29 14:12:11 -0500

    Fix bug with getErrorMessage that was showing first letter of error

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,9 +1,20 @@
 export function getErrorMessage(error) {
+  if (!error.response || !error.response.status) {
+    return '~~An unknown error has occurred. Try again later.';
+  }
   if (error.response.status === 500) {
     return window.vue.$i18n.t('info.error_500');
   }
+
+  const message = Array.isArray(error.response.data.errors[0])
+    ? error.response.data.errors[0].message[0]
+    : error.response.data.errors[0].message;
+
   if (error.response.status === 400) {
-    return `${error.response.data.errors[0].field}:  ${error.response.data.errors[0].message[0]}`;
+    // Show the error field, unless it is 'non_field_errors'
+    let { field } = error.response.data.errors[0];
+    field = field === 'non_field_errors' ? '' : `${field}: `;
+    return `${field}${message}`;
   }
-  return error.response.data.errors[0].message[0];
+  return message;
 }
