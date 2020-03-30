282d12d145f008a0729cdcb936b7d72b03ebdd74
Braden Mars <bradenmars@bradenmars.me>
2020-03-30 00:13:06 -0500

    feat(mixins): add user update/save methods to user mixin

--- src/mixins/user.js
+++ src/mixins/user.js
@@ -1,7 +1,29 @@
 import User from '@/models/User';
+import { getErrorMessage } from '@/utils/errors';
 import { mapGetters } from 'vuex';
 
 export default {
+  methods: {
+    async saveUser() {
+      try {
+        await User.api().patch(`/users/${this.currentUser.id}`, {
+          ...this.currentUser.$toJson(),
+          preferences: this.currentUser.preferences,
+          states: this.currentUser.states,
+        });
+      } catch (error) {
+        throw getErrorMessage(error);
+      }
+    },
+    updateUser(value, key) {
+      User.update({
+        where: this.currentUser.id,
+        data: {
+          [key]: value,
+        },
+      });
+    },
+  },
   computed: {
     ...mapGetters('auth', ['isLoggedIn', 'isOrphan', 'userId']),
     currentUser() {
