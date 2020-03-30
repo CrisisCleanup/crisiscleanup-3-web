db8350beb1cd0638182e8b65acf58f1dd18c943c
Braden Mars <bradenmars@bradenmars.me>
2020-03-30 00:59:51 -0500

    feat(phone): display language tags in agent more info

--- src/components/phone/ContactMoreInfo.vue
+++ src/components/phone/ContactMoreInfo.vue
@@ -11,39 +11,21 @@
               variant="h4"
               weight="300"
               class="text-crisiscleanup-grey-800"
-              >Languages</base-text
+              >{{ lang.languages }}</base-text
             >
           </div>
           <div class="flex flex-col">
             <div class="align-left">
-              <div class="flex flex-row">
-                <div class="flex flex-col px-2">
-                  <tag
-                    class="text-crisiscleanup-red-300 border-crisiscleanup-red-300"
-                    >English</tag
-                  >
-                </div>
-                <div class="flex flex-col px-2">
-                  <tag
-                    class="text-crisiscleanup-dark-blue border-crisiscleanup-dark-blue"
-                    >Spanish</tag
-                  >
+              <div class="flex flex-row tags">
+                <div
+                  v-for="l in languages"
+                  :key="`l_${l}`"
+                  class="flex flex-col px-2 tag-container"
+                >
+                  <tag class="tag-item">{{ l }}</tag>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="justify-between flex flex-row m-3">
-          <div class="flex flex-col w-1/2">
-            <base-text
-              variant="h4"
-              weight="300"
-              class="text-crisiscleanup-grey-800"
-              >Counties</base-text
-            >
-          </div>
-          <div class="flex flex-col w-1/2">
-            <base-text variant="body">Texas, Louisiana</base-text>
           </div>
         </div>
       </div>
@@ -55,20 +37,41 @@
 
 <script>
 import VueTypes from 'vue-types';
+import { LangMixin, UserMixin } from '@/mixins';
 export default {
   name: 'Info',
+  mixins: [UserMixin, LangMixin],
   props: {
     toggleOpen: VueTypes.bool.def(false),
+  },
+  computed: {
+    lang() {
+      return this.getLang({
+        languages: '~~Languages',
+      });
+    },
+    languages() {
+      return this.currentUser.languages.map(
+        ({ name_t }) => name_t.split(' ')[0],
+      );
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped>
-.show-btn {
-  color: #f79820;
-  text-align: left;
-  text-decoration: underline;
-  font-size: 13px;
-  padding-left: 10px;
+<style scoped lang="scss">
+.tags {
+  .tag {
+    &-container {
+      &:first-child {
+        @apply text-crisiscleanup-red-300;
+        @apply border-crisiscleanup-red-300;
+      }
+      &:last-child {
+        @apply text-crisiscleanup-lightblue-400;
+        @apply border-crisiscleanup-lightblue-400;
+      }
+    }
+  }
 }
 </style>
