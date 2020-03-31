cc986cf67afcacf72d6bf2cf6af27ae1091c3794
Braden Mars <bradenmars@bradenmars.me>
2020-03-31 16:39:57 -0500

    feat(case): add new CaseTile component

---
+++ src/components/cards/case/CaseTile.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="case-card">
+    <div class="case--head">
+      <div class="case--svg" v-html="svg" />
+      <base-text>{{ caseNumber }}</base-text>
+    </div>
+    <div class="case--body">
+      <base-text>{{ fullAddress }}</base-text>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'CaseTile',
+  props: {
+    caseNumber: VueTypes.string,
+    fullAddress: VueTypes.string,
+    state: VueTypes.string,
+    worktype: VueTypes.string,
+    svg: VueTypes.string,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.case {
+  &-card {
+    @apply bg-crisiscleanup-dark-200 p-2;
+    display: flex;
+    flex-direction: column;
+    &--head {
+      display: flex;
+      flex-grow: 1;
+    }
+    &--body {
+      display: flex;
+      flex-wrap: 1;
+      flex-grow: 1;
+    }
+  }
+}
+</style>
