f1c788e414837588058f18d6a0817a156b85265d
Braden Mars <bradenmars@bradenmars.me>
2020-03-31 16:22:57 -0500

    feat(mixins): add agent mixin for code sharing agent state

---
+++ src/mixins/agent.js
@@ -0,0 +1,54 @@
+import Pda from '@/models/Pda';
+import PhoneOutbound from '@/models/PhoneOutbound';
+import Worksite from '@/models/Worksite';
+import { mapActions, mapGetters } from 'vuex';
+
+export const AgentMixin = {
+  methods: {
+    ...mapActions('phone', ['setCaseStatus']),
+    async fetchCasesByType(caseModel, ids) {
+      const cases = await Promise.all(
+        ids.map(async (id) => {
+          if (!caseModel.exists(id)) {
+            await caseModel.api().get(`/${caseModel.entity}/${id}`);
+          }
+          return caseModel.find(id);
+        }),
+      );
+      return cases;
+    },
+  },
+  async fetchAllCases() {
+    const worksites = await this.fetchCasesByType(Worksite, this.worksites);
+    const pdas = await this.fetchCasesByType(Pda, this.pdas);
+    // not technically a case, but it works
+    const outboundIds = await this.fetchCasesByType(
+      PhoneOutbound,
+      this.outboundIds,
+    );
+    return { worksites, pdas, outboundIds };
+  },
+  computed: {
+    ...mapGetters('phone', [
+      'currentCase',
+      'callerId',
+      'pdas',
+      'worksites',
+      'caseStatusId',
+      'currentOutbound',
+      'currentCaseType',
+    ]),
+    callerName() {
+      return this.currentCase ? this.currentCase.name : 'Unknown';
+    },
+    callerTotalCases() {
+      return this.pdas.length + this.worksites.length;
+    },
+    callerHistory() {
+      return {
+        total: 2,
+        recent: 6,
+      };
+    },
+  },
+};
--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,5 +1,14 @@
+import { AgentMixin } from './agent';
 import { IconsMixin } from './enums';
 import { LangMixin, LocaleMixin } from './locale';
 import UserMixin from './user';
+import { WorksitesMixin } from './worksite';
 
-export { LocaleMixin, UserMixin, LangMixin, IconsMixin };
+export {
+  LocaleMixin,
+  UserMixin,
+  LangMixin,
+  IconsMixin,
+  AgentMixin,
+  WorksitesMixin,
+};
