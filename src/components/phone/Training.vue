a5908e9d0d662de0637602165e6b11c3ba6d9d20
Vishnu Karthik <vishnu@arroyodev.com>
2020-04-07 22:49:09 -0500

    fixed training.vue content, added translation tags

--- src/components/phone/Training.vue
+++ src/components/phone/Training.vue
@@ -27,16 +27,10 @@
       <!--- Training Video Description --->
       <div class="flex-col">
         <h1 class="px-10 my-3 text-3xl font-bold text-crisiscleanup-dark-500">
-          <!-- {{ training.title }} -->
-          {{ $t(`~~Crisis Cleanup Basic Training`) }}
+          {{ $t(training.title) }}
         </h1>
         <p class="px-10 text-xl text-crisiscleanup-dark-400">
-          <!-- {{ training.description }} -->
-          {{
-            $t(
-              `~~In this session, you will learn how the basics of how crisis clean up system works. You will know enough be comfortable creating cases, looking up records, inviting collogues, escalating cases, and asking for help.`,
-            )
-          }}
+          {{ $t(training.description) }}
         </p>
 
         <!-- <h1 class="px-10 my-6 text-2xl font-bold text-crisiscleanup-dark-500">
