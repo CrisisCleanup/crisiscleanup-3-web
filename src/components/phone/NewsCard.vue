95ce13214af68e214e59d299275b8a77f694096a
Vishnu Karthik <vishnu@arroyodev.com>
2020-04-10 00:27:17 -0500

    commented out data, added placeholders in NewsCard

--- src/components/phone/NewsCard.vue
+++ src/components/phone/NewsCard.vue
@@ -2,13 +2,18 @@
   <div>
     <div class="flex flex-row justify-between">
       <!--- media/picture --->
-      <div class="w-1/6 m-3">
-        <img :src="imagePath" alt="Video Preview" />
+      <div class="w-2/6 m-3">
+        <!-- <img :src="imagePath" alt="Video Preview" /> -->
+        <img src="@/assets/newspicss.png" class="w-auto h-full" />
       </div>
       <div class="w-4/6 my-4">
         <!--- body blurb --->
         <base-text variant="bodysm" class="justify-between">
-          {{ description }}
+          <!-- {{ description }} -->
+          {{ this.$t( `~~For those of you who have ever used our call system in the
+          past (as clunky as it was) You're going to LOVE this new system. It is
+          easier to ever! However it is brand new, so if you find bugs, please
+          let us know.`), }}
         </base-text>
         <div class="flex flex-row justify-between">
           <div class="flex-col">
