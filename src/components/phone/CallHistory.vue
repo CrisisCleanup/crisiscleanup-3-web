02e8db001384339208d852b56b8767f070282eaf
Vishnu Karthik <vishnu@arroyodev.com>
2020-04-17 23:21:12 -0500

    added column headers to CallHistory.vue

--- src/components/phone/CallHistory.vue
+++ src/components/phone/CallHistory.vue
@@ -1,3 +1,14 @@
+<template>
+  <div>
+    <div class="flex flex-row justify-between">
+      <th>Phone Number</th>
+      <th>Case #</th>
+      <th>Call Status</th>
+      <th>Notes</th>
+      <th>Call Completed</th>
+    </div>
+  </div>
+</template>
 <script>
 export default {
   name: 'CallHistory',
