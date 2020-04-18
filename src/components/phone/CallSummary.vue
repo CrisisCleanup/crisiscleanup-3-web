75dd1dc328d0b46470c823c00d1273815e97abd6
Vishnu Karthik <vishnu@arroyodev.com>
2020-04-18 12:50:24 -0500

    converted from flex to css grid table

--- src/components/phone/CallSummary.vue
+++ src/components/phone/CallSummary.vue
@@ -1,33 +1,35 @@
 <template>
-  <div>
-    <div class="flex flex-row justify-between">
-      <th>
-        <!-- {{ phone }} -->
-        <base-text variant="bodysm" weight="300">601-123-4567</base-text>
-      </th>
-      <th>
-        <!-- {{ name }} -->
-        <base-text variant="bodysm" weight="300">Julie Smith</base-text>
-      </th>
-      <th>
-        <!-- {{ caseNumber }} -->
-        <base-text variant="bodysm" weight="300">C11</base-text>
-      </th>
-      <th>
-        <!-- {{ callStatus }} -->
-        <base-text variant="bodysm" weight="300">Call Status</base-text>
-      </th>
-      <th>
-        <!-- {{ notes }} -->
-        <base-text variant="bodysm" weight="300"
-          >Lorem ipsum adshnasdsoih alsdfgnx ad aldsk asdiigh</base-text
-        >
-      </th>
-      <th>
-        <!-- {{ callCompleted }} -->
-        <base-text variant="bodysm" weight="300">10 mins ago</base-text>
-      </th>
-    </div>
+  <div class="attribute-container part-id">
+    <table>
+      <tr>
+        <th>
+          <!-- {{ phone }} -->
+          <base-text variant="bodysm" weight="300">601-123-4567</base-text>
+        </th>
+        <th>
+          <!-- {{ name }} -->
+          <base-text variant="bodysm" weight="300">Julie Smith</base-text>
+        </th>
+        <th>
+          <!-- {{ caseNumber }} -->
+          <base-text variant="bodysm" weight="300">C11</base-text>
+        </th>
+        <th>
+          <!-- {{ callStatus }} -->
+          <base-text variant="bodysm" weight="300">Call Status</base-text>
+        </th>
+        <th>
+          <!-- {{ notes }} -->
+          <base-text variant="bodysm" weight="300"
+            >Lorem ipsum adshnasdsoih alsdfgnx ad aldsk asdiigh</base-text
+          >
+        </th>
+        <th>
+          <!-- {{ callCompleted }} -->
+          <base-text variant="bodysm" weight="300">10 mins ago</base-text>
+        </th>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -36,3 +38,26 @@ export default {
   name: 'CallSummary',
 };
 </script>
+
+<style scoped>
+.table {
+  display: grid;
+}
+/* .atttribute-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(var(--column-width-min), 1fr));
+}
+.part-id {
+  --column-width-min: 10em;
+}
+.attribute {
+  display: grid;
+  grid-template-columns: minmax(9em, 30%) 1fr;
+}
+.attribute::before {
+  content: attr(data-name);
+}
+.collection-container > li:first-child {
+  display: none;
+} */
+</style>
