8c3122c330dbce528fdfa0549cca7e77d0b04a86
Braden Mars <bradenmars@bradenmars.me>
2020-04-24 10:42:53 -0500

    feat: add new TitledCard widget layout component

---
+++ src/components/phone/Cards/TitledCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <Card>
+    <template #header>
+      <div class="left">
+        <slot name="left">
+          <base-text variant="body" weight="700">
+            {{ title }}
+          </base-text>
+        </slot>
+      </div>
+      <div class="right">
+        <slot name="right"> </slot>
+      </div>
+    </template>
+    <slot />
+  </Card>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+import Card from './Card.vue';
+
+export default {
+  name: 'TitledCard',
+  components: {
+    Card,
+  },
+  props: {
+    title: VueTypes.string,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.right,
+.left {
+  p {
+    @apply text-crisiscleanup-dark-400;
+  }
+}
+</style>
