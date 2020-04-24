a2fac309ce8aed3fa602f9a645ce802e19467eb7
Braden Mars <bradenmars@bradenmars.me>
2020-04-24 10:41:47 -0500

    feat(phone): add new Card component

---
+++ src/components/phone/Cards/Card.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="bg-white shadow-crisiscleanup-card card">
+    <div class="header" slot="header">
+      <div class="header--inner">
+        <slot name="header"></slot>
+      </div>
+    </div>
+    <div class="body">
+      <div class="body--inner">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  .header {
+    @apply py-5 px-6;
+    position: relative;
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 2px;
+      opacity: 0.2;
+      background-color: #979797;
+    }
+  }
+  .body {
+    /* @apply p-4; */
+    display: flex;
+    &--inner {
+      display: flex;
+      flex-grow: 1;
+    }
+  }
+}
+</style>
