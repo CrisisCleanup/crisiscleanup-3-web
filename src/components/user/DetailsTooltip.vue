407ec0f7eff0a2a8e2448833098f4fb6f40c7498
Braden Mars <bradenmars@bradenmars.me>
2020-04-25 04:12:12 -0500

    feat: add UserDetailsTooltip component

---
+++ src/components/user/DetailsTooltip.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-popover popover-class="user-popover" placement="top-start">
+    <base-text class="details-name" variant="body">
+      <span
+        variant="body"
+        class="text-yellow-600 tooltip-target cursor-pointer"
+        >{{ userItem.full_name }}</span
+      >
+      <slot
+    /></base-text>
+    <div slot="popover">
+      <div class="text-base">{{ userItem.full_name }}</div>
+      <div class="text-xs">{{ userItem.organization.name }}</div>
+      <div class="mt-2">
+        <font-awesome-icon icon="envelope" />
+        <a :href="`mailto:${userItem.email}`" class="ml-1">{{
+          userItem.email
+        }}</a>
+      </div>
+      <div v-if="userItem.mobile">
+        <font-awesome-icon icon="phone" />
+        <a :href="`tel:${userItem.mobile}`" class="ml-1">{{
+          userItem.mobile
+        }}</a>
+      </div>
+    </div>
+  </v-popover>
+</template>
+
+<script>
+import { UserMixin } from '@/mixins';
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'UserDetailsTooltip',
+  mixins: [UserMixin],
+  props: {
+    user: VueTypes.number,
+  },
+  computed: {
+    userItem() {
+      return this.getUser(this.user);
+    },
+  },
+};
+</script>
+
+<style>
+.user-popover {
+  @apply bg-black text-white p-3 outline-none;
+  width: 230px;
+  left: 0.75rem !important;
+  z-index: 1000;
+}
+</style>
