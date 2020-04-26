e88a451f7b7a7e1bad3d8d2038202f7d20bd76ea
Braden Mars <bradenmars@bradenmars.me>
2020-04-25 23:23:12 -0500

    feat(phone): use Table in CallHistory phone widget

---
+++ src/components/phone/Widgets/CallHistory.vue
@@ -0,0 +1,67 @@
+<template>
+  <TitledCard title="~~Last 10 People I talked too">
+    <div class="card-container">
+      <Table :columns="historyCols" :data="historyData" />
+    </div>
+  </TitledCard>
+</template>
+
+<script>
+import TitledCard from '@/components/phone/Cards/TitledCard.vue';
+import Table from '@/components/Table.vue';
+
+export default {
+  name: 'CallHistory',
+  components: { TitledCard, Table },
+  computed: {
+    historyData() {
+      return [
+        {
+          // stub data
+          mobile: '+19999999999',
+          cases: ['C10', 'PDA-10'],
+          status: 'Worksite added.',
+          notes: 'User had entered a PDA',
+          completed: '10 minutes ago.',
+        },
+      ];
+    },
+    historyCols() {
+      return [
+        {
+          title: 'Phone Number',
+          dataIndex: 'mobile',
+          key: 'mobile',
+          width: '1fr',
+        },
+        {
+          title: 'Case #',
+          dataIndex: 'cases',
+          key: 'cases',
+          width: '1fr',
+        },
+        {
+          title: 'Call Status',
+          dataIndex: 'status',
+          key: 'status',
+          width: '1fr',
+        },
+        {
+          title: 'Notes',
+          dataIndex: 'notes',
+          key: 'notes',
+          width: '1fr',
+        },
+        {
+          title: 'Completed',
+          dataIndex: 'completed',
+          key: 'completed',
+          width: '1fr',
+        },
+      ];
+    },
+  },
+};
+</script>
+
+<style></style>
