e119014aa257eca1aeba663c23f60fcc8e9c526b
Braden Mars <bradenmars@bradenmars.me>
2020-04-26 04:08:50 -0500

    feat(phone): revamp my statistics card

---
+++ src/components/phone/Widgets/AgentAnalytics.vue
@@ -0,0 +1,43 @@
+<template>
+  <StatsCard
+    class="flex flex-col flex-grow"
+    card-title="~~My Statistics"
+    :metrics="metrics"
+  />
+</template>
+
+<script>
+import StatsCard from '@/components/phone/Cards/StatsCard.vue';
+import { mapGetters } from 'vuex';
+import { UserMixin } from '@/mixins';
+
+export default {
+  name: 'AgentAnalytics',
+  mixins: [UserMixin],
+  components: { StatsCard },
+  computed: {
+    ...mapGetters('phone', ['agentBoard']),
+    currentAgent() {
+      if (!this.agentBoard.length) return [];
+      return this.agentBoard.find((a) => a.user.id === this.currentUser.id);
+    },
+    metrics() {
+      if (!this.agentBoard.length) return [];
+      const stats = new Map();
+      const {
+        total_inbound,
+        total_outbound,
+        total_abandons,
+        total_rejects,
+      } = this.currentAgent;
+      stats.set('~~Received Calls', total_inbound);
+      stats.set('~~Made Calls', total_outbound);
+      stats.set('~~Rejected Calls', total_rejects);
+      stats.set('~~Abandoned Calls', total_abandons);
+      const TStats = new Map();
+      stats.forEach((value, key) => TStats.set(this.$t(key), value));
+      return TStats;
+    },
+  },
+};
+</script>
