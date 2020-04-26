e1559b2488481dcc88987ea5f426e52ff34efce7
Braden Mars <bradenmars@bradenmars.me>
2020-04-26 02:12:18 -0500

    fix(phone): prevent fetching leaderboard data before socket connection

--- src/components/phone/Widgets/Leaderboard.vue
+++ src/components/phone/Widgets/Leaderboard.vue
@@ -1,51 +1,53 @@
 <template>
   <TitledCard title="~~Leaderboard">
-    <div class="card-container">
-      <div v-for="a in agentBoard" :key="a.agent" class="item">
-        <div class="item--profile">
-          <div class="image">
-            <img
-              class="rounded-full"
-              :src="getUser(a.user.id).profilePictureUrl"
-            />
-          </div>
-          <div class="info">
-            <div class="info--user">
-              <UserDetailsTooltip
-                :name-class="'text-h3 font-h3 pr-2 text-crisiscleanup-dark-500 name-tooltip'"
-                :user="a.user.id"
+    <Loader :loading="loading" class="card-container">
+      <template #content>
+        <div v-for="a in agentBoard" :key="a.agent" class="item">
+          <div class="item--profile">
+            <div class="image">
+              <img
+                class="rounded-full"
+                :src="getUser(a.user.id).profilePictureUrl"
               />
-              <base-text
-                variant="h3"
-                weight="400"
-                :class="`dot ${getStateFriendlyName(a.currentState).replace(
-                  ' ',
-                  '',
-                )}`"
-              >
-                &#8226; {{ getStateFriendlyName(a.currentState) }}
-              </base-text>
             </div>
-            <div class="info--org">
-              <base-text variant="bodysm">
-                {{ a.organization.name }}
+            <div class="info">
+              <div class="info--user">
+                <UserDetailsTooltip
+                  :name-class="'text-h3 font-h3 pr-2 text-crisiscleanup-dark-500 name-tooltip'"
+                  :user="a.user.id"
+                />
+                <base-text
+                  variant="h3"
+                  weight="400"
+                  :class="`dot ${getStateFriendlyName(a.currentState).replace(
+                    ' ',
+                    '',
+                  )}`"
+                >
+                  &#8226; {{ getStateFriendlyName(a.currentState) }}
+                </base-text>
+              </div>
+              <div class="info--org">
+                <base-text variant="bodysm">
+                  {{ a.organization.name }}
+                </base-text>
+              </div>
+            </div>
+          </div>
+          <div class="item--metrics">
+            <div
+              class="metric"
+              v-for="m in ['total_inbound', 'total_outbound', 'total_calls']"
+              :key="m"
+            >
+              <base-text variant="h1" weight="700">
+                {{ a[m] }}
               </base-text>
             </div>
           </div>
         </div>
-        <div class="item--metrics">
-          <div
-            class="metric"
-            v-for="m in ['total_inbound', 'total_outbound', 'total_calls']"
-            :key="m"
-          >
-            <base-text variant="h1" weight="700">
-              {{ a[m] }}
-            </base-text>
-          </div>
-        </div>
-      </div>
-    </div>
+      </template>
+    </Loader>
   </TitledCard>
 </template>
 
@@ -54,6 +56,7 @@ import TitledCard from '@/components/phone/Cards/TitledCard.vue';
 import { UserMixin, AgentMixin } from '@/mixins';
 import { mapGetters } from 'vuex';
 import UserDetailsTooltip from '@/components/user/DetailsTooltip.vue';
+import Loader from '@/components/Loader.vue';
 
 export default {
   name: 'Leaderboard',
@@ -61,9 +64,16 @@ export default {
   components: {
     TitledCard,
     UserDetailsTooltip,
+    Loader,
+  },
+  data() {
+    return {
+      loading: true,
+    };
   },
   computed: {
     ...mapGetters('phone', ['agentBoard']),
+    ...mapGetters('socket', ['connected']),
     metricData() {
       return {
         intake: this.agentBoard.map((a) => a.total_inbound),
@@ -73,7 +83,11 @@ export default {
     },
   },
   async mounted() {
-    await this.$store.dispatch('phone/getAgentMetrics');
+    await this.$store.watch(
+      () => this.connected,
+      () => this.$store.dispatch('phone/getAgentMetrics'),
+    );
+    this.loading = false;
   },
 };
 </script>
