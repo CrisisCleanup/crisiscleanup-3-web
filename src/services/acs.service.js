c4bb117749e3bf4534cb20f09c326fd72b890ce7
Braden Mars <bradenmars@bradenmars.me>
2020-04-27 23:21:50 -0500

    feat(phone): update staging/prod ccp envs

--- src/services/acs.service.js
+++ src/services/acs.service.js
@@ -92,8 +92,9 @@ export const initConnect = ({
 };
 
 export const setPopup = ({ open } = { open: true }) => {
-  const loginUrl = `https://ccuhotlinedev.awsapps.com/auth?client_id=06919f4fd8ed324e&redirect_uri=${global.encodeURIComponent(
-    'https://ccuhotlinedev.awsapps.com/connect/auth/code',
+  const [loginRoot] = process.env.VUE_APP_CCP_URL.split('/connect/');
+  const loginUrl = `${loginRoot}/auth?client_id=06919f4fd8ed324e&redirect_uri=${global.encodeURIComponent(
+    `${loginRoot}/connect/auth/code`,
   )}`;
   if (!open) {
     // clear state (kept in localStorage)
