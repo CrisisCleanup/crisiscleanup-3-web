a82c0e572a850819760e2217f932db5f42166161
Braden Mars <bradenmars@bradenmars.me>
2020-05-23 23:58:17 -0500

    test(ui): add tests for validate/dismiss actions

--- src/store/__tests__/ui.tests.js
+++ src/store/__tests__/ui.tests.js
@@ -1,4 +1,5 @@
 import getters from '../modules/ui/getters';
+import actions from '../modules/ui/actions.js';
 
 describe('ui store', () => {
   it('Detects Internet Explorer with IE User Agent', () => {
@@ -21,5 +22,52 @@ describe('ui store', () => {
       },
     };
     expect(getters.isBrowserIE(state)).toBeFalsy();
+  });
+
+  it('validateBrowser shows banner in IE', async () => {
+    const commit = jest.fn();
+    const state = {};
+    const getters = {
+      isBrowserIE: jest.fn(() => true),
+    };
+    await actions.validateBrowser({ commit, state, getters });
+    expect(commit.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "SET_BANNER",
+          Object {
+            "enabled": true,
+            "text": "~~This website does not support Internet Explorer. We recommend using a modern browser such as Firefox, Chrome, Edge, etc.",
+            "type": "error",
+          },
+        ],
+      ]
+    `);
+  });
+
+  it('validateBrowser does not show banner outside IE', async () => {
+    const commit = jest.fn();
+    const state = {};
+    const getters = {
+      isBrowserIE: jest.fn(() => false),
+    };
+    await actions.validateBrowser({ commit, state, getters });
+    expect(commit.mocks.calls.length).toBe(0);
+  });
+
+  it('dismissBrowser closes banner', async () => {
+    const commit = jest.fn();
+    const state = { siteBanner: { enabled: true } };
+    await actions.dismissBanner({ commit, state });
+    expect(commit.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "SET_BANNER",
+          Object {
+            "enabled": false,
+          },
+        ],
+      ]
+    `);
   });
 });
