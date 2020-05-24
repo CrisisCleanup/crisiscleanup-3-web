37a063a7f968760195081faab8ef43f607b0549c
Braden Mars <bradenmars@bradenmars.me>
2020-05-23 22:02:36 -0500

    test(ui): add tests for IE detection

--- src/store/__tests__/ui.tests.js
+++ src/store/__tests__/ui.tests.js
@@ -1,2 +1,25 @@
-import { actions } from '../modules/ui/ui.js'
+import getters from '../modules/ui/getters';
 
+describe('ui store', () => {
+  it('Detects Internet Explorer with IE User Agent', () => {
+    const ieUserAgent =
+      'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)';
+    const state = {
+      browser: {
+        userAgent: ieUserAgent,
+      },
+    };
+    expect(getters.isBrowserIE(state)).toBeTruthy();
+  });
+
+  it('Does not detect Internet Explorer with Chrome User Agent', () => {
+    const chromeUserAgent =
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36';
+    const state = {
+      browser: {
+        userAgent: chromeUserAgent,
+      },
+    };
+    expect(getters.isBrowserIE(state)).toBeFalsy();
+  });
+});
