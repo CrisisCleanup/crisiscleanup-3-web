d424e35a07ba33e8d3c483917b28663412dde031
Braden Mars <bradenmars@bradenmars.me>
2020-07-18 02:57:12 -0500

    feat(phone): add Contact model and types

---
+++ src/models/phone/Contact.js
@@ -0,0 +1,44 @@
+// @flow
+
+/**
+ * Phone Contact model.
+ */
+
+import { Model } from '@vuex-orm/core';
+import type { ContactType } from '@/models/phone/types';
+
+/**
+ * Enum of possible contact states.
+ * @type {{QUEUED: string, ROUTED: string}|*}
+ */
+export const ContactStates = Object.freeze({
+  QUEUED: 'queued', // Currently waiting in the primary queue.
+  ROUTED: 'routed', // Currently has a target agent. (*Does NOT imply an active connection!)
+});
+
+/**
+ * Enum of possible contact actions.
+ * Primarily used for metric purpose.
+ * @type {{CONNECTING: string, ENTER: string, ENDED: string, ERROR: string, CONNECTED: string}|*}
+ */
+export const ContactActions = Object.freeze({
+  ENTER: 'enter_ivr', // Contact has entered primary queue.
+  CONNECTING: 'connecting', // Contact has been accepted by an agent.
+  CONNECTED: 'connected', // Contact has an active connection with at least 1 agent.
+  ENDED: 'ended', // Contact has been terminated.
+  ERROR: 'error', // An exception occurred at some point.
+});
+
+export default class Contact extends Model {
+  static entity = 'phone/contact';
+
+  static primaryKey = 'contactId';
+
+  static fields() {
+    return ({
+      contactId: this.string(),
+      state: this.string(),
+      action: this.string(),
+    }: ContactType);
+  }
+}
