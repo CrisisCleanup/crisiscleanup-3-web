3db42d50415d4e9cc455e1399f275b816ef4fc76
Braden Mars <bradenmars@bradenmars.me>
2020-07-19 01:47:03 -0500

    feat(service): update connect service types

--- src/services/types.js
+++ src/services/types.js
@@ -10,7 +10,26 @@ import '@crisiscleanup/amazon-connect-streams';
 type ACSEventTopic = $Values<typeof ACS.EventTopics>;
 type ACSAgentEvent = $Values<typeof ACS.AgentEvents>;
 type ACSContactEvent = $Values<typeof ACS.ContactEvents>;
+type ACSCoreEvent = $Values<typeof ACS.CoreEvents>;
 
-type ACSCallback = connect.AgentCallback | connect.ContactCallback;
+type ACSCallback = connect.AgentCallback | connect.ContactCallback | Function;
 
-export type { ACSEventTopic, ACSAgentEvent, ACSContactEvent, ACSCallback };
+type ACSConfig = {|
+  ccpUrl: string,
+  region: string,
+  softphone: {|
+    allowFramedSoftphone: boolean,
+  |},
+  loginPopup: boolean,
+  loginPopupAutoClose: boolean,
+  loginUrl?: string,
+|};
+
+export type {
+  ACSEventTopic,
+  ACSAgentEvent,
+  ACSContactEvent,
+  ACSCoreEvent,
+  ACSCallback,
+  ACSConfig,
+};
