ae9ecf91f89489918741b56b098a48b5ac2d735d
Braden Mars <bradenmars@bradenmars.me>
2020-07-18 22:12:48 -0500

    fix(service): connect service types, unskip test

--- src/services/__tests__/connect.service.test.js
+++ src/services/__tests__/connect.service.test.js
@@ -4,8 +4,10 @@
 
 import * as ACS from '../connect.service.js';
 
+jest.mock('@crisiscleanup/amazon-connect-streams');
+
 describe('connect service tests', () => {
-  it.skip('should bind events', () => {
+  it('should bind events', () => {
     const cbMock = jest.fn();
     ACS.bindEvents(ACS.EventTopics.AGENT, {
       [ACS.AgentEvents.ON_ROUTABLE]: cbMock,
