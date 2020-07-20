e3b5c0bdf04bddde3b1092e5c6f71937c8b060a0
Tobi Abiodun <abiodun.tobi@gmail.com>
2020-07-20 18:37:26 -0400

    More changes

---
+++ src/components/CompletedTransferModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <modal
+    :title="$t('termsConditionsModal.terms_conditions_title')"
+    modal-classes="max-w-lg h-64"
+  >
+    <div
+      class="p-3"
+      v-html="$t('termsConditionsModal.accept_terms_conditions')"
+    ></div>
+    <div slot="footer" class="flex items-center justify-center py-2 border-t">
+      <base-button
+        data-cy="termsmodal.acceptBtn"
+        :text="$t('actions.accept')"
+        :alt="$t('actions.accept')"
+        variant="solid"
+        class="px-6 p-3"
+        :action="
+          () => {
+            $emit('acceptedTerms');
+          }
+        "
+      >
+        {{ $t('actions.accept') }}
+      </base-button>
+    </div>
+  </modal>
+</template>
+
+<script>
+export default {
+  name: 'TermsandConditionsModal',
+};
+</script>
+
+<style scoped></style>
