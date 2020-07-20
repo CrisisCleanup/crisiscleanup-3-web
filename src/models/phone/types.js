38f283b6194a8a3b75d7a52644770ecb6e14f028
Braden Mars <bradenmars@bradenmars.me>
2020-07-19 20:46:37 -0500

    feat(contact): add ContactAttributes enum

--- src/models/phone/types.js
+++ src/models/phone/types.js
@@ -6,13 +6,18 @@
 
 import { AgentStates, RouteStates } from '@/models/phone/AgentClient';
 import { ConnectionStates } from '@/models/phone/Connection';
-import { ContactStates, ContactActions } from '@/models/phone/Contact';
+import {
+  ContactStates,
+  ContactActions,
+  ContactAttributes,
+} from '@/models/phone/Contact';
 
 type AgentState = $Values<typeof AgentStates>;
 type RouteState = $Values<typeof RouteStates>;
 type ConnectionState = $Values<typeof ConnectionStates>;
 type ContactState = $Values<typeof ContactStates>;
 type ContactAction = $Values<typeof ContactActions>;
+type ContactAttribute = $Values<typeof ContactAttributes>;
 
 type ConnectionType = {|
   connectionId: string,
@@ -45,6 +50,7 @@ export type {
   ConnectionState,
   ContactState,
   ContactAction,
+  ContactAttribute,
   AgentClientType,
   ContactType,
   ConnectionType,
