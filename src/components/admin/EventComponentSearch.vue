58ee456f060bcd09c08cf685bc3d0cf95f81475b
Braden Mars <bradenmars@bradenmars.me>
2020-07-25 06:23:54 -0500

    feat(events): add EventComponentSearch component

---
+++ src/components/admin/EventComponentSearch.vue
@@ -0,0 +1,68 @@
+<template>
+  <autocomplete
+    v-bind="$attrs"
+    :icon="icons.search"
+    :suggestions="eventSuggestions"
+    size="large"
+    display-property="key"
+    :loading="loading"
+    :clear-on-selected="false"
+    @search="debouncedCompletions"
+    @selected="fetchSelected"
+  />
+</template>
+
+<script>
+// @flow
+import Autocomplete from '@/components/Autocomplete.vue';
+import EventComponent, { EventComponentTypes } from '@/models/EventComponent';
+import type {
+  EventComponentType,
+  EventComponentTypeT,
+  SuggestedComponentItem,
+} from '@/models/EventComponent';
+import { IconsMixin } from '@/mixins';
+import VueTypes from 'vue-types';
+import _ from 'lodash';
+
+export default {
+  name: 'EventComponentSearch',
+  components: { Autocomplete },
+  mixins: [IconsMixin],
+  props: {
+    type: VueTypes.oneOf<EventComponentTypeT>(
+      Object.values(EventComponentTypes),
+    ),
+  },
+  data() {
+    return ({
+      debouncedCompletions: null,
+      eventSuggestions: [],
+      loading: false,
+      selectedComponent: null,
+    }: {
+      debouncedCompletions: Function | null,
+      eventSuggestions: SuggestedComponentItem[],
+      loading: boolean,
+      selectedComponent: null | EventComponentType,
+    });
+  },
+  created() {
+    this.debouncedCompletions = _.debounce(this.fetchCompletions, 250);
+  },
+  methods: {
+    async fetchCompletions(key: string): void {
+      this.loading = true;
+      this.eventSuggestions = await EventComponent.getKeyCompletions({
+        partial: key,
+        type: this.type,
+      });
+      this.loading = false;
+    },
+    async fetchSelected({ id }: { id: number }): void {
+      this.selectedComponent = await EventComponent.fetchOrFindId(id);
+      this.$emit('update:selected-component', this.selectedComponent);
+    },
+  },
+};
+</script>
