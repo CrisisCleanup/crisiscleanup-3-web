278af06f15acecae1e56570adbe7e25322756796
Braden Mars <bradenmars@bradenmars.me>
2020-07-28 05:05:06 -0500

    feat(language): add static types

--- src/models/Language.js
+++ src/models/Language.js
@@ -1,13 +1,24 @@
+// @flow
+/**
+ * Language Model
+ */
+
 import { Model } from '@vuex-orm/core';
+
+export type LanguageType = {|
+  id: number,
+  subtag: string,
+  name_t: string,
+|};
 
 export default class Language extends Model {
   static entity = 'languages';
 
   static fields() {
-    return {
+    return ({
       id: this.attr(),
       subtag: this.attr(null),
       name_t: this.attr(null),
-    };
+    }: LanguageType);
   }
 }
