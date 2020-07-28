fef93a220eedf751ec651a295537f88143f561a1
Braden Mars <bradenmars@bradenmars.me>
2020-07-28 06:05:38 -0500

    feat(language): add method for translating text

--- src/models/Language.js
+++ src/models/Language.js
@@ -11,6 +11,13 @@ export type LanguageType = {|
   name_t: string,
 |};
 
+type LanguageTranslationResponse = {|
+  text: string,
+  translated_text: string,
+  source_subtag: string,
+  target_subtag: string,
+|};
+
 export default class Language extends Model {
   static entity = 'languages';
 
@@ -20,5 +27,40 @@ export default class Language extends Model {
       subtag: this.attr(null),
       name_t: this.attr(null),
     }: LanguageType);
+  }
+
+  static fetchById(id: number) {
+    return this.api().get(`/languages/${id}`);
+  }
+
+  static async fetchOrFindId(id: number) {
+    const exists = await this.query().whereId(id).exists();
+    if (!exists) {
+      await this.fetchById(id);
+    }
+    return this.find(id);
+  }
+
+  /**
+   * Perform real-time translation of text.
+   * Source language will be auto detected.
+   * @param id - Locale id.
+   * @param text - Text to translate.
+   * @returns {Promise<LanguageTranslationResponse>}
+   */
+  static async translateText(id: number, text: string) {
+    const locale = await this.fetchOrFindId(id);
+    const {
+      response: { data },
+    }: {
+      response: { data: LanguageTranslationResponse },
+    } = await this.api().post(
+      `/languages/${locale.subtag}/translate`,
+      {
+        text,
+      },
+      { save: false },
+    );
+    return data;
   }
 }
