112535839d6971675326b235a231b341beb60b05
Braden Mars <bradenmars@bradenmars.me>
2020-07-28 16:47:21 -0500

    feat(events): update EventSearch to perform compound search instead of completions

--- src/components/admin/EventSearch.vue
+++ src/components/admin/EventSearch.vue
@@ -1,58 +1,60 @@
 <template>
-  <autocomplete
+  <base-input
     :icon="icons.search"
-    :suggestions="eventSuggestions"
     size="large"
-    display-property="key"
-    placeholder="Events"
-    :loading="loading"
-    :clear-on-selected="false"
-    @search="debouncedCompletions"
-    @selected="fetchSelected"
+    placeholder="Events Query"
+    @input="(value) => debouncedQuery(value)"
   />
 </template>
 
 <script>
 // @flow
-import Autocomplete from '@/components/Autocomplete.vue';
 import Event from '@/models/Event';
-import type { SuggestedEventItem, EventType } from '@/models/Event';
+import type { EventType, EventSearchResult } from '@/models/Event';
 import { IconsMixin } from '@/mixins';
 import _ from 'lodash';
 
 export default {
   name: 'EventSearch',
-  components: { Autocomplete },
   mixins: [IconsMixin],
   data() {
     return ({
       debouncedCompletions: null,
-      eventSuggestions: [],
+      eventResults: [],
+      eventItems: [],
       loading: false,
       selectedEvent: null,
     }: {
       debouncedCompletions: Function | null,
-      eventSuggestions: SuggestedEventItem[],
+      eventResults: EventSearchResult[],
+      eventItems: EventType[],
       loading: boolean,
       selectedEvent: null | EventType,
     });
   },
   created() {
-    this.debouncedCompletions = _.debounce(this.fetchCompletions, 250);
+    this.debouncedQuery = _.debounce(this.makeQuery, 400);
   },
   methods: {
-    async fetchCompletions(key: string): void {
+    async makeQuery(key: string): void {
       this.loading = true;
-      this.eventSuggestions = await Event.getCompletions(key);
+      this.$emit('update:loading', this.loading);
+      this.eventResults = await Event.search(key, 5);
+      await this.fetchDetails();
       this.loading = false;
+      this.$emit('update:loading', this.loading);
     },
-    async fetchSelected({ id }: { id: number }): void {
-      const exists = await Event.query().whereId(id).exists();
-      if (!exists) {
-        await Event.fetchById(id);
-      }
-      this.selectedEvent = Event.find(id);
-      this.$emit('update:selected-event', this.selectedEvent);
+    async fetchDetails(): void {
+      this.eventItems = [];
+      await _.mapValues(this.eventResults, async ({ id }) => {
+        const exists = await Event.query().whereId(id).exists();
+        if (!exists) {
+          await Event.fetchById(id);
+        }
+        const event = Event.find(id);
+        this.eventItems.push(event);
+      });
+      this.$emit('update:event-items', this.eventItems);
     },
   },
 };
