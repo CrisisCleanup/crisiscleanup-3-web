6f730b65a7dc347155a8860b5cdab6d71f381ee2
Braden Mars <bradenmars@bradenmars.me>
2020-08-06 04:41:50 -0500

    feat(admin): add admin page root

---
+++ src/pages/admin/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <PageLayout :tabs="tabs" />
+</template>
+
+<script>
+import PageLayout from '@/layouts/page/Page.vue';
+import { reactive } from '@vue/composition-api';
+
+export default {
+  name: 'AdminPage',
+  components: { PageLayout },
+  setup() {
+    const tabs = reactive([
+      {
+        key: 'nav.admin_dashboard',
+        title: 'Home',
+      },
+      {
+        key: 'nav.admin_tools',
+        title: 'Events',
+      },
+    ]);
+    return { tabs };
+  },
+};
+</script>
+
+<style scoped></style>
