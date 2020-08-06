93a15d6387dfaecb862ed7e5de044e30024f99be
Braden Mars <bradenmars@bradenmars.me>
2020-08-06 04:40:10 -0500

    feat(page): add base Page layout

---
+++ src/layouts/page/Page.vue
@@ -0,0 +1,30 @@
+<template>
+  <Loader
+    :loading="isLoading"
+    class="p-6 bg-crisiscleanup-light-grey h-full overflow-auto"
+  >
+    <template #content>
+      <template v-if="tabs">
+        <PageTabBar :tabs="tabs" />
+      </template>
+      <slot v-else></slot>
+    </template>
+  </Loader>
+</template>
+
+<script>
+import Loader from '@/components/Loader.vue';
+import VueTypes from 'vue-types';
+import PageTabBar from './PageTabBar.vue';
+
+export default {
+  name: 'PageLayout',
+  components: { Loader, PageTabBar },
+  props: {
+    isLoading: VueTypes.bool.def(false),
+    tabs: PageTabBar.props.tabs.def(null),
+  },
+};
+</script>
+
+<style scoped></style>
