f4d90946ffb1f8aa0fa320ec3d84f70fcfe3d1a0
Braden Mars <bradenmars@bradenmars.me>
2020-08-06 04:39:05 -0500

    feat(css): add cssnext features, add postcss-nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [
+    require('tailwindcss'),
+    /**
+     * postcss-preset-env
+     * @see http://preset-env.cssdb.org/features
+     */
+    require('postcss-preset-env')({
+      stage: 1,
+    }),
+    require('postcss-nested'),
+    require('autoprefixer'),
+  ],
 };
