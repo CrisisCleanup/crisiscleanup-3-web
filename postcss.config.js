57cbb75b4afceaad049c6b0149ecb5c5f17a945e
Braden Mars <bradenmars@bradenmars.me>
2020-08-09 05:07:35 -0500

    feat(css): add postcss-variables/atroot/easings/extend-rule/property-lookup/responsive-type/more
    will-change/will-change-transition/reporter/pxtorem and lost

--- postcss.config.js
+++ postcss.config.js
@@ -5,10 +5,24 @@ module.exports = {
      * postcss-preset-env
      * @see http://preset-env.cssdb.org/features
      */
+    require('postcss-extend-rule'),
+    require('postcss-advanced-variables')({
+      disable: '@import',
+    }),
     require('postcss-preset-env')({
       stage: 1,
     }),
+    require('postcss-atroot'),
+    require('postcss-property-lookup'),
     require('postcss-nested'),
+    require('postcss-will-change-transition'),
+    require('postcss-will-change'),
+    require('postcss-responsive-type'),
+    require('postcss-easings'),
     require('autoprefixer'),
+    require('postcss-pxtorem')({
+      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+    }),
+    require('postcss-reporter'),
   ],
 };
