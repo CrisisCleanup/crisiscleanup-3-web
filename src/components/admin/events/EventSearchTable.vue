fc19c3b7e634873afc68b261a0e7bbfec161a89e
Braden Mars <bradenmars@bradenmars.me>
2020-08-10 02:47:46 -0500

    feat(events): add EventSearchTable refactored component
    refactored to utilize vue composition api and restyled

---
+++ src/components/admin/events/EventSearchTable.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="esearch">
+    <div class="esearch__input">
+      <base-input
+        icon="search"
+        size="large"
+        placeholder="Events Query"
+        @input="(value) => makeQuery(value)"
+      />
+    </div>
+    <div class="esearch__table">
+      <base-text variant="h3">
+        {{ $t('eventBuilder.event_details') }}
+      </base-text>
+      <Table :loading="loading" :columns="tableCols" :data="tableData" />
+    </div>
+  </div>
+</template>
+
+<script>
+// @flow
+
+// import { useSearchEvents } from '@/use';
+import useSearchEvents from '@/use/events/useSearchEvents';
+import { makeTableColumns } from '@/utils/table';
+import { computed } from '@vue/composition-api';
+import Table from '@/components/Table.vue';
+
+export default {
+  name: 'EventSearchTable',
+  components: { Table },
+  setup(props, context) {
+    const { loading, items, makeQuery } = useSearchEvents({ context });
+    const tableCols = computed(() =>
+      makeTableColumns([
+        ['id', '0.2fr'],
+        ['key', '0.7fr'],
+        ['name_t'],
+        ['description'],
+        ['actor_model', '0.5fr'],
+        ['patient_model', '0.5fr'],
+        ['recipient_model', '0.5fr'],
+        ['total_uses', '0.2fr'],
+      ]),
+    );
+    const tableData = computed(() =>
+      items.value.map((e) => e.withTrans<Event>()),
+    );
+    return {
+      loading,
+      items,
+      makeQuery,
+      tableCols,
+      tableData,
+    };
+  },
+};
+</script>
+
+<style scoped lang="postcss">
+.esearch {
+  > div {
+    lost-row: 1/2;
+  }
+}
+</style>
