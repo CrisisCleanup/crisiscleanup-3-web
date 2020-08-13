e768738226ae9b9f0317456cb5f25e0d60350fc3
Braden Mars <bradenmars@bradenmars.me>
2020-08-12 23:59:23 -0500

    feat(models): add baseFields and fetchAll to CCUModel

--- src/models/model.js
+++ src/models/model.js
@@ -4,8 +4,38 @@
  */
 
 import { Model } from '@vuex-orm/core';
+import _ from 'lodash';
+import { QueryResult } from 'aws-sdk/clients/kendra';
+
+export interface BaseFieldsT {
+  id: number;
+  invalidated_at?: Date;
+  created_at: Date;
+  updated_at: Date;
+  created_by: number;
+  updated_by: number;
+}
 
 export default class CCUModel<T> extends Model {
+  static historyFields = {
+    invalidated_at: this.attr('', (value) => Date.parse(value)),
+    created_at: this.attr('', (value) => Date.parse(value)),
+    updated_at: this.attr('', (value) => Date.parse(value)),
+    created_by: this.number(),
+    updated_by: this.number(),
+  };
+
+  static baseFields = (omit: string[] = []): $Shape<BaseFieldsT> =>
+    _.omit(
+      {
+        id: this.number(),
+        ...this.historyFields,
+      },
+      omit,
+    );
+
+  static entity = '';
+
   /**
    * Fetch and store item by id.
    * @param id - item id.
@@ -26,5 +56,26 @@ export default class CCUModel<T> extends Model {
       await this.fetchById(id);
     }
     return this.find(id);
+  }
+
+  /**
+   * Fetch all items from api.
+   * @param params - any additional query params.
+   * @param dataKey - data key to use.
+   * @returns {Promise<Instance<CCUModel extends {new(...args: any[]): infer R} ? R : any>[]>}
+   */
+  static async fetchAll(
+    params?: {
+      [key: string]: string,
+    },
+    dataKey?: string,
+  ): Promise<QueryResult<T>> {
+    const _dataKey = dataKey || 'results';
+    const _params = params || {};
+    await this.api().get(`/${this.entity}`, {
+      dataKey: _dataKey,
+      params: _params,
+    });
+    return this.query().all();
   }
 }
