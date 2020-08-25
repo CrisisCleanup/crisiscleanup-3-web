90cedab146ea2cfbdfdc2170ceb1e165a8501190
Braden Mars <bradenmars@bradenmars.me>
2020-08-25 16:23:34 -0500

    feat(ctrl): add realtime metrics to controller store

--- src/store/modules/phone/types.js
+++ src/store/modules/phone/types.js
@@ -8,12 +8,14 @@ import { AuthStates } from '@/store/modules/phone/streams';
 import {
   ControllerActionTabs,
   ControllerPages,
+  Metrics,
 } from '@/store/modules/phone/controller';
 
 type AuthState = $Values<typeof AuthStates>;
 
 type ControllerPage = $Values<typeof ControllerPages>;
 type ControllerActionTab = $Values<typeof ControllerActionTabs>;
+type PhoneMetric = $Keys<typeof Metrics>;
 
 type CaseType = {|
   id: number,
@@ -25,10 +27,23 @@ type ViewStateT = {|
   actionTab: ControllerActionTab,
 |};
 
+type MetricsStateT = {
+  [$ElementType<PhoneMetric, 0>]: number,
+};
+
+type PhoneMetricUpdate = {|
+  name: PhoneMetric,
+  type: string,
+  value: string,
+|};
+
 export type {
   AuthState,
   CaseType,
   ControllerActionTab,
   ControllerPage,
   ViewStateT,
+  PhoneMetric,
+  MetricsStateT,
+  PhoneMetricUpdate,
 };
