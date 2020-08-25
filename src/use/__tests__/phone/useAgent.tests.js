2e09fb98d45ba8f56591afbd043d6625d9e0325a
Braden Mars <bradenmars@bradenmars.me>
2020-08-25 05:30:13 -0500

    fix(connect): fix issue where existing connect socket connection caused blank screen

--- src/use/__tests__/phone/useAgent.tests.js
+++ src/use/__tests__/phone/useAgent.tests.js
@@ -7,12 +7,23 @@ import VueCompositionApi from '@vue/composition-api';
 import { useGetters } from '@u3u/vue-hooks';
 import AgentClient from '@/models/phone/AgentClient';
 import useAgent from '@/use/phone/useAgent';
+import VuexORM, { Database } from '@vuex-orm/core';
+import Vuex from 'vuex';
 
-jest.mock('@/models/phone/AgentClient');
+// jest.mock('@/models/phone/AgentClient');
 jest.mock('@u3u/vue-hooks');
+
+const database = new Database();
+database.register(AgentClient, {});
 
 const localVue = createLocalVue();
 localVue.use(VueCompositionApi);
+localVue.use(Vuex);
+
+// eslint-disable-next-line no-unused-vars
+const mockStore = new Vuex.Store({
+  plugins: [VuexORM.install(database)],
+});
 
 describe('useAgent', () => {
   it('loads until ready', () => {
@@ -27,22 +38,23 @@ describe('useAgent', () => {
 
   it('retrieves agent when ready', () => {
     const firstMock = jest.fn();
-    AgentClient.query.mockReturnValue({
-      first: firstMock,
-    });
+    AgentClient.query = jest.fn();
+    AgentClient.query.mockReturnValue({ first: firstMock });
     useGetters.mockReturnValue({
       agentClientId: {
         value: null,
       },
     });
+    firstMock.mockReturnValue({ agentId: 'abc' });
 
-    const { agent } = useAgent();
+    const { agent, loading } = useAgent();
     useGetters.mockReturnValue({
       agentClientId: {
         value: 'abc',
       },
     });
-    firstMock.mockReturnValue({ agentId: 'abc' });
+    console.log(agent);
     expect(agent.value.agentId).toBe('abc');
+    expect(loading.value).toBe(false);
   });
 });
