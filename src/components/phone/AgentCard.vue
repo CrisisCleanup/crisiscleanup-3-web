9ae57d0f243cd6083b92502666ad14ac9aa0d31a
Braden Mars <bradenmars@bradenmars.me>
2020-08-25 05:45:54 -0500

    feat(phone): replace agent card state code w/ hooks

--- src/components/phone/AgentCard.vue
+++ src/components/phone/AgentCard.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="agentcard shadow-crisiscleanup-card">
     <div class="card-edit">
-      <ccu-icon
-        @click.native="() => forceEdit()"
-        size="md"
-        :type="icons.edit"
-      />
+      <ccu-icon @click.native="() => forceAgentEdit()" size="md" type="edit" />
     </div>
     <div class="profile">
       <div class="profile--img">
@@ -16,251 +12,177 @@
         />
       </div>
       <div class="profile--details">
-        <base-text variant="h2" :weight="700">
+        <base-text :weight="700" variant="h2">
           {{ currentUser.full_name }}
         </base-text>
-        <base-text variant="h1" :weight="400">
+        <base-text :weight="400" variant="h1">
           {{ currentUser.mobile }}
         </base-text>
         <base-button
-          variant="text"
+          :action="() => showMoreState.toggle()"
           class="more-info"
-          :action="() => (toggleOpen = !toggleOpen)"
+          variant="text"
         >
-          {{ toggleOpen ? lang.toggle.less : lang.toggle.more }}
+          {{ showMoreState.state.value ? 'less' : 'more' }}
         </base-button>
       </div>
     </div>
     <div class="info-card py-3">
-      <transition name="fade" mode="out-in">
-        <more-info v-if="toggleOpen" />
+      <transition mode="out-in" name="fade">
+        <more-info v-if="showMoreState.state.value" />
       </transition>
     </div>
     <div class="action">
       <div class="inline-flex status">
-        <span :class="`dot ${currentState.state}`" />
-        <base-text :weight="600" variant="body">{{
-          currentState.statusText
-        }}</base-text>
+        <span :class="`dot ${agent.friendlyState}`" />
+        <base-text :weight="600" variant="body"
+          >{{ agentState.statusText | startCase }}
+        </base-text>
       </div>
       <base-button
-        :disabled="!currentState.enabled"
-        :action="toggleAvailable"
-        variant="solid"
+        :action="() => handleAgentState()"
+        :disabled="!agentState.enabled"
         size="large"
+        variant="solid"
       >
-        {{ currentState.text }}
+        {{ $t(agentState.text) }}
       </base-button>
     </div>
     <trainings-modal
-      :visible="isShowingTrainingModal"
-      @onClose="isShowingTrainingModal = false"
+      :visible="trainingState.state.value"
+      @onClose="() => trainingState.toggle(false)"
       @onComplete="onTrainingComplete"
     ></trainings-modal>
     <agent-edit-card
-      :active="editCardActive"
-      :request="agentNeeded"
+      :active="editCardState.state.value"
+      :request="validations"
       @user-updated="() => validateAgent()"
     />
   </div>
 </template>
 
 <script>
-import { IconsMixin, UserMixin, LangMixin, TrainingMixin } from '@/mixins';
-import { mapActions, mapGetters } from 'vuex';
-import { STATES as CCState, EVENTS as CCEvent } from '@/services/acs.service';
 import ContactMoreInfo from '@/components/phone/ContactMoreInfo.vue';
 import TrainingsModal from '@/components/phone/TrainingsModal.vue';
-import { EventBus } from '@/event-bus';
 import CallerIDEditCard from '@/components/phone/CallerIDEditCard.vue';
-import { ERRORS as AgentErrors } from '@/models/Agent';
+import useAgentState from '@/use/phone/useAgentState';
+import useAgent from '@/use/phone/useAgent';
+import { reactive, onMounted } from '@vue/composition-api';
+import useToggle from '@/use/useToggle';
+import useUser from '@/use/user/useUser';
+import useTraining from '@/use/user/useTraining';
+import Agent, { ERRORS as AgentErrors } from '@/models/Agent';
+import ControllerStore, {
+  ControllerPages,
+} from '@/store/modules/phone/controller';
+import { useStore } from '@u3u/vue-hooks';
+import { getModule } from 'vuex-module-decorators';
+import { unwrap } from '@/utils/wrap';
+
+const useValidations = ({ currentUser }) => {
+  const editCardState = useToggle();
+
+  const state = reactive({
+    phone: false,
+    lang: false,
+  });
+
+  const validateAgent = async () => {
+    editCardState.toggle(false);
+    state.phone = false;
+    state.lang = false;
+    try {
+      await Agent.api().fetch({
+        user: {
+          id: unwrap(currentUser).id,
+          email: unwrap(currentUser).email,
+        },
+      });
+    } catch (errs) {
+      const {
+        MOBILE_INVALID,
+        MOBILE_NOT_FOUND,
+        LANGUAGE_NOT_FOUND,
+        LANGUAGE_NOT_SUPPORTED,
+      } = AgentErrors;
+      const invalidPhone = (el) =>
+        [MOBILE_INVALID, MOBILE_NOT_FOUND].includes(el);
+      const invalidLang = (el) =>
+        [LANGUAGE_NOT_FOUND, LANGUAGE_NOT_SUPPORTED].includes(el);
+      editCardState.toggle(true);
+      if (errs.some(invalidPhone)) {
+        state.phone = true;
+      }
+      if (errs.some(invalidLang)) {
+        state.lang = true;
+      }
+    }
+  };
+
+  const forceAgentEdit = () => {
+    state.phone = true;
+    state.lang = true;
+    editCardState.toggle(true);
+  };
+
+  onMounted(async () => validateAgent());
+
+  return {
+    editCardState,
+    validateAgent,
+    validations: state,
+    forceAgentEdit,
+  };
+};
 
 export default {
   name: 'AgentCard',
-  mixins: [IconsMixin, LangMixin, UserMixin, TrainingMixin],
   components: {
     moreInfo: ContactMoreInfo,
     'agent-edit-card': CallerIDEditCard,
     TrainingsModal,
   },
-  data() {
-    return {
-      toggleOpen: true,
-      isShowingTrainingModal: false,
-      editCardActive: false,
-      showConnectLogin: false,
-      agentNeeded: {
-        phone: false,
-        lang: false,
-      },
-      agent: {},
-    };
-  },
-  async mounted() {
-    await this.loadTrainingData();
-  },
-  methods: {
-    ...mapActions('phone', ['setAgentState', 'getAgent']),
-    forceEdit() {
-      this.agentNeeded.phone = true;
-      this.agentNeeded.lang = true;
-      this.editCardActive = true;
-    },
-    async authenticate() {
-      if (!this.connectReady) {
-        await this.validateAgent();
-        if (!this.editCardActive) {
-          return EventBus.$emit('acs:requestAgent');
-        }
-        return this.editCardActive;
-      }
-      return this.editCardActive;
-    },
-    async validateAgent() {
-      this.editCardActive = false;
-      this.agentNeeded.phone = false;
-      this.agentNeeded.lang = false;
-      try {
-        this.agent = await this.getAgent();
-      } catch (errs) {
-        const {
-          MOBILE_INVALID,
-          MOBILE_NOT_FOUND,
-          LANGUAGE_NOT_FOUND,
-          LANGUAGE_NOT_SUPPORTED,
-        } = AgentErrors;
-        const invalidPhone = (el) =>
-          [MOBILE_INVALID, MOBILE_NOT_FOUND].includes(el);
-        const invalidLang = (el) =>
-          [LANGUAGE_NOT_FOUND, LANGUAGE_NOT_SUPPORTED].includes(el);
-        this.editCardActive = true;
-        if (errs.some(invalidPhone)) {
-          this.agentNeeded.phone = true;
-        }
-        if (errs.some(invalidLang)) {
-          this.agentNeeded.lang = true;
-        }
-      }
-    },
-    async toggleAvailable() {
-      if (!this.allTrainingCompleted) {
-        this.isShowingTrainingModal = true;
-        return this.isShowingTrainingModal;
-      }
+  setup(props, context) {
+    const showMoreState = useToggle({ state: true });
+    const store = useStore();
+    const ctrlStore = getModule(ControllerStore, store.value);
+    const trainingState = useToggle();
 
-      if (!this.connectReady) {
-        await this.validateAgent();
-        if (!this.editCardActive) {
-          this.authenticate();
-        }
-        return this.editCardActive;
-      }
+    const { allTrainingCompleted, onTrainingComplete } = useTraining({
+      tests: [2, 3],
+    });
 
-      if (this.currentPage === 'controller') {
-        return this.$toasted.error(
-          this.$t('~~You must complete the open call to take another!'),
+    const { agent, agentState, toggleAgentState } = useAgentState({
+      ...useAgent(),
+      context,
+      isTrained: allTrainingCompleted,
+    });
+
+    const handleAgentState = () => {
+      if (ctrlStore.view === ControllerPages.CONTROLLER) {
+        context.root.$toasted.error(
+          context.root.$t('~~You must complete the open call to take another!'),
         );
+        return;
       }
+      toggleAgentState();
+    };
 
-      if (this.agentAvailable) {
-        return this.setAgentState(CCState.OFFLINE);
-      }
-      return this.setAgentState(CCState.ROUTABLE);
-    },
-    async onTrainingComplete() {
-      await this.loadTrainingData();
-      this.isShowingTrainingModal = false;
-    },
-  },
-  computed: {
-    ...mapGetters('phone', [
-      'agentState',
-      'agentAvailable',
-      'connectReady',
-      'popupOpen',
-      'currentPage',
-    ]),
-    lang() {
-      return this.getLang({
-        start: '~~Start Taking Calls',
-        ready: '~~Ready for Next Call',
-        stop: '~~Stop Taking Calls',
-        train: '~~Start Training',
-        authenticate: '~~Authenticate',
-        toggle: {
-          more: '~~More Info',
-          less: '~~Less Info',
-        },
-        status: {
-          offline: '~~Offline',
-          available: '~~Available',
-          oncall: '~~On Call',
-          paused: '~~Paused',
-        },
-      });
-    },
-    currentState() {
-      if (!this.allTrainingCompleted) {
-        return {
-          enabled: true,
-          text: this.lang.train,
-          statusText: this.lang.status.offline,
-        };
-      }
-      if (!this.connectReady) {
-        return {
-          enabled: true,
-          text: this.lang.authenticate,
-          statusText: this.lang.status.offline,
-        };
-      }
-      const state = {
-        enabled: true,
-        key: 'start',
-        state: 'offline',
-      };
-      switch (this.agentState) {
-        case CCState.POLLING:
-        case CCState.ROUTABLE:
-        case CCState.AGENT_CALLING:
-          state.key = 'stop';
-          state.state = 'available';
-          EventBus.$emit(CCEvent.AVAILABLE);
-          break;
-        case CCState.PENDING_CALL:
-        case CCState.INCOMING:
-        case CCState.CONNECTING:
-        case CCState.AGENT_PENDING:
-          state.key = 'ready';
-          state.state = 'oncall';
-          state.enabled = false;
-          break;
-        case CCState.ON_CALL:
-          state.key = 'ready';
-          state.state = 'oncall';
-          state.enabled = false;
-          EventBus.$emit(CCEvent.ON_CALL);
-          break;
-        case CCState.PAUSED:
-          state.key = 'ready';
-          state.state = 'paused';
-          EventBus.$emit(CCEvent.PAUSED);
-          break;
-        case CCState.OFFLINE:
-        case CCState.DISCONNECTED:
-          break;
-        default:
-          break;
-      }
-      state.text = this.lang[state.key];
-      state.statusText = this.lang.status[state.state];
-      return state;
-    },
+    return {
+      trainingState,
+      showMoreState,
+      agent,
+      agentState,
+      handleAgentState,
+      onTrainingComplete,
+      ...useUser(),
+      ...useValidations({ currentUser: useUser().currentUser }),
+    };
   },
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .agentcard {
   @apply bg-white p-3 px-6;
   display: flex;
@@ -271,11 +193,13 @@ export default {
     display: flex;
     justify-content: flex-end;
   }
+
   .profile {
     display: flex;
     justify-content: space-evenly;
     align-items: center;
     flex-grow: 1;
+
     &--img {
       @apply shadow-md;
       max-width: 30%;
@@ -283,18 +207,22 @@ export default {
       border-radius: 50%;
       position: relative;
     }
+
     &--details .more-info {
       @apply text-primary-dark;
       text-decoration: underline;
+
       &:hover {
         @apply text-primary-light;
         background-color: transparent;
       }
+
       .card {
         transition: 300ms ease;
       }
     }
   }
+
   .action {
     display: flex;
     flex-direction: column;
@@ -314,12 +242,14 @@ export default {
       border-radius: 50%;
       display: inline-block;
       @apply mr-2 bg-crisiscleanup-red-500;
-      &.available {
+      &.online {
         @apply bg-crisiscleanup-green-300;
       }
-      &.oncall {
+
+      &.talking {
         @apply bg-crisiscleanup-dark-blue;
       }
+
       &.paused {
         @apply bg-crisiscleanup-yellow-500;
       }
