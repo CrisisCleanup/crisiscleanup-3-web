73da617d465a4c200d87687c519822802144b3cf
Braden Mars <bradenmars@bradenmars.me>
2020-08-28 04:56:36 -0500

    feat(phone): update phone root page with composables

--- src/pages/phone/Index.vue
+++ src/pages/phone/Index.vue
@@ -1,208 +1,100 @@
 <template>
-  <div class="w-full h-full">
-    <component :is="currentComponent" />
-    <IncomingPopup v-if="callIncoming && casesResolved" />
-  </div>
+  <Loader class="w-full h-full" :loading="loading">
+    <template #content>
+      <component v-if="!loading" :is="pageComponent" />
+      <IncomingPopup v-show="callPending" :cases="cases" :agent="agent" />
+    </template>
+  </Loader>
 </template>
 
 <script>
 // @flow
-import { mapGetters, mapActions } from 'vuex';
 import { EventBus } from '@/event-bus';
-import { EVENTS as CCEvent, STATES as CCState } from '@/services/acs.service';
-import Worksite from '@/models/Worksite';
-import PhoneOutbound from '@/models/PhoneOutbound';
-import Pda from '@/models/Pda';
-import { AgentMixin } from '@/mixins';
+import { EVENTS as CCEvent } from '@/services/acs.service';
 import IncomingPopup from '@/components/phone/Popup.vue';
-import { mixin as VueTimer } from 'vue-timers';
-import { isNil } from 'lodash';
-import { getModule } from 'vuex-module-decorators';
-import ControllerStore, {
-  ControllerPages,
-} from '@/store/modules/phone/controller';
-import Controller from './Controller.vue';
+import { ControllerPages } from '@/store/modules/phone/controller';
+import useAgent from '@/use/phone/useAgent';
+import { computed, onMounted, watch } from '@vue/composition-api';
+import Loader from '@/components/Loader.vue';
+import { useIntervalFn } from '@vueuse/core';
+import useContact from '@/use/phone/useContact';
+import useController from '@/use/phone/useController';
 import Dashboard from './Dashboard.vue';
+import Controller from './Controller.vue';
 
 export default {
   name: 'Phone',
-  mixins: [AgentMixin, VueTimer],
-  components: { IncomingPopup },
-  // timers: {
-  //   getNextCallback: { time: 60000, autostart: true, repeat: true },
-  // },
-  computed: {
-    ...mapGetters('phone', [
-      'connectReady',
-      'agentState',
-      'callIncoming',
-      'currentPage',
-      'currentContactId',
-      'casesResolved',
-    ]),
-    pages() {
-      return {
+  components: { IncomingPopup, Loader },
+  setup(props, context) {
+    const { agent, loading } = useAgent();
+    const controller = useController();
+    const {
+      callPending,
+      callConnected,
+      currentContact,
+      callState,
+    } = useContact({
+      agent,
+    });
+    const cases = computed(() => [
+      ...callState.worksites.value,
+      ...callState.pdas.value,
+    ]);
+
+    const { start } = useIntervalFn(() => {
+      agent.value.heartbeat().then(() => {
+        context.root.$log.debug('triggering heartbeat');
+      });
+    }, 30000);
+
+    watch(
+      () => loading.value,
+      () => {
+        if (!loading.value) {
+          agent.value.heartbeat().then(() => start());
+        }
+      },
+    );
+
+    const updatePage = async () => {
+      if (callConnected.value) {
+        await controller.actions.setView({
+          page: ControllerPages.CONTROLLER,
+        });
+      }
+      if (!callConnected.value && !currentContact.value) {
+        await controller.actions.setView({ page: ControllerPages.DASHBOARD });
+      }
+    };
+
+    watch(
+      () => callConnected.value,
+      async () => updatePage(),
+    );
+
+    onMounted(() => {
+      EventBus.$emit(CCEvent.INIT);
+      updatePage();
+    });
+
+    const pageComponent = computed(() => {
+      const pages = {
         [ControllerPages.DASHBOARD]: Dashboard,
         [ControllerPages.CONTROLLER]: Controller,
       };
-    },
-    currentComponent() {
-      const controller = getModule(ControllerStore, this.$store);
-      return this.pages[controller.view.page];
-    },
-  },
-  methods: {
-    ...mapActions('phone', [
-      'setPopup',
-      'setCurrentCase',
-      'setOutboundId',
-      'setContactState',
-      'setResolved',
-    ]),
-    async getNextCallback() {
-      this.$log.debug('checking next callback...');
-      if (
-        !this.agentOnCall &&
-        !this.currentContactId &&
-        this.connectReady &&
-        this.agentState === CCState.ROUTABLE
-      ) {
-        try {
-          let aId = this.agentId;
-          if (!aId) {
-            aId = await this.$store.dispatch('phone/getAgent');
-          }
-          const callback = await PhoneOutbound.api().getNextOutbound({
-            agentId: aId,
-          });
-          this.$log.debug('serving up next callback...', callback);
-          await PhoneOutbound.api().callOutbound(callback.id);
-        } catch (e) {
-          this.$log.debug('no callbacks available!', e);
-        }
-      }
-    },
-    async resolveCases({ outboundIds, pdas, worksites }) {
-      this.$log.debug('resolving caller cases...', pdas, worksites);
-      const incompletePdas = pdas.filter((p) => isNil(p.worksite));
-      await this.addCases({
-        worksites,
-        pdas: incompletePdas,
-      });
-      const currentCase = {
-        id: null,
-        type: null,
-      };
-      const worksiteCases = await this.fetchCasesByType(Worksite, worksites);
-      const pdasCases = await this.fetchCasesByType(Pda, pdas);
-      const outboundPhones = await this.fetchCasesByType(
-        PhoneOutbound,
-        outboundIds,
-      );
-      const freshPdas = [];
-      this.$log.debug('caller PDAs:', pdasCases);
-      this.$log.debug('caller Worksites:', worksiteCases);
-      this.$log.debug('caller outbound Ids:', outboundPhones);
+      return pages[controller.state.view.value.page];
+    });
 
-      await Promise.all(
-        pdasCases.map(async (p) => {
-          if (p.worksite) {
-            this.$log.debug(
-              `fetching worksite [${p.worksite}] from pda: ${p.id}`,
-            );
-            if (!Worksite.exists(p.worksite)) {
-              await Worksite.api().fetch(p.worksite);
-            }
-            const wksite = Worksite.find(p.worksite);
-            worksiteCases.push(wksite);
-          } else {
-            freshPdas.push(p);
-          }
-        }),
-      );
-
-      this.cases = {
-        worksites: worksiteCases,
-        pdas: pdasCases,
-      };
-      this.$log.debug('resolved cases:', this.cases);
-
-      if (freshPdas.length >= 1) {
-        this.$log.debug(
-          'this appears to be a non-verified PDA call',
-          freshPdas,
-        );
-        currentCase.type = 'pda';
-        // set the first one to active
-        currentCase.id = freshPdas.shift().id;
-        return this.setCurrentCase(currentCase);
-      }
-      if (worksiteCases.length >= 1) {
-        this.$log.debug(
-          'this call appears to refer to an existing worksite(s)',
-          worksiteCases,
-        );
-        currentCase.type = 'worksite';
-        currentCase.id = worksiteCases.shift().id;
-        return this.setCurrentCase(currentCase);
-      }
-      // no pdas or worksites found
-      // must be a new case
-      this.$log.debug('this call appears to refer to a new worksite');
-      currentCase.type = 'new';
-      currentCase.id = -1;
-      return this.setCurrentCase(currentCase);
-    },
+    return {
+      pageComponent,
+      agent,
+      loading,
+      callPending,
+      callConnected,
+      currentContact,
+      cases,
+    };
   },
-  created() {
-    EventBus.$emit(CCEvent.INIT);
-  },
-  // created() {
-  //   // EventBus.$emit(CCEvent.INIT);
-  //   PhoneResource.api().get('/phone_resources', {
-  //     dataKey: 'results',
-  //   });
-  //   EventBus.$on(CCEvent.INBOUND, async (attrs) => {
-  //     if (!this.$store.getters['phone/casesResolved']) {
-  //       this.$log.info('inbound event recv, resolving cases');
-  //       await this.setResolved(true);
-  //       await this.resolveCases(attrs);
-  //     }
-  //   });
-  //   EventBus.$on(CCEvent.ON_CALL, () => {
-  //     this.setCurrentPage('controller');
-  //   });
-  //   EventBus.$on(CCEvent.OFF_CALL, () => {
-  //     this.$store.dispatch('phone/resetState');
-  //     this.setCurrentPage('dashboard');
-  //   });
-  //   if (!this.connectReady) {
-  //     this.$store.dispatch('phone/syncContact').then(() => {
-  //       this.$store.dispatch('phone/syncExternalContact');
-  //     });
-  //     this.unsub = this.$store.subscribe((mutation) => {
-  //       if (mutation.type === 'phone/setAgentState') {
-  //         this.$toasted.success('Success!');
-  //         if (this.callIncoming || this.agentOnCall) {
-  //           this.setCurrentPage('controller');
-  //         } else {
-  //           this.setCurrentPage('dashboard');
-  //         }
-  //         this.setPopup(false);
-  //         this.unsub();
-  //       }
-  //     });
-  //   } else if (this.callIncoming || this.agentOnCall) {
-  //     this.setCurrentPage('controller');
-  //   } else {
-  //     this.setCurrentPage('dashboard');
-  //   }
-  // },
-  // beforeDestroy() {
-  //   if (this.unsub) {
-  //     this.unsub();
-  //   }
-  // },
 };
 </script>
 
