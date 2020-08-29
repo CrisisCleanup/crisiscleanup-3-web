034a293336af931a9d1e9e2d9dd42c24accb1f16
Braden Mars <bradenmars@bradenmars.me>
2020-08-29 05:53:06 -0500

    feat(pkg): upgrade storybook to v6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,17 @@
 const webpackConfig = require('@vue/cli-service/webpack.config');
+const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.(js|mdx)'],
+  stories: ['../src/**/*.stories.@(js|mdx)'],
   addons: [
     '@storybook/addon-storysource',
-    '@storybook/addon-actions/register',
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-a11y/register',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
     '@storybook/addon-links',
-    '@storybook/addon-backgrounds/register',
+    '@storybook/addon-backgrounds',
     '@storybook/addon-docs',
+    '@storybook/addon-controls',
   ],
   webpackFinal: (config) => {
     return {
@@ -34,6 +36,11 @@ module.exports = {
         alias: {
           ...config.resolve.alias,
           ...webpackConfig.resolve.alias,
+          'core-js/modules': path.resolve(
+            __dirname,
+            '..',
+            'node_modules/core-js/modules',
+          ),
         },
       },
     };
--- babel.config.js
+++ babel.config.js
@@ -30,7 +30,18 @@ module.exports = {
   ],
   env: {
     storybook: {
-      presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+      presets: ['@babel/preset-flow', ['@babel/preset-env']],
+      plugins: [
+        // @compat: decorators needs to come
+        // before class properties
+        [
+          '@babel/plugin-proposal-decorators',
+          {
+            legacy: true,
+          },
+        ],
+        ['@babel/plugin-proposal-class-properties'],
+      ],
     },
     test: {
       plugins: ['require-context-hook'],
