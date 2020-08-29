666837d0d4caa4fae3a9ff5b5dfbb82b7f422078
Braden Mars <bradenmars@bradenmars.me>
2020-08-29 05:57:46 -0500

    feat(loader): add overlay prop, add fade transition

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="relative">
-    <div
-      v-if="loading"
-      data-cy="overlay.loader"
-      class="absolute bottom-0 left-0 right-0 top-0 bg-crisiscleanup-light-grey opacity-75 flex items-center justify-center"
-    >
-      <slot name="element">
-        <spinner />
-      </slot>
-    </div>
-    <slot v-if="!loading" name="content" />
+    <transition name="fade">
+      <div
+        v-if="loading"
+        data-cy="overlay.loader"
+        class="absolute bottom-0 left-0 right-0 top-0 bg-crisiscleanup-light-grey opacity-75 flex items-center justify-center"
+      >
+        <slot name="element">
+          <spinner />
+        </slot>
+      </div>
+    </transition>
+    <template v-if="!overlay">
+      <slot v-if="!loading" name="content" />
+    </template>
+    <template v-else>
+      <slot v-bind="{ isLoading: loading }" name="content"></slot>
+    </template>
   </div>
 </template>
 
@@ -20,6 +27,20 @@ export default {
   name: 'Loader',
   props: {
     loading: VueTypes.bool.def(true),
+    overlay: VueTypes.bool.def(false),
   },
 };
 </script>
+
+<style lang="postcss">
+.fade {
+  &-leave-active,
+  &-enter-active {
+    transition: opacity 0.5s easeInOutCubic;
+  }
+  &-leave-to,
+  &-enter {
+    opacity: 0;
+  }
+}
+</style>
