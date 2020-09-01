fae418efc214fa33888006dcb2666be289beecbc
Braden Mars <bradenmars@bradenmars.me>
2020-08-31 19:00:58 -0500

    feat(phone): update phone dashboard layout

--- src/pages/phone/Dashboard.vue
+++ src/pages/phone/Dashboard.vue
@@ -1,68 +1,59 @@
 <template>
-  <Loader :loading="loading" class="root h-full">
-    <template #content>
-      <phone-layout>
-        <template #grid-start>
-          <div class="grid--start">
-            <agent-block />
-          </div>
-        </template>
-        <template #grid-main>
-          <div class="grid--main w-auto">
-            <div class="dash-grid h-full">
-              <div class="grid--leader">
-                <leaderboard />
-              </div>
-              <div class="grid--train">
-                <NewsTrainingCard
-                  @phone:showTraining="
-                    ($event) => (isShowingTrainingModal = $event)
-                  "
-                />
-              </div>
-              <div class="grid--history">
-                <CallHistory />
-              </div>
-            </div>
-          </div>
-          <div class="grid--footer">
-            <div v-if="currentUser.isAdmin" class="contact-container">
-              <ContactTable />
-            </div>
-          </div>
-        </template>
-      </phone-layout>
-      <TrainingModal
-        :visible="isShowingTrainingModal"
-        @onClose="isShowingTrainingModal = false"
-        @onComplete="onTrainingComplete"
-      ></TrainingModal>
-    </template>
-  </Loader>
+  <div class="phone__container">
+    <div class="phone__agent">
+      <div class="phone__agentcard">
+        <AgentCard />
+      </div>
+      <div class="phone__general">
+        <GeneralStatistics />
+      </div>
+      <div class="phone__agentmetrics">
+        <AgentAnalytics />
+      </div>
+    </div>
+
+    <div class="phone__main">
+      <div>
+        <leaderboard />
+        <NewsTrainingCard
+          @phone:showTraining="($event) => (isShowingTrainingModal = $event)"
+        />
+      </div>
+      <CallHistory />
+    </div>
+    <div v-if="currentUser.isAdmin" class="contact-container">
+      <ContactTable />
+    </div>
+    <TrainingModal
+      :visible="isShowingTrainingModal"
+      @onClose="isShowingTrainingModal = false"
+      @onComplete="onTrainingComplete"
+    ></TrainingModal>
+  </div>
 </template>
 
 <script>
 import User from '@/models/User';
-import Loader from '@/components/Loader.vue';
 import Leaderboard from '@/components/phone/Widgets/Leaderboard.vue';
-import PhoneLayout from '@/layouts/Phone.vue';
-import AgentBlock from '@/components/phone/blocks/Agent.vue';
 import CallHistory from '@/components/phone/Widgets/CallHistory.vue';
 import ContactTable from '@/components/phone/Widgets/ContactTable.vue';
 import NewsTrainingCard from '@/components/phone/Widgets/NewsTrainingCard.vue';
 import TrainingModal from '@/components/phone/TrainingsModal.vue';
+import AgentCard from '@/components/phone/AgentCard.vue';
+import GeneralStatistics from '@/components/phone/Widgets/GeneralStatistics.vue';
+import AgentAnalytics from '@/components/phone/Cards/StatsCard.vue';
 
 export default {
-  name: 'Phone',
+  name: 'PhoneDashboard',
   components: {
-    PhoneLayout,
-    AgentBlock,
-    Loader,
+    AgentAnalytics,
+    AgentCard,
     CallHistory,
     NewsTrainingCard,
     Leaderboard,
     ContactTable,
     TrainingModal,
+    GeneralStatistics,
   },
   data() {
     return {
@@ -83,42 +74,42 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-$areas: leader train history;
-
-.phonegrid {
-  &.grid-container {
-    grid:
-      [r1] 'start main' [r1end]
-      [r2] 'footer footer' [r2end]
-      / 1fr 3.75fr;
-  }
-}
-
-.grid--main {
-  .dash-grid {
-    display: inline-grid;
-    grid-gap: 2rem;
-    width: 100%;
-    padding-right: 2rem;
-    grid:
-      [r1] 'leader train' 1.25fr [r1end]
-      [r2] 'history history' 2.75fr [r2end]
-      / 1fr 1fr;
-    .grid {
-      @each $area in $areas {
-        &--#{$area} {
-          grid-area: $area;
-          display: flex;
-          flex-direction: column;
+<style scoped lang="postcss">
+.phone {
+  &__container {
+    @apply h-full;
+    lost-flex-container: row;
+    @apply w-full h-full;
+    .phone__agent {
+      @apply h-full;
+      lost-column: 1/4;
+      lost-flex-container: column;
+      @screen xl {
+        lost-column: 1/5;
+      }
+      box-sizing: border-box;
+      & > div {
+        margin-bottom: 30px;
+      }
+    }
+    .phone__main {
+      @apply h-full;
+      lost-flex-container: column;
+      lost-column: 3/4;
+      box-sizing: border-box;
+      @screen xl {
+        lost-column: 4/5;
+      }
+      & > div {
+        lost-row: 1/2;
+        &:first-child {
+          lost-flex-container: row;
+          & > div {
+            lost-column: 1/2;
+          }
         }
       }
     }
   }
-}
-
-.contact-container {
-  padding-right: 2rem;
-  @apply bg-crisiscleanup-light-grey;
 }
 </style>
