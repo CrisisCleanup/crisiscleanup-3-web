b708b6d906fc82e256edbc381e03ef649fb5a2c7
Braden Mars <bradenmars@bradenmars.me>
2020-09-04 06:58:22 -0500

    feat(charts): add BarLineChart component

---
+++ src/components/charts/BarLineChart.vue
@@ -0,0 +1,11 @@
+<script>
+import { Line, Bar, mixins } from 'vue-chartjs';
+const { reactiveProp } = mixins;
+export default {
+  mixins: [Line, Bar, reactiveProp],
+  props: ['chartdata', 'options'],
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  },
+};
+</script>
