ce55b19652ae0601ab950a13194f5b4287ced733
Braden Mars <bradenmars@bradenmars.me>
2020-09-04 01:13:54 -0500

    feat(phone): add outbound polling and debugger to root page

--- src/pages/phone/Index.vue
+++ src/pages/phone/Index.vue
@@ -2,6 +2,7 @@
   <PageLayout class="h-full w-full" :is-loading="false">
     <component :is="pageComponent" />
     <IncomingPopup v-show="callPending" />
+    <PhoneDebugger v-if="currentUser.isAdmin" />
   </PageLayout>
 </template>
 
@@ -16,18 +17,22 @@ import { computed, onMounted, watch } from '@vue/composition-api';
 import { useIntervalFn } from '@vueuse/core';
 import useContact from '@/use/phone/useContact';
 import useController from '@/use/phone/useController';
+import PhoneDebugger from '@/components/phone/Widgets/PhoneDebugger.vue';
+import useIncident from '@/use/worksites/useIncident';
+import useUser from '@/use/user/useUser';
 import Dashboard from './Dashboard.vue';
 import Controller from './Controller.vue';
 
 export default {
   name: 'PhoneRoot',
-  components: { IncomingPopup, PageLayout },
+  components: { IncomingPopup, PageLayout, PhoneDebugger },
   setup(props, context) {
     const { agent, loading } = useAgent();
     const controller = useController();
     const { callPending, callConnected, currentContact } = useContact({
       agent,
     });
+    const { currentIncident } = useIncident();
 
     const { start } = useIntervalFn(() => {
       agent.value.heartbeat().then(() => {
@@ -55,6 +60,31 @@ export default {
       }
     };
 
+    const requestOutbound = async () => {
+      if (!agent.value) return;
+      if (
+        !callConnected.value &&
+        !currentContact.value &&
+        !callPending.value &&
+        agent.value.isRoutable
+      ) {
+        await controller.actions.serveOutbound({
+          agent: agent.value,
+          incident: currentIncident.value,
+        });
+      }
+    };
+
+    const pollOutbound = useIntervalFn(
+      () => {
+        requestOutbound().then(() =>
+          context.root.$log.debug('outbound requested!'),
+        );
+      },
+      60000,
+      true,
+    );
+
     watch(
       () => callConnected.value,
       async () => updatePage(),
@@ -80,6 +110,8 @@ export default {
       callPending,
       callConnected,
       currentContact,
+      pollOutbound,
+      ...useUser(),
     };
   },
 };
