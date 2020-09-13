36f5a681d75fb4093c070459c473a8b92f81bfd6
Braden Mars <bradenmars@bradenmars.me>
2020-09-13 18:38:58 -0500

    feat(use): add locale support to usePhoneMetrics hook

--- src/use/phone/usePhoneMetrics.js
+++ src/use/phone/usePhoneMetrics.js
@@ -3,27 +3,40 @@
  * Use Phone Metrics Hook
  */
 
-import { computed, onMounted } from '@vue/composition-api';
+import { computed, ref } from '@vue/composition-api';
 import ControllerStore from '@/store/modules/phone/controller';
 import { getModule } from 'vuex-module-decorators';
 import { useStore } from '@u3u/vue-hooks';
 import type { PhoneMetric } from '@/store/modules/phone/types';
+import { wrap } from '@/utils/wrap';
+import Language from '@/models/Language';
+import _ from 'lodash';
 
 export type UsePhoneMetricsProps = {|
   metrics: PhoneMetric[],
+  category: string,
 |};
 
-export default ({ metrics }: UsePhoneMetricsProps) => {
+export default ({ metrics, category }: UsePhoneMetricsProps) => {
+  const _category = wrap(category);
   const store = useStore();
   const ctrlStore = getModule(ControllerStore, store.value);
+  const _locales = ref();
 
-  const generalMetrics = computed(() => ctrlStore.getGeneralMetrics(metrics));
-  const updateGenMetrics = async () => ctrlStore.updateMetrics();
+  const generalMetrics = computed(() =>
+    ctrlStore.getGeneralMetrics(metrics, _category.value),
+  );
+  const updateGenMetrics = async () => {
+    await ctrlStore.updateMetrics();
+    const subtags = _.keys(_.omit(ctrlStore.metrics, ['all']));
+    _locales.value = await Language.fetchBySubtags(subtags);
+  };
 
-  onMounted(async () => ctrlStore.updateMetrics());
+  const locales = computed(() => (_locales.value ? _locales.value : []));
 
   return {
     generalMetrics,
     updateGenMetrics,
+    locales,
   };
 };
