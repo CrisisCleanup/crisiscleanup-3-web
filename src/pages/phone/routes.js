4ee873c46666880233ade9ab35a7703f99a5abdc
Braden Mars <bradenmars@bradenmars.me>
2020-09-30 19:38:55 -0500

    feat(phone): add beta banner toggle switch to phone routes

--- src/pages/phone/routes.js
+++ src/pages/phone/routes.js
@@ -1,3 +1,5 @@
+import { BannerTypes } from '@/store/modules/ui/types';
+import BetaBanner from '@/components/notifications/BetaBanner.vue';
 import PhoneLayout from './Index.vue';
 
 const routes = [
@@ -9,6 +11,20 @@ const routes = [
       layout: 'authenticated',
       can: 'phone_agent',
       fail: '$from',
+      banner: {
+        type: BannerTypes.INFO,
+        enabled: true,
+        component: {
+          render() {
+            return (
+              <BetaBanner
+                text={'~~Having issues? Switch back to the old system.'}
+                beta-feature="aws_connect_phone"
+              />
+            );
+          },
+        },
+      },
     },
   },
 ];
