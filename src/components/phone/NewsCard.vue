7ed365fa44c01fe152399b3065f889034ab02806
Braden Mars <bradenmars@bradenmars.me>
2020-10-04 04:27:36 -0500

    fix(phone): prep news components for aaron

--- src/components/phone/NewsCard.vue
+++ src/components/phone/NewsCard.vue
@@ -1,15 +1,13 @@
 <template>
   <!-- News 1 -->
-  <div class="h-full flex flex-row flex-grow justify-around p-3 news-card">
+  <div class="h-full flex flex-row flex-grow justify-start p-3 news-card">
     <!--- media/picture --->
-    <div class="card--img">
-      <!-- <img :src="imagePath" alt="Video Preview" /> -->
-      <img src="@/assets/news/hurricane.png" class="news-img shadow" :title="$t('phoneDashboard.news_1_img_title')" />
+    <div class="card--img w-1/6">
+      <img :src="media.path" class="news-img shadow" :alt="media.alt" />
     </div>
     <div class="card--body">
       <!--- body blurb --->
-      <base-text variant="bodysm" :v-html="$t('phoneDashboard.news_1')">
-      </base-text>
+      <div :v-html="content" />
       <div class="flex flex-row justify-between">
         <div class="flex-col">
           <!--- Date --->
@@ -17,111 +15,7 @@
             variant="bodysm"
             class="justify-between text-crisiscleanup-grey-800 alight-left"
           >
-            {{ $t('phoneDashboard.news_1_date') }}
-            <!--{{ [timeToComplete, ''] | duration('humanize') }}-->
-          </base-text>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- End News -->
-  <!-- News 2 -->
-  <div class="h-full flex flex-row flex-grow justify-around p-3 news-card">
-    <!--- media/picture --->
-    <div class="card--img">
-      <!-- <img :src="imagePath" alt="Video Preview" /> -->
-      <img src="@/assets/news/list.png" class="news-img shadow" :title="$t('phoneDashboard.news_2_img_title')" />
-    </div>
-    <div class="card--body">
-      <!--- body blurb --->
-      <base-text variant="bodysm" v-html="$t('phoneDashboard.news_2')">
-      </base-text>
-      <div class="flex flex-row justify-between">
-        <div class="flex-col">
-          <!--- Date --->
-          <base-text
-            variant="bodysm"
-            class="justify-between text-crisiscleanup-grey-800 alight-left"
-          >
-            {{ $t('phoneDashboard.news_2_date') }}
-            <!--{{ [timeToComplete, ''] | duration('humanize') }}-->
-          </base-text>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- End News -->
-  <!-- News 3 -->
-  <div class="h-full flex flex-row flex-grow justify-around p-3 news-card">
-    <!--- media/picture --->
-    <div class="card--img">
-      <!-- <img :src="imagePath" alt="Video Preview" /> -->
-      <img src="@/assets/news/char_3_phone_blue.png" class="news-img shadow" :title="$t('phoneDashboard.news_3_img_title')" />
-    </div>
-    <div class="card--body">
-      <!--- body blurb --->
-      <base-text variant="bodysm" v-html="$t('phoneDashboard.news_3')">
-      </base-text>
-      <div class="flex flex-row justify-between">
-        <div class="flex-col">
-          <!--- Date --->
-          <base-text
-            variant="bodysm"
-            class="justify-between text-crisiscleanup-grey-800 alight-left"
-          >
-            {{ $t('phoneDashboard.news_3_date') }}
-            <!--{{ [timeToComplete, ''] | duration('humanize') }}-->
-          </base-text>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- End News -->
-  <!-- News 4 -->
-  <div class="h-full flex flex-row flex-grow justify-around p-3 news-card">
-    <!--- media/picture --->
-    <div class="card--img">
-      <!-- <img :src="imagePath" alt="Video Preview" /> -->
-      <img src="@/assets/news/smart_phone.png" class="news-img shadow" :title="$t('phoneDashboard.news_4_img_title')" />
-    </div>
-    <div class="card--body">
-      <!--- body blurb --->
-      <base-text variant="bodysm" v-html="$t('phoneDashboard.news_4')">
-      </base-text>
-      <div class="flex flex-row justify-between">
-        <div class="flex-col">
-          <!--- Date --->
-          <base-text
-            variant="bodysm"
-            class="justify-between text-crisiscleanup-grey-800 alight-left"
-          >
-            {{ $t('phoneDashboard.news_4_date') }}
-            <!--{{ [timeToComplete, ''] | duration('humanize') }}-->
-          </base-text>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- End News -->
-  <!-- News 5 -->
-  <div class="h-full flex flex-row flex-grow justify-around p-3 news-card">
-    <!--- media/picture --->
-    <div class="card--img">
-      <!-- <img :src="imagePath" alt="Video Preview" /> -->
-      <img src="@/assets/news/photo_lady_phone_computer.png" class="news-img shadow" :title="$t('phoneDashboard.news_5_img_title')" />
-    </div>
-    <div class="card--body">
-      <!--- body blurb --->
-      <base-text variant="bodysm" v-html="$t('phoneDashboard.news_5')">
-      </base-text>
-      <div class="flex flex-row justify-between">
-        <div class="flex-col">
-          <!--- Date --->
-          <base-text
-            variant="bodysm"
-            class="justify-between text-crisiscleanup-grey-800 alight-left"
-          >
-            {{ $t('phoneDashboard.news_5_date') }}
+            {{ date }}
             <!--{{ [timeToComplete, ''] | duration('humanize') }}-->
           </base-text>
         </div>
@@ -138,9 +32,13 @@ export default {
   name: 'NewsCard',
   props: {
     completed: VueTypes.bool,
-    imagePath: VueTypes.string,
-    description: VueTypes.string,
+    media: VueTypes.shape({
+      path: VueTypes.string,
+      alt: VueTypes.string,
+    }),
+    content: VueTypes.string,
     timeToComplete: VueTypes.string,
+    date: VueTypes.string,
   },
   data() {
     return {
@@ -166,10 +64,13 @@ export default {
   }
   .card {
     &--img {
-      flex: 1;
+      img {
+        object-fit: cover;
+        width: 128px;
+        height: 128px;
+      }
     }
     &--body {
-      flex: 2.4;
       @apply pl-3 text-crisiscleanup-dark-500;
     }
   }
