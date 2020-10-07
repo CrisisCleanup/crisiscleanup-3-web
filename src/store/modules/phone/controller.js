2e89a320cbb84f64c8c7e5d813a2cbf84c2e9f04
Aaron Titus <aaron@crisiscleanup.org>
2020-10-07 13:01:10 -0600

    Misc localizations

--- src/store/modules/phone/controller.js
+++ src/store/modules/phone/controller.js
@@ -86,7 +86,7 @@ export const Metrics = Object.freeze({
   CALLBACKS_QUEUED: [
     'contactsInQueueOutbound',
     'phoneDashboard.remaining_callbacks',
-    '~~This is the current total of missed calls waiting to be returned.',
+    'phoneDashboard.total_count_missed_calls',
   ],
   AVAILABLE: ['agentsAvailable', 'phoneDashboard.volunteers_available'],
   AGENTS_ON_CALL: ['agentsOnCall', 'phoneDashboard.volunteers_on_phone'],
@@ -95,7 +95,7 @@ export const Metrics = Object.freeze({
   CALLDOWNS_QUEUED: [
     'contactsScheduledOutbound',
     'phoneDashboard.remaining_calldowns',
-    '~~A calldown is an outbound call to a survivor with an open case to check if they still require help. This helps keep the map clean and minimize the time that volunteers must spend calling to find work or else they quit early and go home.  Your job is to update the case with a note as well as change the status of work items if they are already completed.',
+    'phoneDashboard.calldown_explanation',
   ],
   // admin metrics (includes unlocked+locked)
   CALLBACKS_QUEUED_ALL: [
