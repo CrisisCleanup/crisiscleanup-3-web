dbe73e7d846f2ca462b57c6a0beae4d159c5e5a0
Aaron Titus <aaron@crisiscleanup.org>
2020-10-10 11:46:41 -0600

    Add Most recent to Phone Alert

--- src/components/phone/Popup.vue
+++ src/components/phone/Popup.vue
@@ -91,7 +91,7 @@
             <base-text variant="bodysm">
               {{ callerHistory.total }}
               {{
-                ` ${$t('phoneDashboard.calls')} | ${$t(callerHistory.recent)}`
+                ` ${$t('phoneDashboard.calls')} | ${$t('phoneDashboard.most_recent')} ${$t(callerHistory.recent)}`
               }}
             </base-text>
           </tag>
