ff1437abb04847835cd4b65bd9ffd71b04db6480
pablo014 <angelopablo014@gmail.com>
2021-06-25 14:10:55 -0600

    feat(pewpew): make dynamic and add first tab of extended

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,7 +2,10 @@
   <HomeLayout>
     <template #grid-content>
       <div class="grid--main h-screen">
-        <OrganizationActivity :organization-info="test" />
+        <OrganizationActivity
+          :organization-info="test"
+          :organization-image="testImage"
+        />
         <div class="cover-image"></div>
       </div>
     </template>
@@ -21,7 +24,41 @@ export default {
         name: 'Southeastern WI COAD',
         orgType: 'Humanitarian Service',
         availability: 'Works in all 50 States',
+        address: '3132 Brinkley Road, Temple Hills, Maryland 20748',
+        type: '501(c)(3) Charity',
+        role: 'COAD',
+        cases: 45,
+        claimed: 15,
+        calls: 25200,
+        value: 2700000,
+        financialAssistance: [5, 4, 4, 3, 3, 5],
+        sbaLoans: [1, 3, 5, 7, 7, 5],
+        incidents: [
+          {
+            name: 'Hurricane Florence',
+            cases: 45,
+            claimed: 103,
+            calls: 25200,
+            value: 2700000,
+          },
+          {
+            name: 'Hurricane Irma',
+            cases: 45,
+            claimed: 103,
+            calls: 25200,
+            value: 2700000,
+          },
+          {
+            name: 'Flint Michigan Water Crisis',
+            cases: 45,
+            claimed: 103,
+            calls: 25200,
+            value: 2700000,
+          },
+        ],
       },
+      testImage:
+        'https://assets.fireside.fm/file/fireside-images/podcasts/images/b/bc7f1faf-8aad-4135-bb12-83a8af679756/cover.jpg?v=3',
     };
   },
 };
