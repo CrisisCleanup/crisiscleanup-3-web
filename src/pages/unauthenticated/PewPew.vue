e870c01b51e90d8a795f8af9bfa48d62f4326a6b
Tobi Abiodun <abiodun.tobi@gmail.com>
2021-06-25 09:47:39 -0400

    Initial map experiments

--- src/pages/unauthenticated/PewPew.vue
+++ src/pages/unauthenticated/PewPew.vue
@@ -125,7 +125,16 @@
         </div>
         <div class="flex-grow grid grid-cols-4">
           <div class="col-span-3 flex flex-col">
-            <div class="h-10"></div>
+            <div class="h-10">
+              <base-button
+                :action="pauseGeneratePoints"
+                :text="$t('~~Pause')"
+              ></base-button>
+              <base-button
+                :action="generatePoints"
+                :text="$t('~~Start')"
+              ></base-button>
+            </div>
             <div class="flex-grow">
               <div class="relative h-full">
                 <div
@@ -280,12 +289,19 @@ import * as L from 'leaflet';
 import { colors, templates } from '@/icons/icons_templates';
 import { makeTableColumns } from '@/utils/table';
 
-import { getMarkerLayer, mapAttribution, mapTileLayer } from '@/utils/map';
+import {
+  calcWaypoints,
+  getMarkerLayer,
+  mapAttribution,
+  mapTileLayerDark as mapTileLayer,
+} from '@/utils/map';
 import { HomeNavigation } from '@/components/home/SideNav';
 import Table from '@/components/Table';
 import { getQueryString } from '@/utils/urls';
 import BarChart from '@/components/charts/BarChart';
+import { Sprite, Texture, Graphics } from 'pixi.js';
 
+const locations = [];
 export default {
   name: 'PewPew',
   components: { Table, BarChart },
@@ -349,7 +365,10 @@ export default {
       if (!this.map) {
         this.map = L.map('map', {
           zoomControl: false,
-        }).setView([35.7465122599185, -96.41150963125656], 10);
+        }).fitBounds([
+          [74.41346754714019, -60.40262499542409],
+          [-8.365558986563846, -141.2619999954241],
+        ]);
       }
       const { map } = this;
 
@@ -365,14 +384,126 @@ export default {
       this.incidents = await this.getRecentIncidents();
       this.organizations = await this.getOrganizations();
       const response = await this.$http.get(
-        `${process.env.VUE_APP_API_BASE_URL}/event_stream?limit=500`,
+        `${process.env.VUE_APP_API_BASE_URL}/event_stream?limit=50`,
       );
       this.markers = response.data.results;
       [this.currentEvent] = this.markers;
-      const worksiteLayer = getMarkerLayer(response.data.results, map, this);
+      const worksiteLayer = getMarkerLayer([], map, this);
       worksiteLayer.addTo(map);
 
       map.attributionControl.setPosition('bottomright');
+    },
+    generateMarker() {
+      this.map.eachLayer((layer) => {
+        if (layer.key === 'marker_layer') {
+          const marker = {
+            actor_location:
+              locations[Math.floor(Math.random() * locations.length)],
+            patient_location:
+              locations[Math.floor(Math.random() * locations.length)],
+          };
+          const markerTemplate = templates.circle;
+          const scale = layer.utils.getScale();
+          const actorCoords = layer.utils.latLngToLayerPoint([
+            marker.actor_location.coordinates[1],
+            marker.actor_location.coordinates[0],
+          ]);
+
+          const actorMarkerSprite = new Sprite();
+          actorMarkerSprite.x = actorCoords.x;
+          actorMarkerSprite.y = actorCoords.y;
+          actorMarkerSprite.x0 = actorCoords.x;
+          actorMarkerSprite.y0 = actorCoords.y;
+          actorMarkerSprite.interactive = true;
+          actorMarkerSprite.anchor.set(0.5, 0.5);
+          actorMarkerSprite.on('click', function () {
+            alert();
+          });
+          let svg = markerTemplate
+            .replace('{{fillColor}}', 'red')
+            .replace('{{strokeColor}}', 'black');
+          actorMarkerSprite.texture = Texture.from(svg);
+          actorMarkerSprite.visible = true;
+
+          const patientCoords = layer.utils.latLngToLayerPoint([
+            marker.patient_location.coordinates[1],
+            marker.patient_location.coordinates[0],
+          ]);
+
+          const patientMarkerSprite = new Sprite();
+          patientMarkerSprite.x = patientCoords.x;
+          patientMarkerSprite.y = patientCoords.y;
+          patientMarkerSprite.x0 = patientCoords.x;
+          patientMarkerSprite.y0 = patientCoords.y;
+          patientMarkerSprite.interactive = false;
+          patientMarkerSprite.anchor.set(0.5, 0.5);
+          // patientMarkerSprite.on('click', function () {
+          //   alert();
+          // });
+          svg = markerTemplate
+            .replace('{{fillColor}}', '#61D5F8')
+            .replace('{{strokeColor}}', 'black');
+          patientMarkerSprite.texture = Texture.from(svg);
+          patientMarkerSprite.visible = true;
+
+          layer._pixiContainer.addChild(actorMarkerSprite);
+          layer._pixiContainer.addChild(patientMarkerSprite);
+
+          const midX = (actorMarkerSprite.x + patientMarkerSprite.x) / 2;
+          const midY = (actorMarkerSprite.y + patientMarkerSprite.y) / 2;
+
+          //
+          // const line = new Graphics();
+          // line.lineStyle(3, 0x3388ff, 1);
+          // line.x = patientCoords.x;
+          // line.y = patientCoords.y;
+          // line.moveTo(0, 0);
+          // line.lineTo(actorCoords.x, actorCoords.y);
+          // line.lineTo(patientCoords.x, patientCoords.y);
+
+          // line.arcTo(midX, midY, actorCoords.x, actorCoords.y, 10);
+          // line.type = 'line';
+          // line.from = [actorCoords.x, actorCoords.y];
+          // line.to = [patientCoords.x, patientCoords.y];
+          // layer._pixiContainer.addChild(line);
+
+          // const rect = new Graphics();
+          // rect.type = 'line';
+          // rect.beginFill(0xffaa00);
+          // rect.drawRect(midX, midY, 1024, 1024);
+          // rect.endFill();
+
+          layer._renderer.render(layer._pixiContainer);
+          layer.redraw();
+
+          const linksGraphics = new Graphics();
+          linksGraphics.lineStyle(50, 0x61d5f8);
+          linksGraphics.moveTo(actorMarkerSprite.x, actorMarkerSprite.y);
+          linksGraphics.lineTo(patientMarkerSprite.x, patientMarkerSprite.y);
+          linksGraphics.from = [actorCoords.x, actorCoords.y];
+          linksGraphics.to = [patientCoords.x, patientCoords.y];
+          linksGraphics.mid = [midX, midY];
+          linksGraphics.type = 'line';
+          linksGraphics.wayPoints = calcWaypoints([
+            actorMarkerSprite,
+            patientMarkerSprite,
+          ]);
+          linksGraphics.currentPoint = 0;
+
+          setTimeout(() => {
+            layer._pixiContainer.addChild(linksGraphics);
+            layer._renderer.render(layer._pixiContainer);
+            layer.redraw();
+          }, 50);
+        }
+      });
+    },
+    generatePoints() {
+      this.eventsInterval = setInterval(this.generateMarker, 2000);
+    },
+    pauseGeneratePoints() {
+      clearInterval(this.eventsInterval);
+      this.eventsInterval = null;
     },
     async getCompletionRateData() {
       const response = await this.$http.get(
