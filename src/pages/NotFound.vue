e755e1051b8fead98b5215d82da0530c98ca5978
pablo014 <angelopablo014@gmail.com>
2021-07-16 13:24:30 -0600

    feat(pewpew): make basic layout for newspost

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,6 +2,20 @@
   <HomeLayout>
     <template #grid-content>
       <div class="grid--main h-screen">
+        <newspost
+          class="text-white w-2/12"
+          is-user-post
+          :user-info="userInfo"
+          avatar-icon="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png"
+          image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png"
+        >
+          <template #header>Hello</template>
+          <template #corner>5 min ago</template>
+          <template #content
+            >Description massa eget egestas purus viverra accumsan in nisl nisi.
+            Ac tincidunt vitae semper quis lectus nulla at.
+          </template>
+        </newspost>
         <div class="cover-image"></div>
       </div>
     </template>
@@ -10,9 +24,19 @@
 
 <script>
 import HomeLayout from '@/layouts/Home';
+import Newspost from '@/components/Newspost.vue';
 export default {
   name: 'NotFound',
-  components: { HomeLayout },
+  components: { HomeLayout, Newspost },
+  data() {
+    return {
+      userInfo: {
+        name: 'Angelo Pablo',
+        organization:
+          'A Really long Organization Name for no reason just as to test truncating a string',
+      },
+    };
+  },
 };
 </script>
 
