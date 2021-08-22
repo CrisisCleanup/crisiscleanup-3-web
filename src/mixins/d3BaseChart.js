93903c1c8eb650159468c80bb3b67d24e094f19f
Deep Panchal <deep302001@gmail.com>
2021-08-22 09:01:08 -0500

    feat(mixins): add d3 base chart mixin to be used by other d3 chart components

---
+++ src/mixins/d3BaseChart.js
@@ -0,0 +1,86 @@
+// @flow
+import * as d3 from 'd3';
+import _ from 'lodash';
+import VueTypes from 'vue-types';
+
+export const D3BaseChartMixin = {
+  props: {
+    /**
+     * Unique chart ID
+     * Defaults to component name with suffixing unique id
+     */
+    chartId: VueTypes.string.def(`d3-chart-${_.uniqueId()}`),
+    /**
+     * top, bottom, left, right margins
+     */
+    marginAll: VueTypes.number.def(5),
+    /**
+     * background color / gradient
+     */
+    bgColor: VueTypes.string.def('transparent'),
+  },
+
+  data() {
+    return {
+      margin: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+      },
+      svg: null,
+      x: null,
+      y: null,
+    };
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      this.margin.top = this.marginAll;
+      this.margin.bottom = this.marginAll;
+      this.margin.left = this.marginAll;
+      this.margin.right = this.marginAll;
+
+      this.destroyChart();
+      this.renderChart();
+    });
+
+    window.addEventListener(
+      'resize',
+      _.debounce(() => {
+        this.destroyChart();
+        this.renderChart();
+      }, 1500),
+    );
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('resize', _.noop);
+  },
+
+  methods: {
+    getWidth(): number {
+      return +d3.select(`#${this.chartId}`).style('width').slice(0, -2) || 0;
+    },
+
+    getHeight(): number {
+      return +d3.select(`#${this.chartId}`).style('height').slice(0, -2) || 0;
+    },
+
+    getInnerWidth(): number {
+      return this.getWidth() - this.margin.left - this.margin.right;
+    },
+
+    getInnerHeight(): number {
+      return this.getHeight() - this.margin.top - this.margin.bottom;
+    },
+
+    renderChart() {
+      console.log('base render chart function');
+    },
+
+    destroyChart() {
+      d3.select(`#${this.chartId} svg`).remove();
+    },
+  },
+};
