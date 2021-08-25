c24e57a50888032acfc723d27f97b90f36e44a97
Braden Mars <bradenmars@bradenmars.me>
2021-08-25 04:26:11 -0500

    feat(comp): add extracted UserProfileMenu comp with color invert prop

---
+++ src/components/header/UserProfileMenu.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="profile-menu flex items-center header-item overflow-hidden h-full"
+    :class="invert && '--invert'"
+  >
+    <v-popover
+      popover-class="menu-popover"
+      placement="bottom-end"
+      data-cy="auth.userprofile"
+    >
+      <div class="profile-menu__body flex cursor-pointer items-center">
+        <Avatar
+          :initials="currentUser && currentUser.first_name"
+          :url="currentUser && currentUser.profilePictureUrl"
+          class="p-1"
+          size="small"
+        />
+        <base-text variant="h3" class="p-3" regular :class="[...styles.title]">
+          {{ currentUser && currentUser.full_name }}
+          <font-awesome-icon class="cursor-pointer" icon="caret-down" />
+        </base-text>
+      </div>
+      <div slot="popover" class="flex flex-col">
+        <base-button
+          data-cy="auth.userprofile.profile"
+          class="text-base p-2 hover:bg-crisiscleanup-light-grey cursor-pointer"
+          :text="$t('actions.profile')"
+          :action="
+            () => {
+              $router.push(`/profile`);
+            }
+          "
+        />
+        <base-button
+          data-cy="auth.userprofile.logout"
+          class="text-base p-2 hover:bg-crisiscleanup-light-grey cursor-pointer"
+          :text="$t('actions.logout')"
+          :action="() => $emit('auth:logout')"
+        />
+      </div>
+    </v-popover>
+  </div>
+</template>
+<script>
+import Avatar from '@/components/Avatar';
+import { UserMixin } from '@/mixins';
+
+export default {
+  name: 'UserProfileMenu',
+  components: { Avatar },
+  mixins: [UserMixin],
+  props: {
+    invert: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    styles() {
+      return {
+        title:
+          this.invert === true
+            ? ['text-white']
+            : ['text-crisiscleanup-dark-300'],
+      };
+    },
+  },
+};
+</script>
+
+<style scoped lang="postcss"></style>
