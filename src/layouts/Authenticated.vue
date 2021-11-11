79743bb6087540f484dee936519836b0c202dbee
Tobi Abiodun <abiodun.tobi@gmail.com>
2021-11-11 17:16:14 -0500

    https://github.com/CrisisCleanup/crisiscleanup-3-api/issues/657

--- src/layouts/Authenticated.vue
+++ src/layouts/Authenticated.vue
@@ -222,7 +222,7 @@ export default {
     }
     await Promise.all([
       Incident.api().get(
-        '/incidents?fields=id,name,short_name,geofence,locations,turn_on_release&limit=150&ordering=-start_at',
+        '/incidents?fields=id,name,short_name,geofence,locations,turn_on_release&limit=250&ordering=-start_at',
         {
           dataKey: 'results',
         },
