9271ea5c93528cba8f68e6a3164bfc7d59174d68
pablo014 <angelopablo014@gmail.com>
2022-04-20 12:45:56 -0500

    fix(adminEvents): make mobile responsive

--- src/pages/admin/AdminEvents.vue
+++ src/pages/admin/AdminEvents.vue
@@ -16,7 +16,7 @@
           </div>
         </TitledCard>
       </div>
-      <div class="events__build">
+      <div class="sm:events__build">
         <TitledCard
           class="build__input"
           :title="$t('adminDashboard.event_builder')"
