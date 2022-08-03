f6f6a586fe773b83ab6b5a762d965aebffc617fa
Deep Panchal <deep302001@gmail.com>
2022-08-03 11:53:06 -0500

    feat(use): update usePhoneService hook with inject

--- src/use/usePhoneService.ts
+++ src/use/usePhoneService.ts
@@ -1,5 +1,12 @@
+import { inject } from 'vue';
+import PhoneService from '@/services/phone.service';
+
 export default () => {
+  const phoneService = inject<PhoneService>('$phoneService');
+  if (!phoneService) {
+    throw new Error('PhoneService not found');
+  }
   return {
-    $phoneService: window.vue.$phoneService,
+    $phoneService: phoneService,
   };
 };
