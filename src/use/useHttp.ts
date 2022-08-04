daa72a100d4cc9ddfe84417f5abb3d32637d98bd
Deep Panchal <deep302001@gmail.com>
2022-08-04 16:34:13 -0500

    fix(use): add nilish check for axios inject on useHttp

--- src/use/useHttp.ts
+++ src/use/useHttp.ts
@@ -2,8 +2,11 @@ import type { AxiosInstance } from 'axios';
 import { inject } from 'vue';
 
 export default () => {
-  const axios = inject<AxiosInstance>('axios');
+  const axios = inject<AxiosInstance>('$axios');
+  if (!axios) {
+    throw new Error('$axios not found');
+  }
   return {
-    $http: axios as AxiosInstance,
+    $http: axios,
   };
 };
