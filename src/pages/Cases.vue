4fe2e9d4c74285c22d9c8199553d1ddfe5447356
pablo014 <angelopablo014@gmail.com>
2022-08-26 10:15:20 -0600

    fix(cases): red cross hover

--- src/pages/Cases.vue
+++ src/pages/Cases.vue
@@ -83,18 +83,16 @@
                     @input="toggleHeatMap"
                   >
                     <div class="flex">
-                      <img class="w-5 h-5" src="@/assets/red-cross-logo.jpg" />
-                      <ccu-icon
+                      {{ $t('casesVue.show_damaged_areas') }}
+                      <img
+                        class="w-5 h-5"
+                        src="@/assets/red-cross-logo.jpg"
                         v-tooltip="{
                           content: $t('casesVue.damage_assessment_help'),
                           trigger: 'hover',
                           classes: 'interactive-tooltip w-72',
                         }"
-                        :alt="$t('casesVue.damage_assessment_help')"
-                        type="help"
-                        size="large"
                       />
-                      {{ $t('casesVue.show_damaged_areas') }}
                     </div>
                   </base-checkbox>
                 </div>
