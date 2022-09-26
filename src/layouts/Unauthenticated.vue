33fd0173407a75011066161e17d96134a42f35b7
Deep Panchal <deep302001@gmail.com>
2022-09-26 14:43:42 -0500

    feat(layouts): set localizations for unauthenticated pages based on browser lang

--- src/layouts/Unauthenticated.vue
+++ src/layouts/Unauthenticated.vue
@@ -4,10 +4,52 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import detectBrowserLanguage from 'detect-browser-language';
+import { useMutations } from '@u3u/vue-hooks';
+import { size } from 'lodash';
+import moment from 'moment';
+import { onMounted } from '@vue/composition-api';
+import { i18nService } from '@/services/i18n.service';
+
 export default {
-  computed: {},
-  methods: {},
+  setup(props, context) {
+    const { $t, $log, $http } = context.root;
+    const i18nLocale = context.root.$i18n.locale;
+    const { setLanguage } = useMutations('locale', ['setLanguage']);
+    const currentLanguage = detectBrowserLanguage() as string;
+    console.log('current lang', currentLanguage);
+
+    const setupLanguage = async () => {
+      if (currentLanguage.startsWith('es')) {
+        setLanguage('es-MX'); // if browser language is spanish, set to spanish (mexico)
+      } else {
+        setLanguage(currentLanguage);
+      }
+      if (currentLanguage !== i18nLocale) {
+        try {
+          const data = await i18nService.getLanguage(currentLanguage);
+          const { translations } = data;
+          if (size(translations) > 0) {
+            context.root.$i18n.setLocaleMessage(currentLanguage, translations);
+            context.root.$i18n.locale = currentLanguage;
+            $http.defaults.headers.common['Accept-Language'] = currentLanguage;
+            const htmlHtmlElement = document.querySelector('html');
+            if (htmlHtmlElement) {
+              htmlHtmlElement.setAttribute('lang', currentLanguage);
+            }
+          }
+        } catch (e) {
+          $log.error(e);
+        }
+        moment.locale(currentLanguage.split('-')[0]);
+      }
+    };
+
+    onMounted(async () => {
+      await setupLanguage();
+    });
+  },
 };
 </script>
 
