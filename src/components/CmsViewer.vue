fbcef6ecb1f5cab54b8bdc21a964a763f0a66fff
Deep Panchal <deep302001@gmail.com>
2022-09-29 16:08:25 -0500

    fix(comps): add specific styling on all a tags on news feed

--- src/components/CmsViewer.vue
+++ src/components/CmsViewer.vue
@@ -3,13 +3,13 @@
     <img v-if="image" :src="image" class="w-32 h-32 mr-2" />
     <div>
       <div class="text-2xl my-1 font-bold" v-html="$t(title)"></div>
-      <div class="text-lg" v-html="$t(content)"></div>
+      <div class="cms-viewer cms-viewer__content" v-html="$t(content)"></div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, onMounted } from '@vue/composition-api';
 
 export default defineComponent({
   name: 'CmsViewer',
@@ -30,4 +30,13 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style lang="postcss">
+.cms-viewer {
+  &__content {
+    @apply text-lg;
+    a {
+      text-decoration: underline !important;
+    }
+  }
+}
+</style>
