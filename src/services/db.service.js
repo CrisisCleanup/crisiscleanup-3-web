0b338be81afe67d0ca95c8f57ef46329d4dfa918
Tobi Abiodun <abiodun.tobi@gmail.com>
2022-10-15 09:25:11 -0400

    Switch to saving object instead of large string

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -8,13 +8,13 @@ const dbPromise = openDB('crisiscleanup', 1, {
 const DbService = {
   async setItem(key, val) {
     const idbpDatabase = await dbPromise;
-    return idbpDatabase.put('keyval', JSON.stringify(val), key);
+    return idbpDatabase.put('keyval', val, key);
   },
   async getItem(key) {
     const idbpDatabase = await dbPromise;
     const entry = await idbpDatabase.get('keyval', key);
     if (entry) {
-      return JSON.parse(entry);
+      return entry;
     }
     return null;
   },
