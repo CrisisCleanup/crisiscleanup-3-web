ddec918354a366533ba4eb5a147dbb6509d557cc
Deep Panchal <deep302001@gmail.com>
2022-12-10 17:29:51 -0600

    feat(pages): initial copy of organization comps from old repo
    - broken commit
    
    (cherry picked from commit 73436899a8814ff8f7040f2f973ecd8c07b1b844)

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,16 @@ import PhoneRoutes from './pages/phone/routes';
 import { store } from './store';
 import AdminRoutes from './pages/admin/routes';
 
+import Affiliates from '@/pages/organization/Affiliates.vue';
+import Organization from '@/pages/organization/Index.vue';
+import Invitations from '@/pages/organization/Invitations.vue';
+import Layers from '@/pages/organization/Layers.vue';
+import OrganizationProfile from '@/pages/organization/Profile.vue';
+import Users from '@/pages/organization/Users.vue';
+import UserView from '@/pages/organization/UserView.vue';
+import Teams from '@/pages/organization/Teams.vue';
+import TeamDetail from '@/pages/organization/TeamDetail.vue';
+
 const routes = [
   { path: '/', component: Dashboard, name: 'Home' },
   {
@@ -42,6 +52,67 @@ const routes = [
   ...HomeRoutes,
   ...PhoneRoutes,
   ...AdminRoutes,
+  {
+    path: '/organization',
+    component: Organization,
+    name: 'nav.organization',
+    children: [
+      {
+        path: '',
+        component: Invitations,
+        name: 'nav.organization_invitations',
+        meta: { id: 'invitations' },
+      },
+      {
+        path: 'invitations',
+        component: Invitations,
+        name: 'nav.organization_invitations',
+        meta: { id: 'invitations' },
+      },
+      {
+        path: 'users',
+        component: Users,
+        name: 'nav.organization_users',
+        children: [
+          {
+            path: ':user_id',
+            component: UserView,
+            name: 'nav.organization_users',
+            meta: { id: 'user_detail' },
+          },
+        ],
+      },
+      {
+        path: 'affiliates',
+        component: Affiliates,
+        name: 'nav.organization_affiliates',
+      },
+      {
+        path: 'profile',
+        component: OrganizationProfile,
+        name: 'nav.organization_profile',
+      },
+      {
+        path: 'teams',
+        component: Teams,
+        name: 'nav.organization_teams',
+        children: [
+          {
+            path: ':team_id',
+            component: TeamDetail,
+            name: 'nav.organization_team_detail',
+            meta: { id: 'team_detail' },
+          },
+        ],
+      },
+      {
+        path: 'layers',
+        component: Layers,
+        name: 'nav.organization_layers',
+      },
+    ],
+    meta: { layout: 'authenticated' },
+  },
 ];
 
 const router = createRouter({
