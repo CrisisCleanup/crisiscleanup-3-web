05fdd17789aa28b639bd0aa6202cdc0e4a7ea211
Deep Panchal <deep302001@gmail.com>
2022-12-10 15:58:37 -0600

    feat(config): improve vite config
    (cherry picked from commit d94f72c51b8a6dc147a900d0357dc241263ad14d)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,35 @@
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
+import _ from 'lodash';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    __VUE_PROD_DEVTOOLS__: true,
-  },
-  plugins: [vue(), vueJsx()],
+export default defineConfig(async ({ command }) => {
+  const vitePlugins = [
+    vue({
+      reactivityTransform: true,
+    }),
+    vueJsx(),
+  ];
+
+  const configs: Array<Partial<UserConfig>> = [];
+  configs.push(
+    {
+      define: {
+        __VUE_PROD_DEVTOOLS__: true,
+      },
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('src', import.meta.url)),
+        },
+      },
+    },
+    {
+      plugins: vitePlugins,
+    },
+  );
+
+  return _.merge({}, ...configs) as UserConfig;
 });
