6a1a8a2aa58dde3b60784e63f6faecb63e72d32b
Deep Panchal <deep302001@gmail.com>
2022-12-11 11:49:36 -0600

    feat(config): update xo config to use eslint rules override from eslintrc.cjs
    (cherry picked from commit f1298599f83829c53bd54560740eae70ddeab058)

--- xo.config.cjs
+++ xo.config.cjs
@@ -1,13 +1,9 @@
+const eslintConfig = require('./.eslintrc.cjs');
+
 module.exports = {
   prettier: true,
   envs: ['browser', 'es2022'],
   rules: {
-    '@typescript-eslint/naming-convention': [
-      'warn',
-      {
-        selector: 'variable',
-        format: ['camelCase'],
-      },
-    ],
+    ...eslintConfig.rules,
   },
 };
