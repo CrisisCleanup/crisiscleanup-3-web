e20f5fb4fbcbbffe494f38747f5a538cfd0c0625
Deep Panchal <deep302001@gmail.com>
2022-12-11 10:44:59 -0600

    feat(config): extend eslint config with xo config from node_modules to show errors/warnings in editor
    (cherry picked from commit 013d3fa0feb03992bc4cc1f6b9cc2108a5c32cac)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,23 @@
+const path = require('path');
+
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
     'plugin:prettier/recommended',
+    // show eslint errors/warnings in the editor
+    path.join(__dirname, 'node_modules/xo/config/plugins.cjs'),
   ],
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    "project": ["tsconfig.json"]
+    project: ['tsconfig.json'],
   },
-  plugins: [
-    'vue',
-    "@typescript-eslint",
-  ],
-  rules: {
-  }
-}
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+};
