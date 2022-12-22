46e6c407dad7787eb3c92ed54e35853c8504b98b
Deep Panchal <deep302001@gmail.com>
2022-12-22 11:14:48 -0600

    refactor(routes): move org routes from router.ts to pages/organization/routes.ts
    (cherry picked from commit 6aa8693a50f4f88fdafac5e0a4d568d19b8f001e)

---
+++ src/pages/organization/routes.ts
@@ -0,0 +1,75 @@
+import Affiliates from '@/pages/organization/Affiliates.vue';
+import Organization from '@/pages/organization/Index.vue';
+import Invitations from '@/pages/organization/Invitations.vue';
+import Layers from '@/pages/organization/Layers.vue';
+import OrganizationProfile from '@/pages/organization/Profile.vue';
+import Users from '@/pages/organization/Users.vue';
+import UserView from '@/pages/organization/UserView.vue';
+import Teams from '@/pages/organization/Teams.vue';
+import TeamDetail from '@/pages/organization/TeamDetail.vue';
+
+export const routes = [
+  {
+    path: '/organization',
+    component: Organization,
+    name: 'nav.organization',
+    children: [
+      {
+        path: '',
+        component: Invitations,
+        name: 'nav.organization_invitations',
+        meta: { id: 'invitations' },
+      },
+      {
+        path: 'invitations',
+        component: Invitations,
+        name: 'nav.organization_invitations',
+        meta: { id: 'invitations' },
+      },
+      {
+        path: 'users',
+        component: Users,
+        name: 'nav.organization_users',
+        children: [
+          {
+            path: ':user_id',
+            component: UserView,
+            name: 'nav.organization_users',
+            meta: { id: 'user_detail' },
+          },
+        ],
+      },
+      {
+        path: 'affiliates',
+        component: Affiliates,
+        name: 'nav.organization_affiliates',
+      },
+      {
+        path: 'profile',
+        component: OrganizationProfile,
+        name: 'nav.organization_profile',
+      },
+      {
+        path: 'teams',
+        component: Teams,
+        name: 'nav.organization_teams',
+        children: [
+          {
+            path: ':team_id',
+            component: TeamDetail,
+            name: 'nav.organization_team_detail',
+            meta: { id: 'team_detail' },
+          },
+        ],
+      },
+      {
+        path: 'layers',
+        component: Layers,
+        name: 'nav.organization_layers',
+      },
+    ],
+    meta: { layout: 'authenticated' },
+  },
+];
+
+export default routes;
