ed83648eff298fc8a352e32fd9bd5a5b648ccd10
Deep Panchal <deep302001@gmail.com>
2022-12-22 15:47:37 -0600

    feat(migrate): improve+migrate pages/organization/Invitations.vue, add typings
    (cherry picked from commit 277cf4a6012328769d59fbc36b2564a585f9f2bd)

--- src/pages/organization/Invitations.vue
+++ src/pages/organization/Invitations.vue
@@ -24,11 +24,11 @@
         </div>
       </div>
       <Table
+        ref="invitationRequestsTable"
         class="border text-xs"
         :data="invitationRequests"
         :columns="currentRequestsColumns"
         :loading="false"
-        ref="invitationRequestsTable"
         :sorter="invitationRequestsSorter"
         @change="handleInvitationRequestsTableChange"
       >
@@ -97,13 +97,13 @@
         </div>
       </div>
       <Table
+        ref="invitationsTable"
         class="border text-xs mt-4"
         :data="invitations"
         :columns="invitationsColumns"
         :loading="false"
-        @change="handleInvitationsTableChange"
         :sorter="invitationSorter"
-        ref="invitationsTable"
+        @change="handleInvitationsTableChange"
       >
         <template #actions="slotProps">
           <div class="flex mr-2 justify-center w-full">
@@ -127,7 +127,7 @@
               :alt="$t('actions.delete_invitation')"
               type="trash"
               size="small"
-              @click.native="() => deleteInvitations([slotProps.item])"
+              @click="() => deleteInvitations([slotProps.item])"
             />
           </div>
         </template>
@@ -136,172 +136,179 @@
   </div>
 </template>
 
-<script>
-import Table from '@/components/Table';
+<script lang="ts">
+import moment from 'moment';
+import { computed, defineComponent, onMounted, reactive, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useToast } from 'vue-toastification';
+import InviteUsers from './InviteUsers.vue';
+import Table from '@/components/Table.vue';
 import InvitationRequest from '@/models/InvitationRequest';
 import Invitation from '@/models/Invitation';
-import InviteUsers from './InviteUsers';
 
-export default {
+type TableInstance = InstanceType<typeof Table>;
+type SorterObject = Record<string, unknown> & {
+  key?: string;
+  direction?: 'asc' | 'desc';
+};
+
+export default defineComponent({
   name: 'Invitations',
   components: { InviteUsers, Table },
-  data() {
-    return {
-      currentRequestsColumns: [
-        {
-          title: this.$t('invitationsVue.requestor'),
-          dataIndex: 'requestor',
-          key: 'full_name',
-          sortable: true,
+  setup(props) {
+    const { t } = useI18n();
+    const $toasted = useToast();
+    const invitationsTable = ref<TableInstance | null>(null);
+    const invitationRequestsTable = ref<TableInstance | null>(null);
+    const invitationSorter = ref<SorterObject>({});
+    const invitationRequestsSorter = ref<SorterObject>({});
+
+    const currentRequestsColumns = reactive([
+      {
+        title: t('invitationsVue.requestor'),
+        dataIndex: 'requestor',
+        key: 'full_name',
+        sortable: true,
+      },
+      {
+        title: t('invitationsVue.email'),
+        dataIndex: 'email',
+        key: 'email',
+        sortable: true,
+      },
+      {
+        title: t('invitationsVue.phone'),
+        dataIndex: 'phone',
+        key: 'mobile',
+        sortable: true,
+      },
+      {
+        title: t('invitationsVue.request_date'),
+        dataIndex: 'requested_at',
+        key: 'requested_at',
+        sortable: true,
+        transformer: (requested_at: Date) => {
+          return moment(requested_at).format('L');
         },
-        {
-          title: this.$t('invitationsVue.email'),
-          dataIndex: 'email',
-          key: 'email',
-          sortable: true,
+      },
+      {
+        title: '',
+        dataIndex: 'actions',
+        key: 'actions',
+        width: '2.5fr',
+      },
+    ]);
+    const invitationsColumns = reactive([
+      {
+        title: t('invitationsVue.email'),
+        dataIndex: 'email',
+        key: 'invitee_email',
+        width: '250px',
+        sortable: true,
+      },
+      {
+        title: t('invitationsVue.invited_by'),
+        dataIndex: 'inviter',
+        key: 'inviter',
+        width: '1fr',
+        sortable: true,
+      },
+      {
+        title: t('invitationsVue.status'),
+        dataIndex: 'status',
+        key: 'status',
+        width: '1fr',
+        sortable: true,
+      },
+      {
+        title: t('invitationsVue.expiration_date'),
+        dataIndex: 'expires_at',
+        key: 'expires_at',
+        width: '1fr',
+        transformer: (expires_at: Date) => {
+          return moment(expires_at).format('L');
         },
-        {
-          title: this.$t('invitationsVue.phone'),
-          dataIndex: 'phone',
-          key: 'mobile',
-          sortable: true,
-        },
-        {
-          title: this.$t('invitationsVue.request_date'),
-          dataIndex: 'requested_at',
-          key: 'requested_at',
-          sortable: true,
-          transformer: (requested_at) => {
-            return this.$moment(requested_at).format('L');
-          },
-        },
-        {
-          title: '',
-          dataIndex: 'actions',
-          key: 'actions',
-          width: '2.5fr',
-        },
-      ],
-      invitationsColumns: [
-        {
-          title: this.$t('invitationsVue.email'),
-          dataIndex: 'email',
-          key: 'invitee_email',
-          width: '250px',
-          sortable: true,
-        },
-        {
-          title: this.$t('invitationsVue.invited_by'),
-          dataIndex: 'inviter',
-          key: 'inviter',
-          width: '1fr',
-          sortable: true,
-        },
-        {
-          title: this.$t('invitationsVue.status'),
-          dataIndex: 'status',
-          key: 'status',
-          width: '1fr',
-          sortable: true,
-        },
-        {
-          title: this.$t('invitationsVue.expiration_date'),
-          dataIndex: 'expires_at',
-          key: 'expires_at',
-          width: '1fr',
-          transformer: (expires_at) => {
-            return this.$moment(expires_at).format('L');
-          },
-          sortable: true,
-        },
-        {
-          title: '',
-          dataIndex: 'actions',
-          key: 'actions',
-          width: '1.5fr',
-        },
-        {
-          title: '',
-          dataIndex: 'delete',
-          key: 'delete',
-          width: '0.5fr',
-        },
-      ],
-      invitationSorter: {},
-      invitationRequestsSorter: {},
-    };
-  },
-  computed: {
-    invitationRequests() {
+        sortable: true,
+      },
+      {
+        title: '',
+        dataIndex: 'actions',
+        key: 'actions',
+        width: '1.5fr',
+      },
+      {
+        title: '',
+        dataIndex: 'delete',
+        key: 'delete',
+        width: '0.5fr',
+      },
+    ]);
+
+    const invitationRequests = computed(() => {
       const baseQuery = InvitationRequest.query()
         .where('approved_at', null)
         .where('rejected_at', null);
 
-      if (this.invitationRequestsSorter.key) {
-        const { key, direction } = this.invitationRequestsSorter;
+      if (invitationRequestsSorter.value.key) {
+        const { key, direction } = invitationRequestsSorter.value;
         return baseQuery.orderBy(key, direction).get();
       }
       return baseQuery.orderBy('id', 'desc').get();
-    },
-    invitations() {
-      if (this.invitationSorter.key) {
-        const { key, direction } = this.invitationSorter;
+    });
+    const invitations = computed(() => {
+      if (invitationSorter.value.key) {
+        const { key, direction } = invitationSorter.value;
         return Invitation.query().orderBy(key, direction).get();
       }
       return Invitation.query().orderBy('id', 'desc').get();
-    },
-  },
-  async mounted() {
-    await Promise.all([
-      this.loadAllInvitationRequests(),
-      this.loadAllInvitations(),
-    ]);
-  },
-  methods: {
-    async loadAllInvitationRequests() {
+    });
+
+    async function loadAllInvitationRequests() {
       await InvitationRequest.api().get(`/invitation_requests`, {
         dataKey: 'results',
       });
-    },
-    exportInvitationsTable() {
-      this.$refs.invitationsTable.exportTableCSV();
-    },
-    exportInvitationRequestsTable() {
-      this.$refs.invitationRequestsTable.exportTableCSV();
-    },
-    async deleteExpiredInvitations() {
-      const invitations = Invitation.query().where(
-        'status',
-        (status) => status === 'Expired',
-      );
-
-      await this.deleteInvitations(invitations);
-    },
-    async loadAllInvitations() {
+    }
+    function exportInvitationsTable() {
+      if (invitationsTable.value) {
+        invitationsTable.value.exportTableCSV();
+      } else {
+        console.error('Invitations table ref not found');
+      }
+    }
+    function exportInvitationRequestsTable() {
+      if (invitationRequestsTable.value) {
+        invitationRequestsTable.value.exportTableCSV();
+      } else {
+        console.error('Invitation Requests table ref not found');
+      }
+    }
+    async function deleteExpiredInvitations() {
+      const invitations = Invitation.query()
+        .where('status', (status: string) => status === 'Expired')
+        .get();
+      await deleteInvitations(invitations);
+    }
+    async function loadAllInvitations() {
       await Invitation.api().get(`/invitations`, {
         dataKey: 'results',
       });
-    },
-    async acceptInvitationRequest(request) {
+    }
+    async function acceptInvitationRequest(request: Invitation) {
       await InvitationRequest.api().acceptInvitationRequest(request);
-      await this.loadAllInvitationRequests();
-      await this.$toasted.success(
-        this.$t('invitationsVue.invitation_request_accepted'),
-      );
-    },
-    async rejectInvitationRequest(request) {
+      await loadAllInvitationRequests();
+      await $toasted.success(t('invitationsVue.invitation_request_accepted'));
+    }
+    async function rejectInvitationRequest(request: Invitation) {
       await InvitationRequest.api().rejectInvitationRequest(request);
-      await this.loadAllInvitationRequests();
-      await this.$toasted.success(
-        this.$t('invitationsVue.invitation_request_declined'),
-      );
-    },
-    async resendInvitation(invitation) {
+      await loadAllInvitationRequests();
+      await $toasted.success(t('invitationsVue.invitation_request_declined'));
+    }
+    async function resendInvitation(invitation: Invitation) {
       await Invitation.api().resendInvitation(invitation);
-      await this.loadAllInvitations();
-      await this.$toasted.success(this.$t('invitationsVue.invitation_resent'));
-    },
-    async deleteInvitations(invitations) {
+      await loadAllInvitations();
+      await $toasted.success(t('invitationsVue.invitation_resent'));
+    }
+    async function deleteInvitations(invitations: Invitation[]) {
       await Promise.all(
         invitations.map((invitation) =>
           Invitation.api().delete(`/invitations/${invitation.id}`, {
@@ -310,19 +317,53 @@ export default {
         ),
       );
 
-      await this.$toasted.success(this.$t('invitationsVue.invitation_deleted'));
-    },
-    handleInvitationsTableChange({ sorter }) {
-      this.invitationSorter = { ...sorter };
-    },
-    handleInvitationRequestsTableChange({ sorter }) {
-      this.invitationRequestsSorter = { ...sorter };
-    },
+      await $toasted.success(t('invitationsVue.invitation_deleted'));
+    }
+    function handleInvitationsTableChange({
+      sorter,
+    }: {
+      sorter: SorterObject;
+    }) {
+      invitationSorter.value = { ...sorter };
+    }
+    function handleInvitationRequestsTableChange({
+      sorter,
+    }: {
+      sorter: SorterObject;
+    }) {
+      invitationRequestsSorter.value = { ...sorter };
+    }
+
+    onMounted(async () => {
+      await Promise.all([loadAllInvitationRequests(), loadAllInvitations()]);
+    });
+
+    return {
+      invitationRequestsTable,
+      invitationsTable,
+      currentRequestsColumns,
+      invitationsColumns,
+      invitationSorter,
+      invitationRequestsSorter,
+      invitationRequests,
+      invitations,
+      exportInvitationsTable,
+      exportInvitationRequestsTable,
+      deleteExpiredInvitations,
+      loadAllInvitationRequests,
+      loadAllInvitations,
+      acceptInvitationRequest,
+      rejectInvitationRequest,
+      resendInvitation,
+      deleteInvitations,
+      handleInvitationsTableChange,
+      handleInvitationRequestsTableChange,
+    };
   },
-};
+});
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 .table-action-button {
   @apply mx-2 shadow bg-white px-3 p-1 text-xs text-crisiscleanup-dark-300 font-light;
 }
