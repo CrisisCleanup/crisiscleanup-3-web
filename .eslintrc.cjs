5b0fd9572efbd479ea0f08b8816e5f91650e0280
Deep Panchal <deep302001@gmail.com>
2022-12-22 10:56:20 -0600

    feat(config): set correct parsers + extends for eslint
    (cherry picked from commit 727a879da9681553c4bfc7ad044379b88039e68e)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,16 +7,19 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended', // See: https://eslint.vuejs.org/user-guide/#bundle-configurations
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     // show eslint errors/warnings in the editor
     path.join(__dirname, 'node_modules/xo/config/plugins.cjs'),
   ],
   overrides: [],
+  // See: https://eslint.vuejs.org/user-guide/#how-to-use-a-custom-parser
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['tsconfig.json'],
   },
   plugins: ['vue', '@typescript-eslint'],
   rules: {
