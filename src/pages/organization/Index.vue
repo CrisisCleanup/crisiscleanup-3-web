d147ae21e7140bb6bc0acba95e152ad5c4d2fa80
Deep Panchal <deep302001@gmail.com>
2022-12-22 12:23:40 -0600

    feat(migrate): improve + migrate pages/organization/Index.vue to vue3
    (cherry picked from commit ca6850aaa430c324aa8bcfa70f7e25c9a4be5af5)

--- src/pages/organization/Index.vue
+++ src/pages/organization/Index.vue
@@ -1,24 +1,15 @@
 <template>
   <div class="flex flex-col h-full" data-cy="myorg-dashboard">
     <div
-      class="
-        h-12
-        bg-white
-        mx-5
-        border-t
-        flex
-        justify-center
-        text-crisiscleanup-grey-700
-      "
+      class="h-12 bg-white mx-5 border-t flex justify-around text-crisiscleanup-grey-700"
     >
       <template v-for="r in routes">
         <router-link
           v-if="r.isActive"
+          :key="`myorg-nav-${r.name}`"
           :to="`/organization/${r.name}`"
           class="flex w-48 justify-center mx-2 cursor-pointer"
-          tag="div"
           :data-cy="`myorg-nav-${r.name}`"
-          :key="`myorg-nav-${r.name}`"
           :class="r.class ? r.class : null"
         >
           <span class="p-3">
@@ -33,55 +24,67 @@
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { computed, defineComponent, reactive } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default defineComponent({
   name: 'Organization',
-  data() {
-    return {
-      routes: [
+  setup(props) {
+    const route = useRoute();
+    const isInvitationsActive = computed(() => {
+      return route.meta.id === 'invitations';
+    });
+
+    const routeDefaults = {
+      class: {
+        'router-link': true,
+        'router-link-active': isInvitationsActive.value,
+      },
+      isActive: true,
+    };
+
+    const routes = reactive(
+      [
         {
           name: 'invitations',
           key: 'invitation_management',
-          class: { 'router-link-active': this.isInvitationsActive },
-          isActive: true,
         },
         {
           name: 'users',
           key: 'user_management',
-          isActive: true,
         },
         {
           name: 'teams',
           key: 'team_management',
-          isActive: true,
         },
         {
           name: 'profile',
           key: 'organization_profile',
-          isActive: true,
         },
         {
           name: 'affiliates',
           key: 'affiliated_orgs',
-          isActive: true,
         },
         {
           name: 'layers',
           key: 'layer_library',
-          isActive: true,
         },
-      ],
+      ].map((r) => ({
+        ...routeDefaults,
+        ...r,
+      })),
+    );
+
+    return {
+      routes,
+      isInvitationsActive,
     };
   },
-  computed: {
-    isInvitationsActive() {
-      return this.$route.meta.id === 'invitations';
-    },
-  },
-};
+});
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 .router-link:hover {
   text-decoration: none !important;
 }
