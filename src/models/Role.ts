9169737301baeb3003a237fa7ac174c675ac81c5
Deep Panchal <deep302001@gmail.com>
2022-12-23 16:05:04 -0600

    feat(models): define fields on Role vuex model from ccu-api
    (cherry picked from commit 43fe7b4bdb4c122776c222ef28bea51edf40b768)

--- src/models/Role.ts
+++ src/models/Role.ts
@@ -3,6 +3,17 @@ import { Model } from '@vuex-orm/core';
 export default class Role extends Model {
   static entity = 'roles';
 
+  id!: string;
+  name_t!: string;
+  description_t!: string;
+  is_active!: boolean;
+  permissions!: Record<string, unknown>;
+  is_default!: boolean;
+  level!: number;
+  list_order!: number;
+  is_public!: boolean;
+  history!: any;
+
   static fields() {
     return {
       id: this.attr(''),
