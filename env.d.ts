2770cb3bbf37aca4d8282ab17f48196e61669cca
Deep Panchal <deep302001@gmail.com>
2023-01-09 11:17:25 -0600

    feat(types): add env.d.ts for env var types
    (cherry picked from commit 6337b42e66399424633458477be30dd8102ebee0)

---
+++ env.d.ts
@@ -0,0 +1,20 @@
+/// <reference types="vite/client" />
+/// <reference types="vue/macros" />
+/// <reference types="vue/macros-global" />
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_APP_API_BASE_URL: string;
+  readonly VITE_APP_PHONE_DEFAULT_USERNAME: string;
+  readonly VITE_APP_PHONE_DEFAULT_PASSWORD: string;
+  readonly VITE_APP_PITNEYBOWES_BASIC_AUTH_TOKEN: string;
+  readonly VITE_APP_WHAT_3_WORDS_API_KEY: string;
+  readonly VITE_APP_DEFAULT_CALLER_ID: string;
+  readonly VITE_APP_ENGLISH_PHONE_GATEWAY: string;
+  readonly VITE_APP_SPANISH_PHONE_GATEWAY: string;
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_STAGE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
