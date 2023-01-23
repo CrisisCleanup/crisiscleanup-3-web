ef49430542637b33a8a09fb5e3f85c891608ee34
Deep Panchal <deep302001@gmail.com>
2023-01-23 10:40:09 -0600

    fix(comps): fix issues with `UserSearchInput` component
    (cherry picked from commit a6fae87369fd939271c5f1a69df48b86967ec522)

--- src/components/UserSearchInput.vue
+++ src/components/UserSearchInput.vue
@@ -1,14 +1,23 @@
 <template>
-  <autocomplete
-    class="form-field"
-    icon="search"
-    :suggestions="userResults"
-    display-property="full_name"
+  <base-select
+    v-model="selectedUser"
     :placeholder="placeholder"
-    clear-on-selected
-    @selected="$emit('selectedUser', $event)"
-    @search="onUserSearch"
-  />
+    :options="onUserSearch"
+    :min-chars="1"
+    :add-option-on="['enter', 'tab']"
+    value-prop="id"
+    object
+    searchable
+  >
+    <template #option="{ option }">
+      <div class="flex items-center">
+        <div class="flex flex-col">
+          <span class="text-sm">{{ option.full_name }}</span>
+          <span class="text-xs">{{ option.email }}</span>
+        </div>
+      </div>
+    </template>
+  </base-select>
 </template>
 
 <script lang="ts">
@@ -23,21 +32,33 @@ export default defineComponent({
       required: false,
     },
   },
-  setup(props) {
+  emits: ['selectedUser'],
+  setup(props, { emit }) {
     const store = useStore();
-    const userResults = ref<unknown[]>([]);
-    // eslint-disable-next-line unicorn/no-array-callback-reference
+    const userResults = ref<User[]>([]);
     const currentUser = computed(() => User.find(store.getters['auth/userId']));
+    const selectedUser = ref<unknown>();
 
     async function onUserSearch(value: string) {
-      const results = await User.api().get(
-        `/users?search=${value}&limit=10&&organization=${currentUser.organization.id}`,
+      console.log('value', value);
+      const _results = await User.api().get(
+        `/users?search=${value}&limit=10&&organization=${currentUser.value.organization.id}`,
         { dataKey: 'results' },
       );
-      userResults.value = results.entities?.users || [];
+      const results = (_results.entities?.users || []) as User[];
+      userResults.value = results;
+      return results;
     }
 
+    watch(
+      () => selectedUser.value,
+      (value) => {
+        emit('selectedUser', value);
+      },
+    );
+
     return {
+      selectedUser,
       userResults,
       onUserSearch,
     };
