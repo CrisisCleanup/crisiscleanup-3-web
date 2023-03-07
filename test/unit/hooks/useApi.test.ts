489de12afa1eaa53a8f36461c22e1851f4f0d00f
Deep Panchal <deep302001@gmail.com>
2023-03-07 09:37:44 -0600

    test(hooks): improve unit tests for useApi, fix todo tests, add tests
    (cherry picked from commit 8672f531f7208ebcf381ceb869900c78595daa7a)

--- test/unit/hooks/useApi.test.ts
+++ test/unit/hooks/useApi.test.ts
@@ -1,19 +1,19 @@
-import { describe, expect, it, vi } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import { mount } from '@vue/test-utils';
 import type { AxiosInstance } from 'axios';
 import axios from 'axios';
 import { ref } from 'vue';
-import { useApi } from '@/hooks/useApi';
 import { useAxios } from '@vueuse/integrations/useAxios';
+import { useApi } from '@/hooks/useApi';
 
 vi.mock('axios');
 vi.mock('@vueuse/integrations/useAxios', () => {
-  return {
-    useAxios: (
+  const libExports = {
+    useAxios(
       url: string,
       options: Record<string, any>,
       instance: AxiosInstance,
-    ) => {
+    ) {
       const d = {
         id: 1,
         title: 'delectus aut autem',
@@ -26,10 +26,14 @@ vi.mock('@vueuse/integrations/useAxios', () => {
         isLoading: ref(false),
         isAborted: ref(false),
         error: {},
-        abort: () => {},
+        abort() {
+          console.log('abort');
+        },
       };
     },
   };
+  vi.spyOn(libExports, 'useAxios');
+  return libExports;
 });
 
 describe('useApi', () => {
@@ -47,6 +51,10 @@ describe('useApi', () => {
     },
   };
 
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   it('should instantiate', async () => {
     const wrapper = mount(componentOptions, { global: globalOptions });
     const testApi = wrapper.vm.api;
@@ -61,16 +69,65 @@ describe('useApi', () => {
       const wrapper = mount(componentOptions);
       const apiInstance = wrapper.vm.api;
       apiInstance('/users', { method: 'GET' });
-    } catch (error) {
+    } catch (error: unknown) {
       expect(error).toBeInstanceOf(Error);
     }
   });
 
-  it('works as expected', () => {
+  it('should call useAxios with correct arguments', async () => {
+    const wrapper = mount(componentOptions, { global: globalOptions });
+    const testApi = wrapper.vm.api;
+    testApi('/todos/1', {
+      method: 'GET',
+      params: {
+        type: 'all',
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer 12345',
+      },
+    });
+    expect(useAxios).toHaveBeenCalled();
+    expect(useAxios).toHaveBeenCalledTimes(1);
+    expect(useAxios).toHaveBeenCalledWith(
+      '/todos/1',
+      {
+        baseURL: 'https://jsonplaceholder.typicode.com',
+        method: 'GET',
+        params: { type: 'all' },
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer 12345',
+        },
+      },
+      axios,
+    );
+  });
+
+  it('should return correct data', async () => {
     const wrapper = mount(componentOptions, { global: globalOptions });
     const apiInstance = wrapper.vm.api;
-    const r = apiInstance('/todos/1', { method: 'GET' });
-    expect(r.response).toBeDefined();
+    const r = apiInstance('/todos/1', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer 12345',
+      },
+    });
+    expect(r.response.value).toBeDefined();
     expect(r.data.value).toBeDefined();
+    expect(r.response.value).toMatchInlineSnapshot(`
+      {
+        "completed": false,
+        "id": 1,
+        "title": "delectus aut autem",
+      }
+    `);
+    expect(r.data.value).toMatchInlineSnapshot(`
+      {
+        "completed": false,
+        "id": 1,
+        "title": "delectus aut autem",
+      }
+    `);
   });
 });
