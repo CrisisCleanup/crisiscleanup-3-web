c56b416d628cd82f57c89b8e2f916416ef76ce55
Deep Panchal <deep302001@gmail.com>
2023-03-30 12:47:07 -0500

    fix(types): add types for leaflet-pixi-overlay module as it's written in js
    (cherry picked from commit 54afafe64db0b1ace32239b814d2a3933beed76b)

--- src/types/leaflet-pixi-overlay/index.d.ts
+++ src/types/leaflet-pixi-overlay/index.d.ts
@@ -1,1 +1,42 @@
-declare module 'leaflet-pixi-overlay';
+// Types originally added from
+// https://github.com/joshuahhh/strava-atlas/blob/0866e626aa62b950b98530e5ab6c01d726fcede2/src/types.d.ts
+declare module 'leaflet-pixi-overlay' {
+  import type * as PIXI from 'pixi.js';
+  import type * as L from 'leaflet';
+
+  type LeafletPixiOverlay = {
+    key: string;
+    redraw: (data?: any) => void;
+  } & L.Layer;
+
+  type LeafletPixiOverlayOptions = {
+    pane?: string;
+    padding?: number;
+    forceCanvas?: boolean;
+    doubleBuffering?: boolean;
+    resolution?: number;
+    projectionZoom?: (map: L.Map) => number;
+    destroyInteractionManager?: boolean;
+    autoPreventDefault?: boolean;
+    preserveDrawingBuffer?: boolean;
+    clearBeforeRender?: boolean;
+    shouldRedrawOnMove?: (e: L.LeafletEvent) => boolean;
+  };
+
+  type LeafletPixiOverlayUtils = {
+    latLngToLayerPoint(latLng: L.LatLngExpression, zoom?: number): L.Point;
+    layerPointToLatLng(point: L.Point, zoom?: number): L.LatLng;
+    getScale(zoom?: number): number;
+    getRenderer(): PIXI.Renderer;
+    getContainer(): PIXI.Container;
+    getMap(): L.Map;
+  };
+
+  module 'leaflet' {
+    function pixiOverlay(
+      drawCallback: (utils: LeafletPixiOverlayUtils) => void,
+      container: PIXI.Container,
+      options?: LeafletPixiOverlayOptions,
+    ): LeafletPixiOverlay;
+  }
+}
