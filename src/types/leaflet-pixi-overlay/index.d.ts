da3f48261df39cfa58e687bbd5431bac9534577b
Deep Panchal <deep302001@gmail.com>
2023-03-30 17:05:42 -0500

    feat(types): add jsdoc comments on leaflet-pixi-overlay types
    (cherry picked from commit 30eaaf540fb7ba18ce2432c06d1b10b50e30b552)

--- src/types/leaflet-pixi-overlay/index.d.ts
+++ src/types/leaflet-pixi-overlay/index.d.ts
@@ -10,16 +10,66 @@ declare module 'leaflet-pixi-overlay' {
   } & L.Layer;
 
   type LeafletPixiOverlayOptions = {
+    /**
+     * @option pane: String = 'overlayPane'
+     * @default 'overlayPane'
+     * `Map pane` where the overlay layer will be added
+     */
     pane?: string;
+    /**
+     * @option padding: Number = 0.1
+     * How much to extend the clip area around the map view (relative to its size)
+     * e.g. 0.1 would be 10% of map view in each direction
+     */
     padding?: number;
+    /**
+     * @option forceCanvas: Boolean = false
+     * Force use of a 2d-canvas
+     */
     forceCanvas?: boolean;
+    /**
+     * @option doubleBuffering: Boolean = false
+     * Help to prevent flicker when refreshing display on some devices (e.g. iOS devices)
+     * It is ignored if rendering is done with 2d-canvas
+     */
     doubleBuffering?: boolean;
+    /**
+     * @option resolution: Number = 1
+     * Resolution of the renderer canvas
+     */
     resolution?: number;
+    /**
+     * @option projectionZoom(map: map): Number
+     * @param map
+     * return the layer projection zoom level
+     */
     projectionZoom?: (map: L.Map) => number;
+    /**
+     * @option destroyInteractionManager: Boolean = false
+     * Destroy PIXI Interaction Manager
+     */
     destroyInteractionManager?: boolean;
+    /**
+     * @option
+     * Customize PIXI Interaction Manager autoPreventDefault property
+     * This option is ignored if destroyInteractionManager is set
+     */
     autoPreventDefault?: boolean;
+    /**
+     * @option resolution: Boolean = false
+     * Enables drawing buffer preservation
+     */
     preserveDrawingBuffer?: boolean;
+    /**
+     * @option resolution: Boolean = true
+     * Clear the canvas before the new render pass
+     */
     clearBeforeRender?: boolean;
+    /**
+     * @option shouldRedrawOnMove(e: moveEvent): Boolean
+     * @param e
+     * filter move events that should trigger a layer redraw
+     */
     shouldRedrawOnMove?: (e: L.LeafletEvent) => boolean;
   };
 
