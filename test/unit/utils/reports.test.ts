4848066ee3569c9336426d6055afce963ddb1e62
Deep Panchal <deep302001@gmail.com>
2023-03-30 09:36:12 -0500

    test(types): fix types in reports.test.ts
    (cherry picked from commit 229bd064f6f5b8c366597bb1db4c725b816e3a9d)

--- test/unit/utils/reports.test.ts
+++ test/unit/utils/reports.test.ts
@@ -1,9 +1,12 @@
 import { describe, expect, test } from 'vitest';
 import reportsGraphData from '../fixtures/getReportsGraphData.json';
 import { transformGraphData, transformWidgetData } from '@/utils/reports';
+import type { ReportWidgetGraphData } from '@/utils/reports';
 
 describe('utils > reports', () => {
-  const d = Object.entries(reportsGraphData);
+  const d = Object.entries(reportsGraphData) as unknown as Array<
+    [string, ReportWidgetGraphData<Record<string, any>>]
+  >;
 
   for (const [graphName, graphValue] of d) {
     test(`transformWidgetData: ${graphName}`, () => {
