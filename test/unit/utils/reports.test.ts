8d7a17438b9a8aaf142d0cb7da03a20a5a7be480
Deep Panchal <deep302001@gmail.com>
2023-04-05 14:47:43 -0500

    test(unit): try to fix js date issue in CI when running tests for utils > reports
    (cherry picked from commit 4f8a1e76b95b23767bf4457dc7886a9200a7f3dc)

--- test/unit/utils/reports.test.ts
+++ test/unit/utils/reports.test.ts
@@ -1,21 +1,34 @@
-import { describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 import reportsGraphData from '../fixtures/getReportsGraphData.json';
+import { MOCK_DATE } from '../../helpers';
 import { transformGraphData, transformWidgetData } from '@/utils/reports';
 import type { ReportWidgetGraphData } from '@/utils/reports';
 
 describe('utils > reports', () => {
+  beforeEach(() => {
+    // tell vitest we use mocked time
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    // restoring date after each test run
+    vi.useRealTimers();
+  });
+
   const d = Object.entries(reportsGraphData) as unknown as Array<
     [string, ReportWidgetGraphData<Record<string, any>>]
   >;
 
   for (const [graphName, graphValue] of d) {
     test(`transformWidgetData: ${graphName}`, () => {
+      vi.setSystemTime(MOCK_DATE);
       const result = transformWidgetData(graphValue);
       expect(result).toMatchSnapshot();
     });
   }
 
   test('transformGraphData', () => {
+    vi.setSystemTime(MOCK_DATE);
     const result = transformGraphData(d);
     expect(result).toMatchSnapshot();
   });
