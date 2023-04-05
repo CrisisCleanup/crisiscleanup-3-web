974ba2ba79ee4d8efabeec434053be3ea26c6ab4
Deep Panchal <deep302001@gmail.com>
2023-04-05 14:31:11 -0500

    fix(types): fix types in utils > array.ts
    (cherry picked from commit 56ebfe792493cc3eb7aa441dafaf4004f65f4f53)

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,21 +1,29 @@
-export function groupBy(array, key) {
-  const result = {};
+export function groupBy<T extends Record<string, any>, K extends keyof T>(
+  array: T[],
+  key: K,
+) {
+  const result = {} as Record<T[K], T[]>;
   for (const item of array) {
     if (!result[item[key]]) {
       result[item[key]] = [];
     }
-
     result[item[key]].push(item);
   }
-
   return result;
 }
 
-export function childrenBy(dict, key) {
+/**
+ * Children by 'null' key
+ * @param dict
+ * @param key
+ */
+export function childrenBy<T extends Record<string, any>, K extends keyof T>(
+  dict: T,
+  key: keyof T['null'][0],
+) {
   const array = dict.null;
   for (const item of array) {
     item.children = dict[item[key]];
   }
-
   return array;
 }
