765be0482dd77afba1fe9b10a2aaba8ee8423996
Deep Panchal <deep302001@gmail.com>
2023-04-19 10:33:56 -0500

    refactor(types): wrap pitney-bowes types in pitneybowes namespace
    (cherry picked from commit 16e76a8e15cbcad894b039f1540f5f67af42f0c6)

--- src/types/pitney-bowes.d.ts
+++ src/types/pitney-bowes.d.ts
@@ -1,37 +1,39 @@
-export type PBDistance = {
-  unit: string;
-  value: string;
-};
+export namespace pitneybowes {
+  export type Distance = {
+    unit: string;
+    value: string;
+  };
 
-export type PBMatchedAddress = {
-  formattedAddress: string;
-  mainAddressLine: string;
-  addressLastLine: string;
-  placeName: string;
-  areaName1: string;
-  areaName2: string;
-  areaName3: string;
-  areaName4: string;
-  postCode: string;
-  postCodeExt: string;
-  country: string;
-  addressNumber: string;
-  streetName: string;
-  unitType: string;
-  unitValue: string;
-};
+  export type MatchedAddress = {
+    formattedAddress: string;
+    mainAddressLine: string;
+    addressLastLine: string;
+    placeName: string;
+    areaName1: string;
+    areaName2: string;
+    areaName3: string;
+    areaName4: string;
+    postCode: string;
+    postCodeExt: string;
+    country: string;
+    addressNumber: string;
+    streetName: string;
+    unitType: string;
+    unitValue: string;
+  };
 
-export type PBGeometry = {
-  type: string;
-  coordinates: number[];
-};
+  export type Geometry = {
+    type: string;
+    coordinates: number[];
+  };
 
-export type PBGeosearchLocation = {
-  address: PBMatchedAddress;
-  distance: PBDistance;
-  geometry: PBGeometry;
-};
+  export type GeosearchLocation = {
+    address: PBMatchedAddress;
+    distance: PBDistance;
+    geometry: PBGeometry;
+  };
 
-export type PBGeosearchLocations = {
-  location: PBGeosearchLocation[];
-};
+  export type GeosearchLocations = {
+    location: PBGeosearchLocation[];
+  };
+}
