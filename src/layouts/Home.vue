d6dbca1416d7cb201b8bdc41370df4de98f0c188
Deep Panchal <deep302001@gmail.com>
2023-05-06 16:40:05 -0500

    fix(layouts): remove extra route var caused by rebase
    (cherry picked from commit f08c87e33f850f0ea279caca58dc586308640060)

--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -35,8 +35,8 @@
       </div>
     </div>
     <div
-      class="register flex items-center justify-center m-8 w-max"
       v-if="!hideRegister"
+      class="register flex items-center justify-center m-8 w-max"
     >
       <div class="grid--actions mb-4 flex flex-col w-full">
         <base-text
@@ -171,10 +171,9 @@ export default defineComponent({
       terms: t('publicNav.terms'),
     });
 
-    const route = useRoute();
-
-    //Hide register button on pages where the user is already registering or accepting an invitation
-    const hideRegister = route.name === "nav.register" || route.name === "nav.invitation_token";
+    // Hide register button on pages where the user is already registering or accepting an invitation
+    const hideRegister =
+      route.name === 'nav.register' || route.name === 'nav.invitation_token';
 
     return {
       routes: defaultRoutes,
