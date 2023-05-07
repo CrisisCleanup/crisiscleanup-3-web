14dcce5e3b8f2c3acf06e71c8c946223762a97c1
Deep Panchal <deep302001@gmail.com>
2023-05-07 15:52:33 -0500

    feat(store): add typings to acl vuex store module
    (cherry picked from commit 3ffcd0deaa8770588ddc892643da2404398e44fc)

--- src/store/modules/acl.ts
+++ src/store/modules/acl.ts
@@ -1,54 +1,54 @@
 import Acl from 'browser-acl';
-import type State from '@vuex-orm/core/dist/src/model/contracts/State';
 import * as Sentry from '@sentry/browser';
-import User from '../../models/User';
+import type { Module } from 'vuex';
+import User from '@/models/User';
+import type { CCURootState } from '@/store/types';
 
-const AclState = {
-  acl: new Acl(),
-};
+export interface AclModuleState {
+  acl: Acl;
+}
 
-// Getters
-const getters = {
-  acl: (state: State) => state.acl,
-};
-
-// Actions
-const actions = {};
-
-// Mutations
-const mutations = {
-  addRule(state: State, { key, value }: Record<any, any>) {
-    state.acl.addRule(key, value);
-  },
-  setUserAcl(state: State, user_id: any) {
-    const user = User.find(user_id)!;
-    const { permissions, beta_features } = user;
-    Object.keys(permissions).forEach((permissionKey: string) => {
-      state.acl.rule(permissionKey, user.permissions[permissionKey]);
-    });
-    for (const feature of beta_features) {
-      state.acl.rule(`beta_feature.${feature}`, true);
-    }
-
-    state.acl.rule(
-      `development_mode`,
-      ['development', 'staging'].includes(import.meta.env.NODE_ENV),
-    );
-    state.acl.rule(`app_stage.${import.meta.env.VITE_APP_STAGE}`, true);
-
-    Sentry.setUser({
-      ...state.user,
-      id: user.id,
-      username: user.email,
-      email: user.email,
-    });
-  },
-};
-
-export default {
+const aclModule: Module<AclModuleState, CCURootState> = {
   namespaced: true,
-  state: AclState,
-  getters,
-  actions,
-  mutations,
+  state: {
+    acl: new Acl(),
+  },
+  getters: {
+    acl(state) {
+      return state.acl;
+    },
+  },
+  actions: {},
+  mutations: {
+    addRule(state, { key, value }: Record<string, any>) {
+      state.acl.addRule(key, value);
+    },
+    setUserAcl(state, user_id: any) {
+      const user = User.find(user_id)!;
+      const { permissions, beta_features } = user;
+      Object.keys(permissions).forEach((permissionKey: string) => {
+        state.acl.rule(permissionKey, user.permissions[permissionKey]);
+      });
+      for (const feature of beta_features) {
+        state.acl.rule(`beta_feature.${feature}`, true);
+      }
+
+      state.acl.rule(
+        `development_mode`,
+        ['development', 'staging'].includes(import.meta.env.NODE_ENV),
+      );
+      state.acl.rule(`app_stage.${import.meta.env.VITE_APP_STAGE}`, true);
+
+      console.log('currentState', state);
+
+      Sentry.setUser({
+        ...state.user,
+        id: user.id,
+        username: user.email,
+        email: user.email,
+      });
+    },
+  },
 };
+
+export default aclModule;
