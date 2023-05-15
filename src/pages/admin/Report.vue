e0a8ca06e8160afab29be98da524c9070f8f780a
Tobi Abiodun <abiodun.tobi@gmail.com>
2023-05-15 12:01:03 -0400

    feat: Small change to support report activation logic
    (cherry picked from commit 98c74561bff9552bdcd20cd5c1318c1ae366bd91)

--- src/pages/admin/Report.vue
+++ src/pages/admin/Report.vue
@@ -109,9 +109,16 @@ export default defineComponent({
             },
           },
         );
-        graphData.value = Object.entries(response.data);
+        graphData.value = Object.entries(response.data.widget_data);
       } catch (error) {
-        await $toasted.error(getErrorMessage(error));
+        if (error.response.status === 403) {
+          graphData.value = null;
+          await $toasted.error(
+            t(error.response.data.data.rule.availability_statement_t),
+          );
+        } else {
+          await $toasted.error(getErrorMessage(error));
+        }
       } finally {
         loading.value = false;
       }
