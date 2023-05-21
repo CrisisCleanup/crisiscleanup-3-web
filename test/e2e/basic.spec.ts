a9ff844cb93a26d768927e6b8d8912ce95493e3d
Deep Panchal <deep302001@gmail.com>
2023-05-21 18:49:24 -0500

    test(e2e): add e2e test to login on dev ccu page
    (cherry picked from commit 3b896696144181b31f208d6512238ee9b6591fe7)

--- test/e2e/basic.spec.ts
+++ test/e2e/basic.spec.ts
@@ -5,4 +5,26 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Crisis Cleanup/);
+
+  // Expect page url to be /login
+  await expect(page).toHaveURL(/.*\/login/);
+});
+
+test.only('should login', async ({ page }) => {
+  await page.goto('https://app.dev.crisiscleanup.io/');
+  await page.goto('https://app.dev.crisiscleanup.io/login?from=/');
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill('demo@crisiscleanup.org');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('demodemo1');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await page.waitForLoadState('networkidle');
+
+  // Expect page url to be /incident/*/dashboard
+  await expect(page).toHaveURL(/.*\/incident\/.*\/dashboard/);
+
+  // Expect dashboard span to be visible
+  const dashboardSpan = page.locator('span').filter({ hasText: 'Dashboard' });
+  await expect(dashboardSpan).toBeVisible();
 });
