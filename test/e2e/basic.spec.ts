05d0dedf2cd8f59fa8034ec8540f06418f55e86f
Deep Panchal <deep302001@gmail.com>
2023-06-16 11:17:25 -0500

    test(e2e/basic): add tags to basic e2e test
    (cherry picked from commit 8b0b209dedfac6d0c2cf1957da8dc9e8ce362381)

--- test/e2e/basic.spec.ts
+++ test/e2e/basic.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from '@playwright/test';
+import { testTitleWithTags } from './utils';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://app.dev.crisiscleanup.io/');
-
+test(testTitleWithTags('has title', ['fast', 'primary']), async ({ page }) => {
+  await page.goto('/');
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Crisis Cleanup/);
-
   // Expect page url to be /login
   await expect(page).toHaveURL(/.*\/login/);
 });
