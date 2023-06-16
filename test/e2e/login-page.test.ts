b4fec9c8e986f3650e730396e0391eafb7c9de5d
Deep Panchal <deep302001@gmail.com>
2023-06-16 11:21:28 -0500

    test(e2e): add should have data-testids test on login page w/ fast & primary tags
    (cherry picked from commit 238f97035ea4991a4ae6a3163405379b9d3d6b7f)

--- test/e2e/login-page.test.ts
+++ test/e2e/login-page.test.ts
@@ -1,30 +1,35 @@
 import { test, expect } from '@playwright/test';
+import { testTitleWithTags, getAllTestIds } from './utils';
 
 test.describe('LoginPage', () => {
-  test('should login', async ({ page }) => {
-    await page.goto('https://app.dev.crisiscleanup.io/');
-    await page.goto('https://app.dev.crisiscleanup.io/login?from=/');
-    await page.getByPlaceholder('Email').click();
-    await page.getByPlaceholder('Email').fill('demo@crisiscleanup.org');
-    await page.getByPlaceholder('Password').click();
-    await page.getByPlaceholder('Password').fill('demodemo1');
-    await page.getByRole('button', { name: 'Login' }).click();
-
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
     await page.waitForLoadState('networkidle');
-
-    // Expect page url to be /incident/*/dashboard
-    await expect(page).toHaveURL(/.*\/incident\/.*\/dashboard/);
-
-    // Expect dashboard span to be visible
-    const dashboardSpan = page.locator('span').filter({ hasText: 'Dashboard' });
-    await expect(dashboardSpan).toBeVisible();
   });
 
-  test('checks for powered by aws link', async ({ page }) => {
-    // grab all data test ids on the page
-    await page.goto('https://app.dev.crisiscleanup.io/');
-    await page.goto('https://app.dev.crisiscleanup.io/login?from=/');
-    const awsLink = page.getByTestId('testAwsLink');
-    expect(awsLink).toBeDefined();
-  });
+  test(
+    testTitleWithTags(`should have data-testids`, ['fast', 'primary']),
+    async ({ page }) => {
+      const dataTestIds = await getAllTestIds(page);
+      expect(dataTestIds).toMatchObject([
+        'testIsUnauthenticatedDiv',
+        'testLogoIcon',
+        'testSurvivorContactDiv',
+        'testNavRouteblogLink',
+        'testNavRoutecontactLink',
+        'testRegisterButton',
+        'testLoginTextContent',
+        'testSigninTextContent',
+        'testEmailTextInput',
+        'testPasswordTextInput',
+        'testRequestPasswordResetLink',
+        'testLoginButton',
+        'testRequestAccessButton',
+        'testGlobeIcon',
+        'testBottomSectionDiv',
+        'testAwsLink',
+        'testAwsImgIcon',
+      ]);
+    },
+  );
 });
