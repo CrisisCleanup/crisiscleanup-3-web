b779a25c776cfc06fac9ffa90b16402ab66af66d
Deep Panchal <deep302001@gmail.com>
2023-06-16 10:28:14 -0500

    test(e2e/utils): add getAllTestIds helper util to get all testids on a page
    (cherry picked from commit 5f4f4d640afda423dd3d1bccdb3e6c827da0c261)

---
+++ test/e2e/utils.ts
@@ -0,0 +1,15 @@
+import type { Page } from '@playwright/test';
+
+/**
+ * Query for all data-testid on a given page.
+ * @param page
+ * @param testIdIdentifier
+ */
+export async function getAllTestIds(page: Page, testIdIdentifier = 'testid') {
+  const locators = await page.$$(`[data-${testIdIdentifier}]`);
+  const dataTestIds = await Promise.all(
+    locators.map(async (e) => e.getAttribute(`data-${testIdIdentifier}`)),
+  );
+  console.info(`Found all testIds on ${page.url()}`, dataTestIds);
+  return dataTestIds;
+}
