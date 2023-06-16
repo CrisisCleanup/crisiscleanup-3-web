97606210ec925511fa423be65726870aedd27faa
Triston Lewis <tony.lewis0398@gmail.com>
2023-06-16 10:01:33 -0500

    fix(tickets): add macros, ticket info for cc user
    (cherry picked from commit 7da567bbe638c1576d5ca3ea8cf0ac4acc5c58da)

--- src/components/Tickets/TicketCards.vue
+++ src/components/Tickets/TicketCards.vue
@@ -1,23 +1,14 @@
 <script lang="ts" setup>
 import axios from 'axios';
 import _ from 'lodash';
-// import Dropdown from 'primevue/dropdown'
-// import { useRouter } from '../../../../node_modules/vue-router'
-// import { useToast } from '../../node_modules/vue-toastification'
-// import Textarea from 'primevue/textarea';
-import {
-  computed,
-  defineEmits,
-  defineProps,
-  onMounted,
-  ref,
-  withDefaults,
-  watchEffect,
-  watch,
-} from 'vue';
+import { useStore } from 'vuex';
+import moment from 'moment';
+import Table from '../Table.vue';
+import Modal from '@/components/Modal.vue';
 import BaseText from '@/components/BaseText.vue';
 import BaseButton from '@/components/BaseButton.vue';
 import BaseInput from '@/components/BaseInput.vue';
+import User from '@/models/User';
 import BaseSelect from '@/components/BaseSelect.vue';
 
 const props = defineProps({
@@ -31,17 +22,19 @@ const props = defineProps({
   rawTickets: {
     type: Array,
   },
+  agents: {
+    type: Array,
+  },
+  currentUser: {
+    type: Object,
+  },
 });
 
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_APP_API_BASE_URL}/zendesk`,
 });
-
 const emit = defineEmits<{ (e: 'reFetchTicket'): void }>();
-
-// here temporarily till i pull it from cc page
-const userLoggedIn = ref('Triston Lewis');
-
+const store = useStore();
 const userMap = ref({});
 const userEmailMap = ref({});
 const expanded = ref(false);
@@ -55,6 +48,58 @@ const selectedAgent = ref('');
 const agentList = ref([]);
 const agentDropdown = ref([]);
 const customerList = ref([]);
+const macros = ref([
+  {
+    id: 1,
+    name: 'Test Macro',
+    description: 'This is just for user testing of macros',
+    template: 'This is a test',
+    actions() {
+      console.log('macro applied');
+    },
+  },
+  {
+    id: 2,
+    name: '2nd Test',
+    description: 'This is just for user testing of macros',
+    template: '2nd test',
+    actions() {
+      console.log('macro applied');
+    },
+  },
+]);
+const macroColumns = [
+  {
+    key: 'id',
+    title: 'ID',
+    sortable: true,
+    searchable: true,
+    width: '1fr',
+  },
+  {
+    key: 'name',
+    title: 'Name',
+    sortable: true,
+    searchable: false,
+    width: '1fr',
+  },
+  {
+    key: 'description',
+    title: 'Description',
+    sortable: true,
+    searchable: false,
+    width: '1fr',
+  },
+  {
+    key: 'actions',
+    title: 'Actions',
+    sortable: true,
+    searchable: false,
+    width: '1fr',
+  },
+];
+const macroModalVisibility = ref(false);
+const selectedMacroId = ref();
 
 const processedUsers = () => {
   if (Array.isArray(props.users)) {
@@ -129,8 +174,8 @@ const capitalizeFirstLetter = (string: string) => {
 };
 
 const getCurrentLoggedInUser = () => {
-  currentUserID.value = agentList.value.find(
-    (agent) => agent.name === userLoggedIn.value,
+  currentUserID.value = props.agents.find(
+    (agent) => agent.name === props.currentUser.full_name,
   )?.id;
 };
 
@@ -157,7 +202,6 @@ const getComments = () => {
     .get(`/tickets/${props.ticketData.id}/comments`, {})
     .then((response: any) => {
       comments.value = response.data.comments;
-      console.log('these are the comments', comments.value);
     });
 };
 
@@ -169,6 +213,104 @@ const messageHighlights = (item) => {
   return 'bg-gray-400';
 };
 
+const showMacroModal = () => {
+  macroModalVisibility.value = !macroModalVisibility.value;
+};
+
+const selectedMacro = computed(() => {
+  return macros.value.find((m) => {
+    return m.id === selectedMacroId.value;
+  });
+});
+
+const addMacro = () => {
+  console.log('coming soon');
+};
+
+const editMacro = (macroId) => {
+  const _macro = macros.value.find((m) => m.id === macroId);
+
+  if (_macro) {
+    // Perform your desired operations on the macro object
+    console.log('Editing macro:', _macro);
+    // Additional logic...
+  } else {
+    console.log('Macro not found');
+  }
+};
+
+const formatKey = (key: string) => {
+  return key
+    .replace(/([A-Z])/g, ' $1')
+    .replace(/^./, (str) => str.toUpperCase());
+};
+
+const userStats = [
+  {
+    org: null,
+    roles: null,
+    ccAge: null,
+    lastActive: null,
+    timezone: null,
+    email: null,
+    phone: null,
+    totalTickets: null,
+    os: null,
+  },
+];
+
+const formatDateString = (date: string, format: string) => {
+  return moment(date).format(format);
+};
+
+const ccUser = ref();
+async function getCCUser() {
+  // 16781124470797 == ccid
+  // 360042012811 = cc email
+  try {
+    const _userId =
+      props.ticketData.custom_fields.find(
+        (field) => field.id === 360_042_012_811,
+      )?.value || null;
+    if (_userId !== null) {
+      const _response = await User.api().get(`/users?id__in=${_userId}`, {
+        dataKey: 'results',
+      });
+      ccUser.value = _response.entities.users[0];
+
+      userStats[0].org = ccUser.value.organization.name;
+      userStats[0].roles = ccUser.value.roles;
+      userStats[0].email = ccUser.value.email;
+      userStats[0].phone = ccUser.value.mobile;
+      userStats[0].ccAge = formatDateString(
+        ccUser.value.accepted_terms_timestamp,
+        'MM/DD/YYYY, h:mm:ss A',
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// async function loginAs(userId: string) {
+//   console.log('waiting on merged user data to get user id')
+//   const response = await axios.post(
+//     `${import.meta.env.VITE_APP_API_BASE_URL}/admins/users/login_as`,
+//     {
+//       user: userId,
+//     },
+//   );
+//   store.commit('auth/setUser', response.data);
+//   window.location.replace('/');
+// }
+
+watch(
+  () => selectedMacroId.value,
+  () => {
+    ticketReply.value = selectedMacro.value.template;
+  },
+);
+
 onMounted(() => {
   isLoading.value = true;
   if (props.users) {
@@ -177,6 +319,8 @@ onMounted(() => {
     console.log('users prop is not ready');
   }
 
+  getCurrentLoggedInUser();
+  getCCUser();
   getComments();
   // getAgentList();
   isLoading.value = false;
@@ -184,126 +328,251 @@ onMounted(() => {
 </script>
 
 <template>
+  <!--    {{ccUser}}-->
+
   <div class="ticket__container">
-    <div class="ticket__header">
-      <div class="submitter-info">
-        <BaseText>
-          {{ userMapFunction(ticketData.requester_id) || ticketData.name }}
+    <div class="cc__user-info">
+      <div class="cc_user">
+        <img
+          alt="picture of user"
+          :src="ccUser?.profilePictureUrl || 'https://picsum.photos/200'"
+          class="w-full"
+        />
+        <BaseText>{{
+          ccUser
+            ? ccUser.full_name
+            : userMapFunction(ticketData.requester_id) || ticketData.name
+        }}</BaseText>
+      </div>
+      <div
+        v-for="type in [
+          { name: 'Survivor', color: '#3498DB' },
+          { name: 'Ghost', color: '#F39C12' },
+          { name: 'Worksite', color: '#27AE60' },
+        ]"
+        :key="type.name"
+        :style="`background-color: ${type.color}`"
+        class="user-type border rounded-md text-center p-2 mx-4 my-1 text-xl text-white"
+      >
+        {{ type.name }}
+      </div>
+      <div class="flex items-center justify-center">
+        <BaseButton
+          :action="() => loginAs()"
+          text="Ghost In"
+          variant="primary"
+          class="text-white p-2 mx-4 my-1 text-xl rounded-md w-full"
+        />
+      </div>
+      <div
+        v-for="stats in userStats"
+        :key="stats.org"
+        class="info flex flex-col m-4"
+      >
+        <BaseText v-for="(value, key) in stats" :key="key">
+          <template v-if="!value" #default><span></span></template>
+          <template v-else #default>
+            <span class="font-bold text-xl">{{ formatKey(key) }}: </span>
+            <span class="text-lg">{{ value }}</span>
+          </template>
         </BaseText>
+      </div>
+      <div class="events m-2 text-xs border p-2 flex flex-col gap-2">
+        <BaseText class="text-center">Recent Events:</BaseText>
+        <div>
+          <span class="underline text-primary-light">D W</span> from Crisis
+          Cleanup User <span class="font-bold">Has Loggin In</span>
+        </div>
+        <div>
+          <span class="underline text-primary-light">D W</span> from Crisis
+          Cleanup User
+          <span class="font-bold">Create Case W774 in Hurricane Nicole</span>
+        </div>
+      </div>
+    </div>
+    <div class="col-span-12 md:col-span-9">
+      <div class="ticket__header">
+        <div class="submitter-info">
+          <BaseText>
+            {{ userMapFunction(ticketData.requester_id) || ticketData.name }}
+          </BaseText>
+          <hr />
+          <BaseText>
+            {{
+              userEmailMapFunction(ticketData.requester_id) || ticketData.email
+            }}
+          </BaseText>
+        </div>
+        <div class="ticket-link">
+          <a
+            :href="`https://crisiscleanup.zendesk.com/agent/tickets/${ticketData.id}`"
+          >
+            Launch Ticket</a
+          >
+        </div>
+        <div :class="[ticketData.status, 'ticket-status']">
+          {{ capitalizeFirstLetter(ticketData.status) }}
+        </div>
+      </div>
+
+      <div class="subject-date__container">
+        <BaseText class="">
+          <span class="text-base font-bold">Created on: </span>
+          {{ new Date(ticketData.created_at) }}</BaseText
+        >
         <hr />
         <BaseText>
+          <span class="text-base font-bold"> Subject: </span>
           {{
-            userEmailMapFunction(ticketData.requester_id) || ticketData.email
-          }}
+            expanded ? ticketData?.description : ticketData?.raw_subject
+          }}</BaseText
+        >
+        <BaseText
+          class="m-2 text-primary-light"
+          @click="() => (expanded = !expanded)"
+        >
+          {{ expanded ? 'Less' : 'More' }}
         </BaseText>
       </div>
-      <div class="ticket-link">
-        <a
-          :href="`https://crisiscleanup.zendesk.com/agent/tickets/${ticketData.id}`"
+
+      <div class="comments__container">
+        <BaseText class="comments__header">Comments</BaseText>
+        <div
+          v-for="(item, comment_idx) in comments"
+          :key="comment_idx"
+          class="comments__items"
+          :class="messageHighlights(item)"
         >
-          Launch Ticket</a
-        >
-      </div>
-      <div :class="[ticketData.status, 'ticket-status']">
-        {{ capitalizeFirstLetter(ticketData.status) }}
-      </div>
-    </div>
+          <BaseText class="text-3xl font-bold"
+            >{{ userMapFunction(item.author_id) || ticketData.name }}
+          </BaseText>
+          <br />
+          <BaseText>{{ item.body }}</BaseText>
 
-    <div class="subject-date__container">
-      <BaseText class="">
-        <span class="text-base font-bold">Created on: </span>
-        {{ new Date(ticketData.created_at) }}</BaseText
-      >
-      <hr />
-      <BaseText>
-        <span class="text-base font-bold"> Subject: </span>
-        {{
-          expanded ? ticketData?.description : ticketData?.raw_subject
-        }}</BaseText
-      >
-      <BaseText
-        class="m-2 text-primary-light"
-        @click="() => (expanded = !expanded)"
-      >
-        {{ expanded ? 'Less' : 'More' }}
-      </BaseText>
-    </div>
-
-    <div class="comments__container">
-      <BaseText class="comments__header">Comments</BaseText>
-      <div
-        v-for="(item, idx) in comments"
-        :key="idx"
-        class="comments__items"
-        :class="messageHighlights(item)"
-      >
-        <BaseText class="text-3xl font-bold"
-          >{{ userMapFunction(item.author_id) || ticketData.name }}
-        </BaseText>
-        <br />
-        <BaseText>{{ item.body }}</BaseText>
-
-        <div v-if="item.attachments[0]" class="attachments-container">
-          <hr />
-          <span class="attachments__header">Attachments</span>
-          <div class="attachments__items">
-            <div
-              v-for="(attachment, idx) in item.attachments"
-              :key="idx"
-              class="flex flex-col items-center justify-center border"
-            >
-              <a :href="attachment.content_url" target="_blank ">
-                <img :src="attachment.content_url" class="w-14 h-14" />
-              </a>
+          <div v-if="item.attachments[0]" class="attachments-container">
+            <hr />
+            <span class="attachments__header">Attachments</span>
+            <div class="attachments__items">
+              <div
+                v-for="(attachment, attachment_idx) in item.attachments"
+                :key="attachment_idx"
+                class="flex flex-col items-center justify-center border"
+              >
+                <a :href="attachment.content_url" target="_blank ">
+                  <img :src="attachment.content_url" class="w-14 h-14" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="ticket-reply">
-      <textarea
-        v-model="ticketReply"
-        class="w-full h-full"
-        placeholder="Ticket Reply"
-      />
-    </div>
-
-    <div class="assigned-to__container">
-      <div class="header">
-        <!--        <img src="" class="w-10 h-10 rounded-md" />-->
-        <BaseText
-          >Assigned To:
-          <span class="font-bold">{{
-            assignedUser ? assignedUser.name : 'None'
-          }}</span></BaseText
-        >
-      </div>
-      <BaseSelect
-        v-model="selectedAgent"
-        class="agent-selection"
-        place-holder="Select Agent"
-        :options="agentDropdown"
-      />
-      <BaseButton
-        variant="primary"
-        text="Reassign Ticket"
-        class="reassign-button"
-        :action="() => reAssignTicket"
-      />
-    </div>
-
-    <div class="reply-as">
-      <BaseText class="reply-as__header">Reply As:</BaseText>
-      <div class="buttons__container">
-        <template v-for="status in ['open', 'pending', 'solved']" :key="status">
-          <BaseButton
-            size="md"
-            :text="status.charAt(0).toUpperCase() + status.slice(1)"
-            :class="[status, 'w-1/3']"
-            :action="() => replyToTicket(status)"
+      <div class="ticket-reply">
+        <div class="grid grid-cols-12">
+          <BaseInput
+            v-model="ticketReply"
+            text-area
+            class="w-full h-full col-span-11"
+            placeholder="Ticket Reply"
           />
-        </template>
+          <BaseButton
+            class="rounded-sm m-2"
+            :action="showMacroModal"
+            size="sm"
+            text="Macros"
+            variant="primary"
+          />
+        </div>
+
+        <BaseSelect
+          class="my-2"
+          select-classes="w-full absolute inset-0 outline-none focus:ring-0 appearance-none border-0 text-base font-sans bg-white rounded py-2"
+          placeholder="Apply Macro"
+          :options="macros"
+          :model-value="selectedMacroId"
+          item-key="id"
+          label="name"
+          @update:model-value="(v) => (selectedMacroId = v)"
+        />
+        <modal
+          v-if="macroModalVisibility"
+          closeable
+          title="Macros"
+          class="p-10"
+          @close="showMacroModal()"
+        >
+          <template #default>
+            <div class="flex justify-end">
+              <BaseButton
+                :action="() => addMacro"
+                text="Add Macro"
+                variant="primary"
+                class="text-white p-2 mx-4 my-1 text-xl rounded-md"
+              />
+            </div>
+            <Table
+              :columns="macroColumns"
+              :data="macros"
+              @row-click="(v) => editMacro(v.id)"
+            />
+          </template>
+        </modal>
+      </div>
+
+      <div class="assigned-to__container">
+        <div class="header">
+          <!--        <img src="" class="w-10 h-10 rounded-md" />-->
+          <BaseText
+            >Assigned To:
+            <span class="font-bold">{{
+              assignedUser ? assignedUser.name : 'None'
+            }}</span></BaseText
+          >
+        </div>
+        <BaseSelect
+          v-model="selectedAgent"
+          class="agent-selection"
+          label="name"
+          placeholder="Select Agent"
+          item-key="id"
+          :options="agents"
+          @update:model-value="(v) => (selectedAgent = v)"
+        />
+        <BaseButton
+          variant="primary"
+          text="Reassign Ticket"
+          class="reassign-button"
+          :action="() => reAssignTicket"
+        />
+      </div>
+
+      <div class="reply-as">
+        <BaseText class="reply-as__header">Reply As:</BaseText>
+        <div class="buttons__container">
+          <template
+            v-for="status in ['open', 'pending', 'solved']"
+            :key="status"
+          >
+            <BaseButton
+              size="md"
+              :text="status.charAt(0).toUpperCase() + status.slice(1)"
+              :class="[status, 'w-1/3 rounded-md text-xl']"
+              :action="() => replyToTicket(status)"
+            />
+          </template>
+          <div class="flex items-center justify-center">
+            <BaseButton
+              :action="() => console.log('creating github draft')"
+              class="p-4 border border-black rounded-md w-20"
+            >
+              <template #default>
+                <img
+                  alt="github icon"
+                  src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                /> </template
+            ></BaseButton>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -311,7 +580,15 @@ onMounted(() => {
 
 <style scoped>
 .ticket__container {
-  @apply rounded-lg bg-white border border-gray-600 m-4 text-sm shadow-md;
+  @apply rounded-lg bg-white border border-gray-600 m-4 text-sm shadow-md grid grid-cols-12 overflow-hidden;
+
+  .cc__user-info {
+    @apply col-span-12 md:col-span-3  border-r-2 border-gray-400 overflow-y-auto min-h-full  h-64 md:h-24;
+    .cc_user {
+      @apply border rounded-md m-4 text-center;
+    }
+  }
+
   .ticket__header {
     @apply grid grid-cols-4 px-4 py-2 row-span-2 flex gap-2 border-b-2 border-gray-400;
     .submitter-info {
@@ -360,7 +637,7 @@ onMounted(() => {
       @apply md:col-span-4 my-2;
     }
     .reassign-button {
-      @apply md:col-span-4;
+      @apply md:col-span-4 p-2 rounded-md;
     }
   }
 
