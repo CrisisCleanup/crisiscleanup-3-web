5a2561d2e7bc6c2800a60581fa1e626779f4050a
Deep Panchal <deep302001@gmail.com>
2023-06-19 10:39:36 -0500

    test(e2e): add 'should login' test w/ dashboard info screenshot
    (cherry picked from commit 6d5aeb6bf048878b5e0f0507d8770955aaccf4e8)

--- test/e2e/login-page.test.ts
+++ test/e2e/login-page.test.ts
@@ -1,3 +1,4 @@
+import process from 'node:process';
 import { test, expect } from '@playwright/test';
 import { testTitleWithTags, getAllTestIds } from './utils';
 
@@ -6,6 +7,47 @@ test.describe('LoginPage', () => {
     await page.goto('/');
     await page.waitForLoadState('networkidle');
   });
+
+  test(
+    testTitleWithTags('should login', ['slow', 'primary']),
+    async ({ page }) => {
+      const email = process.env.TEST_APP_EMAIL;
+      const password = process.env.TEST_APP_PASSWORD;
+      if (!email || !password) {
+        throw new Error('Cant login. Email or password not defined.');
+      }
+
+      const emailField = page.getByPlaceholder('Email');
+      const passwordField = page.getByPlaceholder('Password');
+      const loginSubmitButton = page.getByTestId('testLoginButton');
+
+      await emailField.click();
+      await emailField.fill(email);
+      await passwordField.click();
+      await passwordField.fill(password);
+      await loginSubmitButton.click();
+
+      await page.waitForLoadState('networkidle');
+
+      // wait for dashboard root div
+      await page.waitForSelector('[data-testid="testDashboarddiv"]');
+
+      await expect(page).toHaveURL(/.*\/incident\/.*\/dashboard/);
+      await expect(page).toHaveTitle(/.*Dashboard.*/);
+
+      // Expect dashboard span to be visible
+      const dashboardSpan = page
+        .locator('span')
+        .filter({ hasText: 'Dashboard' });
+      await expect(dashboardSpan).toBeVisible();
+
+      // attach info screenshot to test reports
+      await test.info().attach('dashboard-page-screenshot', {
+        body: await page.screenshot(),
+        contentType: 'image/png',
+      });
+    },
+  );
 
   test(
     testTitleWithTags(`should have data-testids`, ['fast', 'primary']),
