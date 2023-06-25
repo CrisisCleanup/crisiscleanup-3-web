8bdf2ae112f52ccb8ec565e323b7a844370f4541
Deep Panchal <deep302001@gmail.com>
2023-06-25 16:39:01 -0500

    test(e2e): add test to check for localizations on page using localization regex to match for key
    (cherry picked from commit 6404f2025acda84cb331d9ece2bfdd054e810cf7)

--- test/e2e/basic.spec.ts
+++ test/e2e/basic.spec.ts
@@ -8,3 +8,34 @@ test(testTitleWithTags('has title', ['fast', 'primary']), async ({ page }) => {
   // Expect page url to be /login
   await expect(page).toHaveURL(/.*\/login/);
 });
+
+// Checks if localizations are loading on Login page
+test(
+  testTitleWithTags('should have valid localizations', ['fast', 'primary']),
+  async ({ page }) => {
+    // Should navigate to login page
+    await page.goto('/');
+
+    // Check if text on page matches the regex for localization
+    // Examples:
+    // actions.login
+    // loginForm.sign_in_msg
+    // loginForm.email_placeholder
+    // homeVue.phone_or_website
+    // publicNav.home
+    // publicNav.blog
+    // publicNav.relief_orgs_only
+
+    // Localization Regex to match
+    const localizationRegex = /^\w+(\.\w+)+$/;
+
+    const loginHeader = page.getByTestId('testLoginTextContent');
+    const loginMsg = page.getByTestId('testSigninTextContent');
+
+    // Check if login header & login msg divs have valid text
+    // We can simply tell if the current stage (i.e. development, staging, etc) is broken
+    // if any of these elements' text content matches the localization regex
+    await expect(loginHeader).not.toHaveText(localizationRegex);
+    await expect(loginMsg).not.toHaveText(localizationRegex);
+  },
+);
