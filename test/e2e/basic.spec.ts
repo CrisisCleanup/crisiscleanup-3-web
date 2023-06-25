aa0196cfe109608546704fee5883546f83c52b88
Deep Panchal <deep302001@gmail.com>
2023-06-25 16:39:34 -0500

    test(e2e): fix title for valid url & title test, format function
    (cherry picked from commit 13e06a84f051ca902ad5d98d7e9db0276d9f4ffd)

--- test/e2e/basic.spec.ts
+++ test/e2e/basic.spec.ts
@@ -1,13 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { testTitleWithTags } from './utils';
 
-test(testTitleWithTags('has title', ['fast', 'primary']), async ({ page }) => {
-  await page.goto('/');
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Crisis Cleanup/);
-  // Expect page url to be /login
-  await expect(page).toHaveURL(/.*\/login/);
-});
+test(
+  testTitleWithTags('should have valid title & url', ['fast', 'primary']),
+  async ({ page }) => {
+    await page.goto('/');
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Crisis Cleanup/);
+    // Expect page url to be /login
+    await expect(page).toHaveURL(/.*\/login/);
+  },
+);
 
 // Checks if localizations are loading on Login page
 test(
