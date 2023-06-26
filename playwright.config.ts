2823a2f679c51aac864c7ce2f434893f57104b99
Deep Panchal <deep302001@gmail.com>
2023-06-26 17:28:09 -0500

    feat(config): run local webserver before running playwright tests, serve preview of built vite app
    (cherry picked from commit 937a3732dc661ee330d1dd9564c346b280e3598a)

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm run build:app && pnpm run preview --port 8080',
+    url: 'http://127.0.0.1:8080',
+    reuseExistingServer: !process.env.CI,
+  },
 });
