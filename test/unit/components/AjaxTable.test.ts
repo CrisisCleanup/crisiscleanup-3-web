e5839e021b1662ab34e78311280e8997d95c0a87
Deep Panchal <deep302001@gmail.com>
2023-06-27 12:57:09 -0500

    test(unit/comps): setup globalOptions for AjaxTable tests to fix stub warnings when running tests
    (cherry picked from commit 084dbba0912049a12469911aece4aaa43e7a11fc)

--- test/unit/components/AjaxTable.test.ts
+++ test/unit/components/AjaxTable.test.ts
@@ -1,7 +1,23 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { mount, flushPromises } from '@vue/test-utils';
+import type { GlobalMountOptions } from '@vue/test-utils/dist/types';
 import { makeTableColumns } from '../../../src/utils/table';
 import AjaxTable from '@/components/AjaxTable.vue';
+
+const globalOptions: GlobalMountOptions = {
+  stubs: {
+    badge: true,
+    spinner: true,
+    'font-awesome-icon': true,
+    'base-text': true,
+    'base-checkbox': true,
+    'base-button': true,
+    'base-link': true,
+    'base-input': true,
+    'base-select': true,
+    'ccu-icon': true,
+  },
+};
 
 describe('AjaxTable', () => {
   it('should render', async () => {
@@ -14,6 +30,7 @@ describe('AjaxTable', () => {
         url: `${import.meta.env.VITE_APP_API_BASE_URL}/table`,
         query: {},
       },
+      global: globalOptions,
     });
     await flushPromises();
     expect(wrapper.html()).toContain('Name from table data');
@@ -31,6 +48,7 @@ describe('AjaxTable', () => {
         url: `${import.meta.env.VITE_APP_API_BASE_URL}/table`,
         query: {},
       },
+      global: globalOptions,
     });
     await flushPromises();
     expect(wrapper.html()).toContain('info.search_items');
