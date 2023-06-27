1a8a588f78ae804376cc294202428b9a4b6f9d46
Deep Panchal <deep302001@gmail.com>
2023-06-27 16:22:59 -0500

    test(globalSetup): add global teardown function
    (cherry picked from commit 8b5eb8af87b513c475f07eeeae1d5a44dd382e81)

--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -1,4 +1,10 @@
+import process from 'node:process';
+
 // src/test-globals.ts
-export const setup = () => {
+export async function setup() {
   process.env.TZ = 'UTC';
-};
+}
+
+export async function teardown() {
+  console.info('Tearing down setup');
+}
