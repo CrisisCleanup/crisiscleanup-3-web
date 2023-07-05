03beb7c2a83278312f8dd2dcc3cd12806b124b8f
Deep Panchal <deep302001@gmail.com>
2023-07-05 13:45:34 -0500

    test(e2e): add dashboard-page.test.ts w/ data-testids test, add testids snapshots
    (cherry picked from commit 03d40731cb2f3c81481277768d19c11f7ab7b2d2)

---
+++ test/e2e/dashboard-page.test.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+import {
+  testTitleWithTags,
+  getAllTestIds,
+  doLogin,
+  generateSnapshot,
+} from './utils';
+
+test.describe('DashboardPage', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    await doLogin(page);
+    await page.waitForLoadState('networkidle');
+  });
+
+  test.only(
+    testTitleWithTags(`should have data-testids`, ['fast', 'primary']),
+    async ({ page }) => {
+      const _dataTestIds = await getAllTestIds(page);
+      const dataTestIds = _dataTestIds.filter(
+        (id) =>
+          ![
+            'testShowAcceptTermsModal',
+            'testModalCancelIcon',
+            'testTermsmodalAcceptButton',
+          ].includes(id ?? ''),
+      ) as string[];
+      expect(generateSnapshot(dataTestIds)).toMatchSnapshot({
+        name: 'dashboard-page-dataTestIds',
+      });
+      await test.info().attach('dashboard-page-screenshot', {
+        body: await page.screenshot({
+          mask: dataTestIds.map((id) => page.getByTestId(id)),
+          maskColor: '#ff00ff11',
+          fullPage: true,
+        }),
+        contentType: 'image/png',
+      });
+    },
+  );
+});
