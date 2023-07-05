66089095e1e35447e16694d36745c4ca7b4451a4
Deep Panchal <deep302001@gmail.com>
2023-07-05 15:40:39 -0500

    test(e2e): add test to visit all links on dashboard and check for 200 response status
    (cherry picked from commit 43ab976b1ec75f027fb46729ec5235c26de6126d)

--- test/e2e/dashboard-page.test.ts
+++ test/e2e/dashboard-page.test.ts
@@ -41,4 +41,48 @@ test.describe('DashboardPage', () => {
       });
     },
   );
+
+  test(
+    testTitleWithTags(`should return 200 status code for all links on page`, [
+      'slow',
+      'primary',
+    ]),
+    async ({ page, context }) => {
+      const linkLocators = await page.getByRole('link').all();
+      const visitedLinks = new Set();
+      for (const link of linkLocators) {
+        const href = await link.getAttribute('href');
+        console.info('Found link', href);
+        const isVisited = visitedLinks.has(href);
+        if (isVisited) {
+          console.info('Skipping already visited link', href);
+        } else if (href && !href.startsWith('mailto:')) {
+          const newPage = await context.newPage();
+          await newPage.bringToFront();
+          const response = await newPage.goto(href, {
+            waitUntil: 'commit',
+          });
+          // Add link to visited links
+          visitedLinks.add(href);
+          if (!response) {
+            console.error('No response from', href);
+            continue;
+          }
+
+          console.info(`Response from ${href}`, {
+            url: response.url(),
+            ok: response.ok(),
+            status: response.status(),
+            headers: response.headers(),
+          });
+          const status = response.status();
+          expect(status).toBe(200);
+          // close newly opened page (tab) to avoid OOM issues
+          await newPage.close();
+          // bring root page back into focus
+          await page.bringToFront();
+        }
+      }
+    },
+  );
 });
