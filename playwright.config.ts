3a93085429cdc7b7665016ac64146fc7e0052a27
Deep Panchal <deep302001@gmail.com>
2023-07-05 12:38:40 -0500

    feat(config): set snapshotDir for e2e tests in playwright config
    (cherry picked from commit a001dcf7b5d6b11eb8bfe549a23ceb478065d59a)

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './test/e2e',
+  snapshotDir: './test/e2e/snapshots',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
