1f07edf501adec5a8060e7d8236d7ca07c2b0abe
Deep Panchal <deep302001@gmail.com>
2023-07-07 11:11:51 -0500

    feat(config): collect video on first retry
    (cherry picked from commit 4b79b34eb442456c6600c70e5e830889217b5ff6)

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
+    video: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
