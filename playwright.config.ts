85fc0b3c57d7dd4957e575c461776286da575d9a
Deep Panchal <deep302001@gmail.com>
2023-07-07 08:48:44 -0500

    feat(config): reduce test timeout to 1 min in playwright.config.ts
    (cherry picked from commit f0d7fbdad83676730dff9072ba2cbccd443b5a97)

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { open: 'never' }]],
-  timeout: 2 * 60 * 1000,
+  timeout: 1 * 60 * 1000,
   expect: {
     timeout: 10_000,
   },
