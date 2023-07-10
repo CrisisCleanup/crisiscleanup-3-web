55a4e5a8f453e60ce940396455d8d970090df5ce
Deep Panchal <deep302001@gmail.com>
2023-07-10 11:07:29 -0500

    feat(config): revert to default test timeouts for playwright
    (cherry picked from commit 2d8564d84fbefbb10bb0b0507c94c3824ae6f087)

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { open: 'never' }]],
-  timeout: 1 * 60 * 1000,
-  expect: {
-    timeout: 10_000,
-  },
+  // timeout: 1 * 60 * 1000,
+  // expect: {
+  //   timeout: 10_000,
+  // },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
