37f376c139575c4077dab09b05d1a4f8a2745abb
Braden Mars <bradenmars@bradenmars.me>
2023-07-14 06:07:50 -0500

    fix(utils.oauth2): support sha256 hashing in non-secure (http) environments
    web crypto subtle api is only available in https
    
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>
    (cherry picked from commit 4accaaadb95d15cfc2b8fa793674fa66ff340e26)

--- src/utils/oauth.ts
+++ src/utils/oauth.ts
@@ -1,26 +1,14 @@
-const crypto = window.crypto || (window as any).msCrypto;
+import sha256 from 'crypto-js/sha256';
+import Base64Url from 'crypto-js/enc-base64url';
 
 export function generateRandomString(): string {
   const array = new Uint32Array(28);
+  const crypto = window.crypto || (window as any).msCrypto;
   crypto.getRandomValues(array);
   return [...array].map((dec) => ('0' + dec.toString(16)).slice(-2)).join('');
 }
 
-function sha256(plain: string): Promise<ArrayBuffer> {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(plain);
-  return crypto.subtle.digest('SHA-256', data);
-}
-
-function base64urlencode(a: ArrayBuffer): string {
-  const uintArray = new Uint8Array(a);
-  const array = [...uintArray];
-  const str = String.fromCharCode.apply(null, array);
-  const base64 = btoa(str);
-  return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-}
-
 export async function pkceChallengeFromVerifier(v: string): Promise<string> {
-  const hashed = await sha256(v);
-  return base64urlencode(hashed);
+  const hashed = sha256(v);
+  return Base64Url.stringify(hashed);
 }
