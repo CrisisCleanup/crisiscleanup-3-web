df02f09a7d71d68a4270fd8e269132ce2247e6e7
Deep Panchal <deep302001@gmail.com>
2023-07-19 17:23:09 -0500

    feat(config): add setup step to login as user & admin and save browser storage, set setup dependency on all other projects
    (cherry picked from commit 57988645bb74f833943cb87a495056cf68182763)

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,19 +40,28 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    // Setup project
+    {
+      name: 'setup',
+      testMatch: /.*\.setup\.ts/,
+    },
+
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+      dependencies: ['setup'],
     },
 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
+      dependencies: ['setup'],
     },
 
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+      dependencies: ['setup'],
     },
 
     /* Test against mobile viewports. */
