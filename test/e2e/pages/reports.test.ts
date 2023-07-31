8c908207aa34d33aeb1f7cfc20a96eb9c79cfb1d
Deep Panchal <deep302001@gmail.com>
2023-07-31 12:23:00 -0500

    test(e2e): add data-testids test for reports page
    (cherry picked from commit a55f860e34d2e0b7e0bbdd50a557adece95d8f0e)

---
+++ test/e2e/pages/reports.test.ts
@@ -0,0 +1,50 @@
+import type { Locator } from '@playwright/test';
+import { expect, test } from '@playwright/test';
+import {
+  normalUserStatePath,
+  selectorMaskColor,
+  testTitleWithTags,
+} from '../utils';
+
+test.describe('Reports', () => {
+  test.use({ storageState: normalUserStatePath });
+
+  test.beforeEach(async ({ page }) => {
+    test.setTimeout(60_000);
+    await page.goto('/dashboard');
+    const myOrgLink = page.getByTestId('testreportsLink');
+    await myOrgLink.click();
+    await page.waitForURL(/.*\/reports.*/);
+    await page.waitForLoadState('networkidle');
+  });
+
+  test(
+    testTitleWithTags('should have visible data-testids', [
+      'primary',
+      'slow',
+      'development',
+      'staging',
+      'production',
+    ]),
+    async ({ page }) => {
+      test.slow();
+      const dataTestIds = ['testReportHeader', 'testReportsContainerDiv'];
+      const locators: Locator[] = [];
+      for (const testId of dataTestIds) {
+        const l = page.getByTestId(testId).first();
+        locators.push(l);
+        console.info('Checking visibility of', testId);
+        await expect(l).toBeVisible();
+      }
+
+      await test.info().attach('reports-page-screenshot', {
+        body: await page.screenshot({
+          mask: locators,
+          maskColor: selectorMaskColor,
+          fullPage: true,
+        }),
+        contentType: 'image/png',
+      });
+    },
+  );
+});
