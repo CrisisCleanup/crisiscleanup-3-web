1257e1021b4b562096863eb18c84d6447b46f35a
Deep Panchal <deep302001@gmail.com>
2023-07-31 11:58:47 -0500

    test(e2e): add data-testids test for other orgs page
    (cherry picked from commit 5f9a9ff94ca18888e5523f8dfc6d199720a2c255)

---
+++ test/e2e/pages/otherOrganizations.test.ts
@@ -0,0 +1,58 @@
+import type { Locator } from '@playwright/test';
+import { expect, test } from '@playwright/test';
+import {
+  normalUserStatePath,
+  selectorMaskColor,
+  testTitleWithTags,
+} from '../utils';
+
+test.describe('WorkPage', () => {
+  test.use({ storageState: normalUserStatePath });
+
+  test.beforeEach(async ({ page }) => {
+    test.setTimeout(60_000);
+    await page.goto('/dashboard');
+    const myOrgLink = page.getByTestId('testother_organizationsLink');
+    await myOrgLink.click();
+    await page.waitForURL(/.*\/other_organizations.*/);
+    await page.waitForLoadState('networkidle');
+  });
+
+  test(
+    testTitleWithTags('should have visible data-testids', [
+      'primary',
+      'slow',
+      'development',
+      'staging',
+      'production',
+    ]),
+    async ({ page }) => {
+      test.slow();
+      const dataTestIds = [
+        'testDoNotSharePublicDiv',
+        'testOrganizationsSearchTextInput',
+        'testOrganizationsDataTable',
+        // These testids are ignored because they are inside table columns
+        // 'testPrimaryContactsDiv',
+        // 'testApprovedRolesContent',
+        // 'testOverdueCountButton',
+      ];
+      const locators: Locator[] = [];
+      for (const testId of dataTestIds) {
+        const l = page.getByTestId(testId).first();
+        locators.push(l);
+        console.info('Checking visibility of', testId);
+        await expect(l).toBeVisible();
+      }
+
+      await test.info().attach('other-organization-page-screenshot', {
+        body: await page.screenshot({
+          mask: locators,
+          maskColor: selectorMaskColor,
+          fullPage: true,
+        }),
+        contentType: 'image/png',
+      });
+    },
+  );
+});
