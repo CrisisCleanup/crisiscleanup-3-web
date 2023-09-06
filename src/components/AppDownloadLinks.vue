92642c8768ff2439f2a63e10b257e97f7bce94d5
Deep Panchal <deep302001@gmail.com>
2023-09-06 14:30:10 -0500

    fix(comps): fix cyclic import issue & alt + testids for AppDownloadLinks.vue
    (cherry picked from commit d684d8f65aff46693b9aa39ec4a9617d2d9be614)

--- src/components/AppDownloadLinks.vue
+++ src/components/AppDownloadLinks.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts">
-
-import BaseText from "@/layouts/Authenticated.vue";
-import CcuIcon from "@/layouts/Authenticated.vue";
 import { useI18n } from 'vue-i18n';
+import BaseText from '@/components/BaseText.vue';
+
 const { t } = useI18n();
 </script>
 
 <template>
   <div class="flex items-center p-1">
-    <BaseText> {{ t('~~Download the App')}}</BaseText>
-    <a href="https://apps.apple.com/us/app/crisis-cleanup/id6463570192" target="_blank">
-      <CcuIcon
-        data-testid="testDownloadAndroidLink"
-        :alt="t('~~Download for Android')"
+    <BaseText> {{ t('~~Download the App') }}</BaseText>
+    <a
+      href="https://apps.apple.com/us/app/crisis-cleanup/id6463570192"
+      target="_blank"
+    >
+      <ccu-icon
+        data-testid="testDownloadIosLink"
+        :alt="t('~~Download for IOS')"
         size="large"
         class="p-1 py-2"
         type="apple-app-store"
         linked
       />
     </a>
-    <a href="https://play.google.com/store/apps/details?id=com.crisiscleanup.prod" target="_blank">
-      <CcuIcon
-        data-testid="testDownloadIosLink"
-        :alt="t('~~Download for Ios')"
+    <a
+      href="https://play.google.com/store/apps/details?id=com.crisiscleanup.prod"
+      target="_blank"
+    >
+      <ccu-icon
+        data-testid="testDownloadAndroidLink"
+        :alt="t('~~Download for Android')"
         size="large"
         class="p-1 py-2"
         type="google-play-store"
@@ -32,6 +37,4 @@ const { t } = useI18n();
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
